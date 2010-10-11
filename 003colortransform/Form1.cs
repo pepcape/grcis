4020d1946c6acc215f20c14bf7273927edebc1ae
pepcape <pepcape@gmail.com>
2010-10-11 11:43:36 +0000

    New: 003colortransform (1st version).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@8 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 003colortransform/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _003colortransform
+{
+  public partial class FormColorTransform : Form
+  {
+    protected Image inputImage = null;
+
+    public FormColorTransform ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonOpen_Click ( object sender, EventArgs e )
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
+
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      ofd.ShowDialog();
+
+      if ( ofd.FileName == "" )
+        return;
+
+      inputImage = Image.FromFile( ofd.FileName );
+
+      recompute();
+    }
+
+    private void recompute ()
+    {
+      Bitmap ibmp = (Bitmap)inputImage;
+      Bitmap bmp;
+      Transform.TransformImage( ref ibmp, out bmp, (double)numericParam.Value );
+      pictureResult.Image = bmp;
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      if ( inputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      sfd.ShowDialog();
+
+      if ( sfd.FileName == "" )
+        return;
+
+      pictureResult.Image.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void numericParam_ValueChanged ( object sender, EventArgs e )
+    {
+      recompute();
+    }
+  }
+}
