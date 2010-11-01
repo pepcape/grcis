4703c098031195edd24e8a7a36ead4bac9d9c057
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-01 13:01:10 +0000

    005bluenoise: 1st version of the project (no actual dithering yet).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@11 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 007bluenoise/Dither.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
+namespace _007bluenoise
+{
+  class Dither
+  {
+    public static void TransformImage ( Bitmap input, out Bitmap output, double param )
+    {
+      // !!!{{ TODO: modify the Floyd-Steinberg dithering here
+
+      int width = input.Width;
+      int height = input.Height;
+      output = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+      for ( int y = 0; y < height; y++ )
+        for ( int x = 0; x < width; x++ )
+        {
+          int xm1 = x > 0 ? x - 1 : x + 1;
+          int ym1 = y > 0 ? y - 1 : y + 1;
+          float gray0 = input.GetPixel( x,   y   ).GetBrightness();
+          float gray1 = input.GetPixel( xm1, y   ).GetBrightness();
+          float gray2 = input.GetPixel( x,   ym1 ).GetBrightness();
+          float gray3 = input.GetPixel( xm1, ym1 ).GetBrightness();
+          int gray = (int)(63.75 * (gray0 + gray1 + gray2 + gray3));
+          Color col = Color.FromArgb( gray, gray, gray );
+          output.SetPixel( x, y, col );
+        }
+
+      // !!!}}
+    }
+  }
+}
---
+++ 007bluenoise/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _007bluenoise
+{
+  public partial class Form1 : Form
+  {
+    protected Image inputImage = null;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonOpen_Click ( object sender, EventArgs e )
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
+
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+        return;
+
+      inputImage = Image.FromFile( ofd.FileName );
+
+      recompute();
+    }
+
+    private void recompute ()
+    {
+      if ( inputImage == null ) return;
+
+      Bitmap ibmp = (Bitmap)inputImage;
+      Bitmap bmp;
+      Dither.TransformImage( ibmp, out bmp, (double)numericParam.Value );
+      pictureBox1.Image = bmp;
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      if ( inputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      pictureBox1.Image.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void numericParam_ValueChanged ( object sender, EventArgs e )
+    {
+      recompute();
+    }
+  }
+}
