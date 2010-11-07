bbd4f6c0f77182b9013615afd5fab7865f32252a
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-07 17:41:07 +0000

    001: portability fix #2, 002: Magni-glass function fix.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@15 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 002warping/WarpMagniGlass.cs
+++ 002warping/WarpMagniGlass.cs
@@ -24,26 +24,26 @@ namespace _002warping
       }
     }
 
-    protected double tmp1, tmp2, tmp3, tmp4;
+    protected double aspectRatioTrimmed, halfFactorOffseted, halfOfAspectRatioX, halfInverseFactorOffseted, halfOfAspectRatioY;
 
     public override void F ( double x, double y, out double u, out double v )
     {
       if ( dirty )
       {
         double aspectRatio = iwidth / (double)iheight;
-        tmp1 = 0.5 * Math.Min( 1.0, aspectRatio );
-        tmp2 = 0.5 * (factor - 1.0);
-        tmp3 = 0.5 * aspectRatio;
-        tmp4 = 0.5 * (1.0 / factor - 1.0);
+        aspectRatioTrimmed = 0.5 * Math.Min( 1.0, aspectRatio );
+        halfFactorOffseted = 0.5 * (factor - 1.0);
+        halfOfAspectRatioX = 0.5 * aspectRatio;
+        halfInverseFactorOffseted = 0.5 * (1.0 / factor - 1.0);
         dirty = false;
       }
       x /= iwidth;
       y /= iheight;
-      double ax = (x - tmp3) / tmp1;
-      double ay = (y - 0.5) / tmp1;
+      double ax = (x - halfOfAspectRatioX) / aspectRatioTrimmed;
+      double ay = (y - 0.5) / aspectRatioTrimmed;
       double rr = ax * ax + ay * ay;    // radius squared
-      double mag = tmp1 * Math.Exp( tmp2 * Math.Log( rr ) );
-      u = tmp3 + mag * ax;
+      double mag = aspectRatioTrimmed * Math.Exp( halfFactorOffseted * Math.Log( rr ) );
+      u = halfOfAspectRatioX + mag * ax;
       v = 0.5 + mag * ay;
       u *= owidth;
       v *= oheight;
@@ -54,19 +54,20 @@ namespace _002warping
       if ( dirty )
       {
         double aspectRatio = iwidth / (double)iheight;
-        tmp1 = 0.5 * Math.Min( 1.0, aspectRatio );
-        tmp2 = 0.5 * (factor - 1.0);
-        tmp3 = 0.5 * aspectRatio;
-        tmp4 = 0.5 * (1.0 / factor - 1.0);
+        aspectRatioTrimmed = 0.5 * Math.Min( 1.0, aspectRatio );
+        halfFactorOffseted = 0.5 * (factor - 1.0);
+        halfOfAspectRatioX = 0.5 * aspectRatio;
+        halfOfAspectRatioY = 0.5 * 1 / aspectRatio;
+        halfInverseFactorOffseted = 0.5 * (1.0 / factor - 1.0);
         dirty = false;
       }
       u /= owidth;
       v /= oheight;
-      double ax = (u - tmp3) / tmp1;
-      double ay = (v - 0.5) / tmp1;
+      double ax = (u - 0.5) / aspectRatioTrimmed;
+      double ay = (v - 0.5) / aspectRatioTrimmed;
       double rr = ax * ax + ay * ay;    // radius squared
-      double mag = tmp1 * Math.Exp( tmp4 * Math.Log( rr ) );
-      x = tmp3 + mag * ax;
+      double mag = rr == 0 ? 0 : aspectRatioTrimmed * Math.Exp( halfInverseFactorOffseted * Math.Log( rr ) );
+      x = 0.5 + mag * ax;
       y = 0.5 + mag * ay;
       x *= iwidth;
       y *= iheight;
