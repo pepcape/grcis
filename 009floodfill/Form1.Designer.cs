3bd9ef8474287d7d9308b7b4937381e53dc90d43
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-08 14:42:32 +0000

    009 floodfill: 1st version of the project.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@19 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 009floodfill/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _009floodfill
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose ( bool disposing )
+    {
+      if ( disposing && (components != null) )
+      {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent ()
+    {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.buttonSave = new System.Windows.Forms.Button();
+      this.numericSize = new System.Windows.Forms.NumericUpDown();
+      this.label1 = new System.Windows.Forms.Label();
+      this.buttonRedraw = new System.Windows.Forms.Button();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericSize)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.panel1.AutoScroll = true;
+      this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.panel1.Controls.Add( this.pictureBox1 );
+      this.panel1.Location = new System.Drawing.Point( 13, 13 );
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size( 680, 380 );
+      this.panel1.TabIndex = 0;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point( 0, 0 );
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size( 680, 380 );
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      // 
+      // buttonSave
+      // 
+      this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.buttonSave.Location = new System.Drawing.Point( 563, 411 );
+      this.buttonSave.Name = "buttonSave";
+      this.buttonSave.Size = new System.Drawing.Size( 130, 23 );
+      this.buttonSave.TabIndex = 2;
+      this.buttonSave.Text = "Save image";
+      this.buttonSave.UseVisualStyleBackColor = true;
+      this.buttonSave.Click += new System.EventHandler( this.buttonSave_Click );
+      // 
+      // numericSize
+      // 
+      this.numericSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.numericSize.Location = new System.Drawing.Point( 50, 414 );
+      this.numericSize.Maximum = new decimal( new int[] {
+            10000,
+            0,
+            0,
+            0} );
+      this.numericSize.Name = "numericSize";
+      this.numericSize.Size = new System.Drawing.Size( 79, 20 );
+      this.numericSize.TabIndex = 3;
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point( 14, 416 );
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size( 30, 13 );
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Size:";
+      // 
+      // buttonRedraw
+      // 
+      this.buttonRedraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonRedraw.Location = new System.Drawing.Point( 249, 411 );
+      this.buttonRedraw.Name = "buttonRedraw";
+      this.buttonRedraw.Size = new System.Drawing.Size( 110, 23 );
+      this.buttonRedraw.TabIndex = 5;
+      this.buttonRedraw.Text = "Redraw";
+      this.buttonRedraw.UseVisualStyleBackColor = true;
+      this.buttonRedraw.Click += new System.EventHandler( this.buttonRedraw_Click );
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size( 712, 446 );
+      this.Controls.Add( this.buttonRedraw );
+      this.Controls.Add( this.label1 );
+      this.Controls.Add( this.numericSize );
+      this.Controls.Add( this.buttonSave );
+      this.Controls.Add( this.panel1 );
+      this.MinimumSize = new System.Drawing.Size( 620, 200 );
+      this.Name = "Form1";
+      this.Text = "009 flood-fill";
+      this.panel1.ResumeLayout( false );
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericSize)).EndInit();
+      this.ResumeLayout( false );
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button buttonSave;
+    private System.Windows.Forms.NumericUpDown numericSize;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button buttonRedraw;
+  }
+}
+
---
+++ 009floodfill/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _009floodfill
+{
+  public partial class Form1 : Form
+  {
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void redraw ()
+    {
+      Cursor.Current = Cursors.WaitCursor;
+
+      Bitmap output = new Bitmap( 512, 512, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+      int size = (int)numericSize.Value;
+
+      pictureBox1.Image = output;
+
+      Cursor.Current = Cursors.Default;
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      pictureBox1.Image.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      redraw();
+    }
+  }
+}
