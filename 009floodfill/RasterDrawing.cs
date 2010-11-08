3715e363691d17d9c8656aa91ea089f7b6927721
pepcape <pepcape@gmail.com>
2010-11-08 14:42:32 +0000

    009 floodfill: 1st version of the project.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@19 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 009floodfill/RasterDrawing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
+namespace _009floodfill
+{
+  class RasterDrawing
+  {
+    public static void FloodFill4 ( Bitmap img, int x, int y, Color color )
+    {
+      // !!!{{ TODO: put your 4-continuous flood fill algorithm here
+
+      // !!!}}
+    }
+
+    public static void FloodFill8 ( Bitmap img, int x, int y, Color color )
+    {
+      // !!!{{ TODO: put your 8-continuous flood fill algorithm here
+
+      FloodFill4( img, x, y, color );
+
+      // !!!}}
+    }
+
+    public static void DrawLine ( Bitmap img, int x1, int y1, int x2, int y2, Color color )
+    {
+    }
+
+  }
+}
