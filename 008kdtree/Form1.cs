da1f798aa857946af0f80d3f6e8908dcd6472fe6
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-08 13:04:29 +0000

    008 kdtree: 1st version of the project.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@18 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 008kdtree/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _008kdtree
+{
+  public partial class Form1 : Form
+  {
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonGenerate_Click ( object sender, EventArgs e )
+    {
+    }
+
+    private void buttonQuery_Click ( object sender, EventArgs e )
+    {
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      pictureBox1.Image.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+  }
+}
