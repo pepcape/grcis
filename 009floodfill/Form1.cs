54aba0c82c2f39ebf4f623052ba9f261a2a8ddf7
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-08 19:59:55 +0000

    009floodfill: template finished,               Raster.Draw class separated (RasterDrawing.cs source file),               time measurement, image hash.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@20 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 009floodfill/Form1.cs
+++ 009floodfill/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
+using Raster;
 
 namespace _009floodfill
 {
@@ -18,13 +20,42 @@ namespace _009floodfill
 
     private void redraw ()
     {
-      Cursor.Current = Cursors.WaitCursor;
+      const int SIZE = 600;
 
-      Bitmap output = new Bitmap( 512, 512, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
-      int size = (int)numericSize.Value;
+      Cursor.Current = Cursors.WaitCursor;
+      Bitmap output = new Bitmap( SIZE, SIZE, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+      int seed = (int)numericSeed.Value;
+      Random rnd = (seed == 0) ? new Random() : new Random( seed );
+
+      for ( int i = 0; i++ < 100;  )
+      {
+        int x1 = rnd.Next( SIZE );
+        int y1 = rnd.Next( SIZE );
+        int x2 = rnd.Next( SIZE );
+        int y2 = rnd.Next( SIZE );
+        Draw.Line( output, x1, y1, x2, y2, Color.Cyan );
+      }
+
+      // call flood-fill N times:
+      int fills = (int)numericSize.Value;
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+
+      for ( int i = 0; i++ < fills; )
+      {
+        int x = rnd.Next( SIZE );
+        int y = rnd.Next( SIZE );
+        Color col = Color.FromArgb( rnd.Next( 255 ), rnd.Next( 255 ), rnd.Next( 255 ) );
+        Draw.FloodFill4( output, x, y, col );
+      }
+
+      sw.Stop();
+      labelElapsed.Text = String.Format( "Elapsed: {0:f} s", 1.0e-3 * sw.ElapsedMilliseconds );
+
+      long hash = Draw.Hash( output );
+      labelHash.Text = String.Format( "{0:X}", hash );
 
       pictureBox1.Image = output;
-
       Cursor.Current = Cursors.Default;
     }
 
