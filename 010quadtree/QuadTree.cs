2ee8e8934ff5dd39ba0efa4cab60bb2c07d63ea4
pepcape <pepcape@gmail.com>
2010-11-15 14:43:37 +0000

    010quadtree: 1st compilable version.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@23 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 010quadtree/QuadTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
+namespace _010quadtree
+{
+  class QuadTree
+  {
+
+    protected class QTNode
+    {
+      QTNode ul, ur, ll, lr;
+
+      Color col;
+
+      public QTNode ( Color initCol )
+      {
+        ul = ur = ll = lr = null;
+        col = initCol;
+      }
+    }
+
+    #region protected data
+
+    protected QTNode root = null;
+
+    protected int width, height;
+
+    #endregion
+
+    #region constructor
+
+    public QuadTree ()
+    {
+      root = null;
+      width = height = 0;
+    }
+
+    #endregion
+
+    #region Q-tree API
+
+    public void EncodeTree ( Bitmap inp )
+    {
+      // !!!{{ TODO: add the Q-tree encoding code here
+
+
+      // !!!}}
+    }
+
+    public Bitmap DecodeTree ()
+    {
+      // !!!{{ TODO: add the Q-tree decoding here
+
+      return null;
+
+      // !!!}}
+    }
+
+    #endregion
+
+  }
+
+}
