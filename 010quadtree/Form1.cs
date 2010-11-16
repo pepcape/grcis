8f0c5909e9b6197c131769515e582d3891d565ca
pepcape <pepcape@gmail.com>
2010-11-16 14:46:23 +0000

    010quadtree: 1st working version (q-tree persistence, difference).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@24 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 010quadtree/Form1.cs
+++ 010quadtree/Form1.cs
@@ -7,60 +7,124 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 using Raster;
 
 namespace _010quadtree
 {
   public partial class Form1 : Form
   {
+    protected Bitmap inputImage = null;
+
+    protected Bitmap outputImage = null;
+
+    protected Bitmap diffImage = null;
+
     public Form1 ()
     {
       InitializeComponent();
     }
 
-    private void redraw ()
+    private void buttonLoad_Click ( object sender, EventArgs e )
     {
-      const int SIZE = 600;
+      OpenFileDialog ofd = new OpenFileDialog();
 
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
+
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+        return;
+
+      pictureBox1.Image =
+      inputImage  = (Bitmap)Image.FromFile( ofd.FileName );
+      outputImage =
+      diffImage   = null;
+    }
+
+    private void buttonGenerate_Click ( object sender, EventArgs e )
+    {
       Cursor.Current = Cursors.WaitCursor;
-      Bitmap output = new Bitmap( SIZE, SIZE, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
-      int seed = (int)numericSeed.Value;
+
+      int width  = (int)numericXres.Value;
+      int height = (int)numericYres.Value;
+      inputImage = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+      int seed   = (int)numericSeed.Value;
       Random rnd = (seed == 0) ? new Random() : new Random( seed );
 
-      for ( int i = 0; i++ < 100;  )
+      for ( int i = 0; i++ < 32; )
       {
-        int x1 = rnd.Next( SIZE );
-        int y1 = rnd.Next( SIZE );
-        int x2 = rnd.Next( SIZE );
-        int y2 = rnd.Next( SIZE );
-        Draw.Line( output, x1, y1, x2, y2, Color.Cyan );
+        int x1 = rnd.Next( width );
+        int y1 = rnd.Next( height );
+        int x2 = rnd.Next( width );
+        int y2 = rnd.Next( height );
+        Draw.Line( inputImage, x1, y1, x2, y2, Color.Yellow );
       }
 
-      // call flood-fill N times:
-      int fills = (int)numericSize.Value;
+      pictureBox1.Image = inputImage;
+      outputImage =
+      diffImage   = null;
+
+      Cursor.Current = Cursors.Default;
+    }
+
+    private void buttonRecode_Click ( object sender, EventArgs e )
+    {
+      if ( inputImage == null ) return;
+      Cursor.Current = Cursors.WaitCursor;
+
       Stopwatch sw = new Stopwatch();
       sw.Start();
 
-      for ( int i = 0; i++ < fills; )
-      {
-        int x = rnd.Next( SIZE );
-        int y = rnd.Next( SIZE );
-        Color col = Color.FromArgb( rnd.Next( 255 ), rnd.Next( 255 ), rnd.Next( 255 ) );
-        //Draw.FloodFill4( output, x, y, col );
-      }
+      // 1. quad-tree encoding
+      QuadTree qt = new QuadTree();
+      qt.EncodeTree( inputImage );
+
+      // 2. quad-tree write (disk file)
+      FileStream fs = new FileStream( "qtree.txt", FileMode.Create );
+      qt.WriteTree( fs );
+      fs.Flush();
+      long fileSize = fs.Position;
 
       sw.Stop();
-      labelElapsed.Text = String.Format( "Elapsed: {0:f} s", 1.0e-3 * sw.ElapsedMilliseconds );
+      labelElapsed.Text = String.Format( "Enc: {0:f}s, {1}kb", 1.0e-3 * sw.ElapsedMilliseconds, (fileSize + 1023L) >> 10 );
 
-      long hash = Draw.Hash( output );
-      labelHash.Text = String.Format( "{0:X}", hash );
+      // 3. quad-tree re-read (disk file)
+      fs.Seek( 0L, SeekOrigin.Begin );
+      qt = new QuadTree();
+      bool result = qt.ReadTree( fs );
+      fs.Close();
+      if ( result )
+      {
+        // 4. quad-tree rendering
+        outputImage = qt.DecodeTree();
 
-      pictureBox1.Image = output;
+        // 5. comparison
+        diffImage = new Bitmap( inputImage.Width, inputImage.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+        long diffHash = Draw.ImageCompare( inputImage, outputImage, diffImage );
+        labelResult.Text = String.Format( "Errs: {0}", diffHash );
+        pictureBox1.Image = checkDiff.Checked ? diffImage : outputImage;
+      }
+      else
+      {
+        labelResult.Text = "File error";
+        pictureBox1.Image = null;
+        outputImage = diffImage = null;
+      }
+
       Cursor.Current = Cursors.Default;
     }
 
     private void buttonSave_Click ( object sender, EventArgs e )
     {
+      if ( outputImage == null ) return;
+
       SaveFileDialog sfd = new SaveFileDialog();
       sfd.Title = "Save PNG file";
       sfd.Filter = "PNG Files|*.png";
@@ -69,12 +133,15 @@ namespace _010quadtree
       if ( sfd.ShowDialog() != DialogResult.OK )
         return;
 
-      pictureBox1.Image.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+      if ( checkDiff.Checked )
+        diffImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+      else
+        outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
     }
 
-    private void buttonRedraw_Click ( object sender, EventArgs e )
+    private void checkDiff_CheckedChanged ( object sender, EventArgs e )
     {
-      redraw();
+      pictureBox1.Image = checkDiff.Checked ? diffImage : outputImage;
     }
   }
 }
