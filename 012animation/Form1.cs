0c289bfd31df0012696152458517c0c09dfcd1aa
pepcape <pepcape@gmail.com>
2010-11-27 01:09:56 +0000

    012animation: animation rendering in separate thread,               Stop button is working at last.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@28 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 012animation/Form1.cs
+++ 012animation/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace _012animation
 {
@@ -15,37 +16,85 @@ namespace _012animation
   {
     protected Bitmap outputImage = null;
 
+    volatile protected Thread aThread = null;
+
+    volatile protected bool cont = true;
+
+    delegate void SetImageCallback ();
+
+    private void SetImage ()
+    {
+      pictureBox1.Image = outputImage;
+      pictureBox1.Invalidate();
+    }
+
+    delegate void SetTextCallback ( string text );
+
+    private void SetText ( string text )
+    {
+      labelElapsed.Text = text;
+    }
+
+    delegate void EndAnimationCallback ();
+
+    private void EndAnimation ()
+    {
+      buttonStart.Enabled = true;
+      buttonStop.Enabled  = false;
+      aThread = null;
+    }
+
     public Form1 ()
     {
       InitializeComponent();
     }
 
-    private void buttonStart_Click ( object sender, EventArgs e )
+    public void RenderAnimation ()
     {
       int maxFrame = (int)numericFrames.Value;
       int width    = (int)numericXres.Value;
       int height   = (int)numericYres.Value;
-      bool show    = checkShow.Checked;
-
-      Cursor.Current = Cursors.WaitCursor;
 
       Animation anim = new Animation();
+      SetImageCallback si = new SetImageCallback( SetImage );
+      SetTextCallback  st = new SetTextCallback( SetText );
 
       for ( int i = 0; i < maxFrame; i++ )
       {
+        if ( !cont ) return;
+
         outputImage = anim.RenderFrame( width, height, i, maxFrame );
-        if ( show )
-          pictureBox1.Image = outputImage;
-        labelElapsed.Text = String.Format( "Frame: {0}", i );
+        if ( checkShow.Checked )
+          Invoke( si );
+        Invoke( st, new object[] { String.Format( "Frame: {0}", i ) } );
         string fileName = String.Format( "out{0:0000}.png", i );
         outputImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
       }
 
-      Cursor.Current = Cursors.Default;
+      EndAnimationCallback ea = new EndAnimationCallback( EndAnimation );
+      Invoke( ea );
+    }
+
+    private void buttonStart_Click ( object sender, EventArgs e )
+    {
+      if ( aThread != null ) return;
+
+      buttonStart.Enabled = false;
+      buttonStop.Enabled  = true;
+      cont = true;
+
+      aThread = new Thread( new ThreadStart( this.RenderAnimation ) );
+      aThread.Start();
     }
 
     private void buttonStop_Click ( object sender, EventArgs e )
     {
+      if ( aThread == null ) return;
+      cont = false;
+
+      aThread.Join();
+
+      EndAnimation();
     }
   }
 }
