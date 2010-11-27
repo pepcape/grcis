d3cfdcb123597079ad5b17ad1489a59e63157271
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-27 14:19:59 +0000

    012animation: fixed multi-threaded Form update.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@29 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 012animation/Animation.cs
+++ 012animation/Animation.cs
@@ -25,12 +25,14 @@ namespace _012animation
       int maxWid = x0 - 2;
       int maxHei = y0 - 2;
 
-      Pen p = new Pen( Color.FromArgb( 255, 255, 20 ), 2.0f );
+      Pen p1 = new Pen( Color.FromArgb( 255, 255,  20 ), 1.0f );
+      Pen p2 = new Pen( Color.FromArgb(  60, 120, 255 ), 2.0f );
 
       double tim = currentFrame / (totalFrames - 1.0);
       int wid = (int)(tim * maxWid);
       int hei = (int)(tim * maxHei);
-      gr.DrawEllipse( p, x0 - wid, y0 - hei, wid + wid, hei + hei );
+      gr.DrawEllipse( p2, x0 - hei, y0 - wid, hei + hei, wid + wid );
+      gr.DrawEllipse( p1, x0 - wid, y0 - hei, wid + wid, hei + hei );
 
       return result;
 
