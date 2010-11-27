792b7b2088d396a12936246b6a278ca179eca494
pepcape <pepcape@gmail.com>
2010-11-27 14:19:59 +0000

    012animation: fixed multi-threaded Form update.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@29 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 012animation/Form1.Designer.cs
+++ 012animation/Form1.Designer.cs
@@ -46,9 +46,9 @@
       ((System.ComponentModel.ISupportInitialize)(this.numericFrames)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericYres)).BeginInit();
       this.SuspendLayout();
-      //
+      // 
       // panel1
-      //
+      // 
       this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                   | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
@@ -59,18 +59,18 @@
       this.panel1.Name = "panel1";
       this.panel1.Size = new System.Drawing.Size( 680, 380 );
       this.panel1.TabIndex = 0;
-      //
+      // 
       // pictureBox1
-      //
+      // 
       this.pictureBox1.Location = new System.Drawing.Point( 0, 0 );
       this.pictureBox1.Name = "pictureBox1";
       this.pictureBox1.Size = new System.Drawing.Size( 680, 380 );
       this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
       this.pictureBox1.TabIndex = 2;
       this.pictureBox1.TabStop = false;
-      //
+      // 
       // numericXres
-      //
+      // 
       this.numericXres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.numericXres.Location = new System.Drawing.Point( 50, 406 );
       this.numericXres.Maximum = new decimal( new int[] {
@@ -86,9 +86,9 @@
             0,
             0,
             0} );
-      //
+      // 
       // label1
-      //
+      // 
       this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label1.AutoSize = true;
       this.label1.Location = new System.Drawing.Point( 14, 408 );
@@ -96,9 +96,9 @@
       this.label1.Size = new System.Drawing.Size( 31, 13 );
       this.label1.TabIndex = 4;
       this.label1.Text = "Xres:";
-      //
+      // 
       // label2
-      //
+      // 
       this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label2.AutoSize = true;
       this.label2.Location = new System.Drawing.Point( 142, 408 );
@@ -106,9 +106,9 @@
       this.label2.Size = new System.Drawing.Size( 44, 13 );
       this.label2.TabIndex = 6;
       this.label2.Text = "Frames:";
-      //
+      // 
       // numericFrames
-      //
+      // 
       this.numericFrames.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.numericFrames.Location = new System.Drawing.Point( 192, 406 );
       this.numericFrames.Maximum = new decimal( new int[] {
@@ -124,29 +124,29 @@
             0,
             0,
             0} );
-      //
+      // 
       // labelElapsed
-      //
+      // 
       this.labelElapsed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.labelElapsed.AutoSize = true;
-      this.labelElapsed.Location = new System.Drawing.Point( 457, 444 );
+      this.labelElapsed.Location = new System.Drawing.Point( 457, 442 );
       this.labelElapsed.Name = "labelElapsed";
       this.labelElapsed.Size = new System.Drawing.Size( 48, 13 );
       this.labelElapsed.TabIndex = 8;
       this.labelElapsed.Text = "Elapsed:";
-      //
+      // 
       // label3
-      //
+      // 
       this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point( 14, 444 );
+      this.label3.Location = new System.Drawing.Point( 14, 440 );
       this.label3.Name = "label3";
       this.label3.Size = new System.Drawing.Size( 31, 13 );
       this.label3.TabIndex = 10;
       this.label3.Text = "Yres:";
-      //
+      // 
       // numericYres
-      //
+      // 
       this.numericYres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.numericYres.Location = new System.Drawing.Point( 50, 439 );
       this.numericYres.Maximum = new decimal( new int[] {
@@ -162,10 +162,10 @@
             0,
             0,
             0} );
-      //
+      // 
       // buttonStop
-      //
-      this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      // 
+      this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.buttonStop.Enabled = false;
       this.buttonStop.Location = new System.Drawing.Point( 324, 436 );
       this.buttonStop.Name = "buttonStop";
@@ -174,10 +174,10 @@
       this.buttonStop.Text = "Stop";
       this.buttonStop.UseVisualStyleBackColor = true;
       this.buttonStop.Click += new System.EventHandler( this.buttonStop_Click );
-      //
+      // 
       // buttonStart
-      //
-      this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      // 
+      this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.buttonStart.Location = new System.Drawing.Point( 324, 405 );
       this.buttonStart.Name = "buttonStart";
       this.buttonStart.Size = new System.Drawing.Size( 110, 23 );
@@ -185,9 +185,9 @@
       this.buttonStart.Text = "Start";
       this.buttonStart.UseVisualStyleBackColor = true;
       this.buttonStart.Click += new System.EventHandler( this.buttonStart_Click );
-      //
+      // 
       // checkShow
-      //
+      // 
       this.checkShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.checkShow.AutoSize = true;
       this.checkShow.Location = new System.Drawing.Point( 460, 411 );
@@ -196,9 +196,9 @@
       this.checkShow.TabIndex = 14;
       this.checkShow.Text = "Show frames?";
       this.checkShow.UseVisualStyleBackColor = true;
-      //
+      // 
       // Form1
-      //
+      // 
       this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size( 712, 474 );
--- 012animation/Form1.cs
+++ 012animation/Form1.cs
@@ -14,34 +14,55 @@ namespace _012animation
 {
   public partial class Form1 : Form
   {
-    protected Bitmap outputImage = null;
-
-    volatile protected Thread aThread = null;
+    protected Thread aThread = null;
 
     volatile protected bool cont = true;
 
-    delegate void SetImageCallback ();
+    delegate void SetImageCallback ( Bitmap newImage );
 
-    private void SetImage ()
+    protected void SetImage ( Bitmap newImage )
     {
-      pictureBox1.Image = outputImage;
-      pictureBox1.Invalidate();
+      if ( pictureBox1.InvokeRequired )
+      {
+        SetImageCallback si = new SetImageCallback( SetImage );
+        BeginInvoke( si, new object[] { newImage } );
+      }
+      else
+        if ( checkShow.Checked )
+        {
+          pictureBox1.Image = newImage;
+          pictureBox1.Invalidate();
+        }
     }
 
     delegate void SetTextCallback ( string text );
 
-    private void SetText ( string text )
+    protected void SetText ( string text )
     {
-      labelElapsed.Text = text;
+      if ( labelElapsed.InvokeRequired )
+      {
+        SetTextCallback st = new SetTextCallback( SetText );
+        BeginInvoke( st, new object[] { text } );
+      }
+      else
+        labelElapsed.Text = text;
     }
 
     delegate void EndAnimationCallback ();
 
-    private void EndAnimation ()
+    protected void EndAnimation ()
     {
-      buttonStart.Enabled = true;
-      buttonStop.Enabled  = false;
-      aThread = null;
+      if ( buttonStart.InvokeRequired )
+      {
+        EndAnimationCallback ea = new EndAnimationCallback( EndAnimation );
+        BeginInvoke( ea );
+      }
+      else
+      {
+        buttonStart.Enabled = true;
+        buttonStop.Enabled = false;
+        aThread = null;
+      }
     }
 
     public Form1 ()
@@ -56,23 +77,19 @@ namespace _012animation
       int height   = (int)numericYres.Value;
 
       Animation anim = new Animation();
-      SetImageCallback si = new SetImageCallback( SetImage );
-      SetTextCallback  st = new SetTextCallback( SetText );
 
       for ( int i = 0; i < maxFrame; i++ )
       {
         if ( !cont ) return;
 
-        outputImage = anim.RenderFrame( width, height, i, maxFrame );
-        if ( checkShow.Checked )
-          Invoke( si );
-        Invoke( st, new object[] { String.Format( "Frame: {0}", i ) } );
+        Bitmap newImage = anim.RenderFrame( width, height, i, maxFrame );
+        SetImage( (Bitmap)newImage.Clone() );
+        SetText( String.Format( "Frame: {0}", i ) );
         string fileName = String.Format( "out{0:0000}.png", i );
-        outputImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
+        newImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
       }
 
-      EndAnimationCallback ea = new EndAnimationCallback( EndAnimation );
-      Invoke( ea );
+      EndAnimation();
     }
 
     private void buttonStart_Click ( object sender, EventArgs e )
@@ -90,8 +107,8 @@ namespace _012animation
     private void buttonStop_Click ( object sender, EventArgs e )
     {
       if ( aThread == null ) return;
-      cont = false;
 
+      cont = false;
       aThread.Join();
 
       EndAnimation();
