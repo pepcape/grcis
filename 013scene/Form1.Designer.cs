993eb5070cf9427ce289974dd1f854f5ec07f5f9
pepcape <pepcape@gmail.com>
2010-11-29 05:15:39 +0000

    013scene: a new project (1st version w/o rendering), New: OpenTK library DLLs, New: sample OBJ file (Utah Teapot).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@30 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 013scene/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace _013scene
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose ( bool disposing )
+    {
+      if ( disposing && (components != null) )
+      {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent ()
+    {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.buttonOpen = new System.Windows.Forms.Button();
+      this.buttonSave = new System.Windows.Forms.Button();
+      this.numericAzimuth = new System.Windows.Forms.NumericUpDown();
+      this.label1 = new System.Windows.Forms.Label();
+      this.buttonRedraw = new System.Windows.Forms.Button();
+      this.labelFaces = new System.Windows.Forms.Label();
+      this.checkNormals = new System.Windows.Forms.CheckBox();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericAzimuth)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.panel1.AutoScroll = true;
+      this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.panel1.Controls.Add( this.pictureBox1 );
+      this.panel1.Location = new System.Drawing.Point( 13, 13 );
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size( 680, 380 );
+      this.panel1.TabIndex = 0;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point( 0, 0 );
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size( 680, 380 );
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      // 
+      // buttonOpen
+      // 
+      this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonOpen.Location = new System.Drawing.Point( 13, 411 );
+      this.buttonOpen.Name = "buttonOpen";
+      this.buttonOpen.Size = new System.Drawing.Size( 108, 23 );
+      this.buttonOpen.TabIndex = 1;
+      this.buttonOpen.Text = "Load scene";
+      this.buttonOpen.UseVisualStyleBackColor = true;
+      this.buttonOpen.Click += new System.EventHandler( this.buttonOpen_Click );
+      // 
+      // buttonSave
+      // 
+      this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.buttonSave.Location = new System.Drawing.Point( 586, 412 );
+      this.buttonSave.Name = "buttonSave";
+      this.buttonSave.Size = new System.Drawing.Size( 107, 23 );
+      this.buttonSave.TabIndex = 2;
+      this.buttonSave.Text = "Save image";
+      this.buttonSave.UseVisualStyleBackColor = true;
+      this.buttonSave.Click += new System.EventHandler( this.buttonSave_Click );
+      // 
+      // numericAzimuth
+      // 
+      this.numericAzimuth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.numericAzimuth.DecimalPlaces = 1;
+      this.numericAzimuth.Location = new System.Drawing.Point( 268, 414 );
+      this.numericAzimuth.Maximum = new decimal( new int[] {
+            360,
+            0,
+            0,
+            0} );
+      this.numericAzimuth.Name = "numericAzimuth";
+      this.numericAzimuth.Size = new System.Drawing.Size( 62, 20 );
+      this.numericAzimuth.TabIndex = 3;
+      this.numericAzimuth.Value = new decimal( new int[] {
+            20,
+            0,
+            0,
+            0} );
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point( 218, 417 );
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size( 47, 13 );
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Azimuth:";
+      // 
+      // buttonRedraw
+      // 
+      this.buttonRedraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonRedraw.Location = new System.Drawing.Point( 417, 412 );
+      this.buttonRedraw.Name = "buttonRedraw";
+      this.buttonRedraw.Size = new System.Drawing.Size( 95, 23 );
+      this.buttonRedraw.TabIndex = 5;
+      this.buttonRedraw.Text = "Redraw";
+      this.buttonRedraw.UseVisualStyleBackColor = true;
+      this.buttonRedraw.Click += new System.EventHandler( this.buttonRedraw_Click );
+      // 
+      // labelFaces
+      // 
+      this.labelFaces.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.labelFaces.AutoSize = true;
+      this.labelFaces.Location = new System.Drawing.Point( 133, 417 );
+      this.labelFaces.Name = "labelFaces";
+      this.labelFaces.Size = new System.Drawing.Size( 33, 13 );
+      this.labelFaces.TabIndex = 6;
+      this.labelFaces.Text = "faces";
+      // 
+      // checkNormals
+      // 
+      this.checkNormals.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.checkNormals.AutoSize = true;
+      this.checkNormals.Location = new System.Drawing.Point( 346, 417 );
+      this.checkNormals.Name = "checkNormals";
+      this.checkNormals.Size = new System.Drawing.Size( 62, 17 );
+      this.checkNormals.TabIndex = 7;
+      this.checkNormals.Text = "normals";
+      this.checkNormals.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size( 712, 446 );
+      this.Controls.Add( this.checkNormals );
+      this.Controls.Add( this.labelFaces );
+      this.Controls.Add( this.buttonRedraw );
+      this.Controls.Add( this.label1 );
+      this.Controls.Add( this.numericAzimuth );
+      this.Controls.Add( this.buttonSave );
+      this.Controls.Add( this.buttonOpen );
+      this.Controls.Add( this.panel1 );
+      this.MinimumSize = new System.Drawing.Size( 660, 200 );
+      this.Name = "Form1";
+      this.Text = "013 scene";
+      this.panel1.ResumeLayout( false );
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericAzimuth)).EndInit();
+      this.ResumeLayout( false );
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button buttonOpen;
+    private System.Windows.Forms.Button buttonSave;
+    private System.Windows.Forms.NumericUpDown numericAzimuth;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button buttonRedraw;
+    private System.Windows.Forms.Label labelFaces;
+    private System.Windows.Forms.CheckBox checkNormals;
+  }
+}
+
---
+++ 013scene/Scene.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Diagnostics;
+using OpenTK;
+
+namespace Scene3D
+{
+  /// <summary>
+  /// B-rep 3D scene with associated corner-table (Jarek Rossignac).
+  /// </summary>
+  public partial class SceneBrep
+  {
+    #region Constants
+
+    /// <summary>
+    /// Invalid handle (for vertices, trinagles, corners..).
+    /// </summary>
+    public const int NULL = -1;
+
+    #endregion
+
+    #region Scene data
+
+    /// <summary>
+    /// Array of vertex coordinates (float[3]).
+    /// </summary>
+    protected List<Vector3> geometry = null;
+
+    /// <summary>
+    /// Array of normal vectors (non mandatory).
+    /// </summary>
+    protected List<Vector3> normals = null;
+
+    /// <summary>
+    /// Vertex pointer (handle) for each corner.
+    /// </summary>
+    protected List<int> vertexPtr = null;
+
+    /// <summary>
+    /// Opposite corner pointer (handle) for each corner.
+    /// Valid only for topological scene (triangles are connected).
+    /// </summary>
+    protected List<int> oppositePtr = null;
+
+    #endregion
+
+    #region Construction
+
+    public SceneBrep ()
+    {
+      Reset();
+    }
+
+    #endregion
+
+    #region B-rep API
+
+    /// <summary>
+    /// [Re]initializes the scene data.
+    /// </summary>
+    public void Reset ()
+    {
+      geometry    = new List<Vector3>( 256 );
+      normals     = null;
+      vertexPtr   = new List<int>( 256 );
+      oppositePtr = null;
+    }
+
+    /// <summary>
+    /// Current number of vertices in the scene.
+    /// </summary>
+    public int Vertices
+    {
+      get
+      {
+        return (geometry == null) ? 0 : geometry.Count;
+      }
+    }
+
+    /// <summary>
+    /// Current number of triangles in the scene.
+    /// </summary>
+    public int Triangles
+    {
+      get
+      {
+        if ( vertexPtr == null ) return 0;
+        Debug.Assert( vertexPtr.Count % 3 == 0, "Invalid V[] size" );
+        return vertexPtr.Count / 3;
+      }
+    }
+
+    /// <summary>
+    /// Current number of corners in the scene (# of triangles times three).
+    /// </summary>
+    public int Corners
+    {
+      get
+      {
+        return (vertexPtr == null) ? 0 : vertexPtr.Count;
+      }
+    }
+
+    /// <summary>
+    /// Add a new vertex defined by its 3D coordinate.
+    /// </summary>
+    /// <param name="v">Vertex coordinate in the object space</param>
+    /// <returns>Vertex handle</returns>
+    public int AddVertex ( Vector3 v )
+    {
+      Debug.Assert( geometry != null );
+
+      int handle = geometry.Count;
+      geometry.Add( v );
+
+      if ( normals != null )
+      {
+        Debug.Assert( normals.Count != handle, "Invalid N[] size" );
+        normals.Add( Vector3.UnitX );
+      }
+
+      return handle;
+    }
+
+    /// <summary>
+    /// Returns object-space coordinates of the given vertex.
+    /// </summary>
+    /// <param name="v">Vertex handle</param>
+    /// <returns>Object-space coordinates</returns>
+    public Vector3 GetVertex ( int v )
+    {
+      Debug.Assert( geometry != null, "Invalid G[]" );
+      Debug.Assert( 0 <= v && v < geometry.Count, "Invalid vertex handle" );
+      return geometry[ v ];
+    }
+
+    /// <summary>
+    /// Assigns a normal vector to an existing vertex
+    /// </summary>
+    /// <param name="v">Vertex handle</param>
+    /// <param name="normal">New normal vector</param>
+    public void SetNormal ( int v, Vector3 normal )
+    {
+      Debug.Assert( geometry != null, "Invalid G[]" );
+      Debug.Assert( 0 <= v && v < geometry.Count, "Invalid vertex handle" );
+
+      if ( normals == null )
+      {
+        normals = new List<Vector3>( geometry.Count );
+        for ( int i = 0; i < geometry.Count; i++ )
+          normals.Add( Vector3.UnitX );
+      }
+
+      normals[ v ] = normal;
+    }
+
+    /// <summary>
+    /// Returns normal vector of the given vertex.
+    /// </summary>
+    /// <param name="v">Vertex handle</param>
+    /// <returns>Normal vector</returns>
+    public Vector3 GetNormal ( int v )
+    {
+      Debug.Assert( normals != null, "Invalid N[]" );
+      Debug.Assert( 0 <= v && v < normals.Count, "Invalid vertex handle" );
+      return normals[ v ];
+    }
+
+    /// <summary>
+    /// Adds a new triangle face defined by its vertices.
+    /// </summary>
+    /// <param name="v1">Handle of the 1st vertex</param>
+    /// <param name="v2">Handle of the 2nd vertex</param>
+    /// <param name="v3">Handle of the 3rd vertex</param>
+    /// <returns>Triangle handle</returns>
+    public int AddTriangle ( int v1, int v2, int v3 )
+    {
+      Debug.Assert( geometry != null, "Invalid G[] size" );
+      Debug.Assert( geometry.Count > v1 &&
+                    geometry.Count > v2 &&
+                    geometry.Count > v3, "Invalid vertex handle" );
+      Debug.Assert( vertexPtr != null && (vertexPtr.Count % 3 == 0),
+                    "Invalid corner-table (V[] size)" );
+
+      int handle1 = vertexPtr.Count;
+      vertexPtr.Add( v1 );
+      vertexPtr.Add( v2 );
+      vertexPtr.Add( v3 );
+
+      if ( oppositePtr != null )
+      {
+        Debug.Assert( oppositePtr.Count == handle1, "Invalid O[] size" );
+        oppositePtr.Add( NULL );
+        oppositePtr.Add( NULL );
+        oppositePtr.Add( NULL );
+      }
+
+      return handle1 / 3;
+    }
+
+    /// <summary>
+    /// Returns vertex handles of the given triangle.
+    /// </summary>
+    /// <param name="tr">Triangle handle</param>
+    /// <param name="v1">Variable to receive the 1st vertex handle</param>
+    /// <param name="v2">Variable to receive the 2nd vertex handle</param>
+    /// <param name="v3">Variable to receive the 3rd vertex handle</param>
+    public void GetTriangleVertices ( int tr, out int v1, out int v2, out int v3 )
+    {
+      Debug.Assert( geometry != null, "Invalid G[] size" );
+      tr *= 3;
+      Debug.Assert( vertexPtr != null && 0 >= tr && tr + 2 < vertexPtr.Count,
+                    "Invalid triangle handle" );
+
+      v1 = vertexPtr[ tr ];
+      v2 = vertexPtr[ tr + 1 ];
+      v3 = vertexPtr[ tr + 2 ];
+    }
+
+    /// <summary>
+    /// Returns vertex coordinates of the given triangle.
+    /// </summary>
+    /// <param name="tr">Triangle handle</param>
+    /// <param name="v1">Variable to receive the 1st vertex coordinates</param>
+    /// <param name="v2">Variable to receive the 2nd vertex coordinates</param>
+    /// <param name="v3">Variable to receive the 3rd vertex coordinates</param>
+    public void GetTriangleVertices ( int tr, out Vector3 v1, out Vector3 v2, out Vector3 v3 )
+    {
+      Debug.Assert( geometry != null, "Invalid G[] size" );
+      tr *= 3;
+      Debug.Assert( vertexPtr != null && 0 >= tr && tr + 2 < vertexPtr.Count,
+                    "Invalid triangle handle" );
+
+      int h1 = vertexPtr[ tr ];
+      int h2 = vertexPtr[ tr + 1 ];
+      int h3 = vertexPtr[ tr + 2 ];
+      v1 = (h1 < 0 || h1 >= geometry.Count) ? Vector3.Zero : geometry[ h1 ];
+      v2 = (h2 < 0 || h2 >= geometry.Count) ? Vector3.Zero : geometry[ h2 ];
+      v3 = (h3 < 0 || h3 >= geometry.Count) ? Vector3.Zero : geometry[ h3 ];
+    }
+
+    #endregion
+
+    #region Corner-table API
+
+    /// <summary>
+    /// [Re]builds the mesh topology (corner-table should be consistent after this call).
+    /// </summary>
+    public void BuildCornerTable ()
+    {
+    }
+
+    /// <summary>
+    /// Returns triangle handle of the given corner
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Triangle handle</returns>
+    public static int cTriangle ( int c )
+    {
+      return c / 3;
+    }
+
+    /// <summary>
+    /// Returns handle of the 1st corner of the given triangle
+    /// </summary>
+    /// <param name="tr">Triangle handle</param>
+    /// <returns>Corner handle</returns>
+    public static int tCorner ( int tr )
+    {
+      return tr * 3;
+    }
+
+    /// <summary>
+    /// Returns the next corner inside the same triangle
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Handle of the next corner</returns>
+    public static int cNext ( int c )
+    {
+      return (c % 3 == 2) ? c - 2 : c + 1;
+    }
+
+    /// <summary>
+    /// Returns the previous corner inside the same triangle
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Handle of the previous corner</returns>
+    public static int cPrev ( int c )
+    {
+      return (c % 3 == 0) ? c + 2 : c - 1;
+    }
+
+    /// <summary>
+    /// Returns vertex handle of the given corner
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Associated vertex's handle</returns>
+    public int cVertex ( int c )
+    {
+      if ( c < 0 ) return NULL;
+
+      Debug.Assert( vertexPtr != null, "Invalid V[] array" );
+      Debug.Assert( c < vertexPtr.Count, "Invalid corner handle" );
+
+      return vertexPtr[ c ];
+    }
+
+    /// <summary>
+    /// Returns opposite corner to the given corner
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Handle of the opposite corner</returns>
+    public int cOpposite ( int c )
+    {
+      if ( c < 0 ) return NULL;
+
+      Debug.Assert( oppositePtr != null, "Invalid O[] array" );
+      Debug.Assert( c < oppositePtr.Count, "Invalid corner handle" );
+
+      return oppositePtr[ c ];
+    }
+
+    /// <summary>
+    /// Returns the "right" corner from the given corner
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Corner handle of the "right" triangle</returns>
+    public int cRight ( int c )
+    {
+      return cOpposite( cNext( c ) );
+    }
+
+    /// <summary>
+    /// Returns the "left" corner from the given corner
+    /// </summary>
+    /// <param name="c">Corner handle</param>
+    /// <returns>Corner handle of the "left" triangle</returns>
+    public int cLeft ( int c )
+    {
+      return cOpposite( cPrev( c ) );
+    }
+
+    #endregion
+  }
+}
