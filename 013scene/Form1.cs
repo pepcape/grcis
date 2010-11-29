e4d3cfc9f3cb34ec86ce1261611317d853bf0411
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2010-11-29 05:15:39 +0000

    013scene: a new project (1st version w/o rendering), New: OpenTK library DLLs, New: sample OBJ file (Utah Teapot).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@30 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 013scene/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using OpenTK;
+using Scene3D;
+
+namespace _013scene
+{
+  public partial class Form1 : Form
+  {
+    protected SceneBrep scene = new SceneBrep();
+
+    protected Bitmap outputImage = null;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonOpen_Click ( object sender, EventArgs e )
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+
+      ofd.Title = "Open Scene File";
+      ofd.Filter = "Wavefront OBJ Files|*.obj" +
+          "|All scene types|*.obj";
+
+      ofd.FilterIndex = 1;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+        return;
+
+      WavefrontObj objReader = new WavefrontObj();
+      StreamReader reader    = new StreamReader( new FileStream( ofd.FileName, FileMode.Open ) );
+      int faces = objReader.ReadBrep( reader, scene );
+
+      labelFaces.Text = String.Format( "{0} faces", faces );
+      redraw();
+    }
+
+    private void redraw ()
+    {
+      if ( scene == null ) return;
+
+      Cursor.Current = Cursors.WaitCursor;
+
+      int width  = panel1.Width;
+      int height = panel1.Height;
+      outputImage = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+
+
+      pictureBox1.Image = outputImage;
+
+      Cursor.Current = Cursors.Default;
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      if ( outputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      redraw();
+    }
+  }
+}
---
+++ 013scene/WavefrontObj.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+using OpenTK;
+
+namespace Scene3D
+{
+  /// <summary>
+  /// Wavefront OBJ file-format.
+  /// </summary>
+  public class WavefrontObj
+  {
+    #region Constants
+
+    /// <summary>
+    /// Comments are introduced by this character.
+    /// </summary>
+    const char COMMENT = '#';
+
+    const string VERTEX = "v";
+
+    const string VERTEX_NORMAL = "vn";
+
+    const string VERTEX_TEXTURE = "vt";
+
+    const string FACE = "f";
+
+    #endregion
+
+    #region Instance data
+
+    public bool MirrorConversion { get; set; }
+
+    #endregion
+
+    #region Construction
+
+    public WavefrontObj ()
+    {
+      MirrorConversion = false;
+    }
+
+    #endregion
+
+    #region File-format API
+
+    public int ReadBrep ( StreamReader reader, SceneBrep scene )
+    {
+      if ( reader == null ) return SceneBrep.NULL;
+
+      Debug.Assert( scene != null );
+      scene.Reset();
+      int faces = 0;
+      List<Vector3> normals = new List<Vector3>( 256 );
+      int[] f = new int[ 3 ];
+
+      do
+      {
+        string line = reader.ReadLine();
+        if ( line == null ) break;
+
+        int commentPos = line.IndexOf( COMMENT );
+        if ( commentPos >= 0 )
+          line = line.Substring( 0, commentPos );
+
+        string[] tokens = line.Split( ' ' );
+
+        switch ( tokens[ 0 ] )
+        {
+          case VERTEX:
+            if ( tokens.Length < 4 ) continue;
+
+            Vector3 coord;
+            if ( !float.TryParse( tokens[ 1 ], NumberStyles.Float, CultureInfo.InvariantCulture, out coord.X ) ||
+                 !float.TryParse( tokens[ 2 ], NumberStyles.Float, CultureInfo.InvariantCulture, out coord.Y ) ||
+                 !float.TryParse( tokens[ 3 ], NumberStyles.Float, CultureInfo.InvariantCulture, out coord.Z ) )
+              continue;
+
+            if ( MirrorConversion )
+              coord.Z = -coord.Z;
+            scene.AddVertex( coord );
+            break;
+
+          case VERTEX_NORMAL:
+            if ( tokens.Length < 4 ) continue;
+
+            Vector3 norm;
+            if ( !float.TryParse( tokens[ 1 ], NumberStyles.Float, CultureInfo.InvariantCulture, out norm.X ) ||
+                 !float.TryParse( tokens[ 2 ], NumberStyles.Float, CultureInfo.InvariantCulture, out norm.Y ) ||
+                 !float.TryParse( tokens[ 3 ], NumberStyles.Float, CultureInfo.InvariantCulture, out norm.Z ) )
+              continue;
+
+            if ( MirrorConversion )
+              norm.Z = -norm.Z;
+            normals.Add( norm );
+            break;
+
+          case FACE:
+            if ( tokens.Length < 4 ) continue;
+            int i;
+
+            for ( i = 0; i < 3; i++ )
+            {
+              string[] vt = tokens[ i + 1 ].Split( '/' );
+              int ti, ni;
+              ti = ni = SceneBrep.NULL;
+              // 0 .. vertex coord index
+              if ( !int.TryParse( vt[ 0 ], out f[ i ] ) ) break;
+              f[ i ]--;
+              if ( vt.Length >= 2 )
+              {
+                // 1 .. texture coord index (not yet)
+                int.TryParse( vt[ 1 ], out ti );
+                ti--;
+                if ( vt.Length >= 3 )
+                {
+                  // 2 .. normal vector index
+                  int.TryParse( vt[ 2 ], out ni );
+                  ni--;
+                }
+              }
+              if ( ni >= 0 && ni < normals.Count )
+                scene.SetNormal( f[ i ], normals[ ni ] );
+            }
+
+            if ( i >= 3 )
+            {
+              scene.AddTriangle( f[ 0 ], f[ 1 ], f[ 2 ] );
+              faces++;
+            }
+
+            break;
+        }
+      }
+      while ( !reader.EndOfStream );
+
+      return faces;
+    }
+
+    #endregion
+
+  }
+}
