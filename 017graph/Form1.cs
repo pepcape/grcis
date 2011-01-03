63ca35595bfcc6d8e5f6b2b2cde63fdce8ce0417
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2011-01-03 14:49:53 +0000

    017graph; 1st version (no graph rendering yet).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@47 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 017graph/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using OpenTK;
+using Scene3D;
+
+namespace _017graph
+{
+  public partial class Form1 : Form
+  {
+    protected Bitmap outputImage = null;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void redraw ()
+    {
+      Cursor.Current = Cursors.WaitCursor;
+
+      int width  = panel1.Width;
+      int height = panel1.Height;
+      outputImage = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+
+      GraphRendering renderer = new GraphRendering();
+      FunctionsR2ToR fun = new FunctionsR2ToR();
+      fun.Variant = (int)numericVariant.Value;
+      renderer.Perspective = checkPerspective.Checked;
+      renderer.Azimuth     = (double)numericAzimuth.Value;
+      renderer.Elevation   = (double)numericElevation.Value;
+      renderer.ViewVolume  = 30.0;
+      renderer.Distance    = 10.0;
+      renderer.Render( outputImage, fun );
+
+      pictureBox1.Image = outputImage;
+
+      Cursor.Current = Cursors.Default;
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      redraw();
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      if ( outputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+  }
+}
---
+++ common/FunctionsR2ToR.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Scene3D
+{
+  /// <summary>
+  /// Functions R^2 -> R.
+  /// </summary>
+  public interface IFunctionR2ToR
+  {
+    double f ( double x, double z );
+
+    int Variant
+    {
+      get;
+      set;
+    }
+
+    double MinX
+    {
+      get;
+      set;
+    }
+
+    double MaxX
+    {
+      get;
+      set;
+    }
+
+    double MinZ
+    {
+      get;
+      set;
+    }
+
+    double MaxZ
+    {
+      get;
+      set;
+    }
+  }
+
+  /// <summary>
+  /// Sample functions R^2 -> R.
+  /// </summary>
+  public class FunctionsR2ToR : IFunctionR2ToR
+  {
+    protected int variant = 0;
+
+    public FunctionsR2ToR ()
+    {
+      variant = 0;
+      MinX = -3.0;
+      MaxX =  3.0;
+      MinZ = -3.0;
+      MaxZ =  3.0;
+    }
+
+    public double f ( double x, double z )
+    {
+      return Math.Cos( x * x + z * z );
+    }
+
+    public int Variant
+    {
+      get;
+      set;
+    }
+
+    public double MinX
+    {
+      get;
+      set;
+    }
+
+    public double MaxX
+    {
+      get;
+      set;
+    }
+
+    public double MinZ
+    {
+      get;
+      set;
+    }
+
+    public double MaxZ
+    {
+      get;
+      set;
+    }
+  }
+}
