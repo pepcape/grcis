a9516ca501cb8bd1ed317bb4d0ebb5268f210f13
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2011-04-25 20:37:54 +0000

    New: 019raytracing project (inherited from 018), no additional functionality yet.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@69 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 019raytracing/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Rendering;
+
+namespace _019raytracing
+{
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Output raster image.
+    /// </summary>
+    protected Bitmap outputImage = null;
+
+    /// <summary>
+    /// Scene to be rendered.
+    /// </summary>
+    protected IRayScene scene = null;
+
+    /// <summary>
+    /// Ray-based renderer in form of image function.
+    /// </summary>
+    protected IImageFunction imf = null;
+
+    /// <summary>
+    /// Image synthesizer used to compute raster images.
+    /// </summary>
+    protected IRenderer rend = null;
+
+    /// <summary>
+    /// Redraws the whole image.
+    /// </summary>
+    private void redraw ()
+    {
+      Cursor.Current = Cursors.WaitCursor;
+
+      int width   = panel1.Width;
+      int height  = panel1.Height;
+      outputImage = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+
+      if ( imf == null )
+        imf = getImageFunction();
+      imf.Width  = width;
+      imf.Height = height;
+
+      if ( rend == null )
+        rend = getRenderer();
+      rend.Width  = width;
+      rend.Height = height;
+
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+
+      rend.RenderRectangle( outputImage, 0, 0, width, height );
+
+      sw.Stop();
+      labelElapsed.Text = String.Format( "Elapsed: {0:f}s", 1.0e-3 * sw.ElapsedMilliseconds );
+
+      pictureBox1.Image = outputImage;
+
+      Cursor.Current = Cursors.Default;
+    }
+
+    /// <summary>
+    /// Shoots single primary ray only..
+    /// </summary>
+    /// <param name="x">X-coordinate inside the raster image.</param>
+    /// <param name="y">Y-coordinate inside the raster image.</param>
+    private void singleSample ( int x, int y )
+    {
+      if ( imf == null )
+        imf = getImageFunction();
+
+      imf.Width  = panel1.Width;
+      imf.Height = panel1.Height;
+      double[] color = new double[ 3 ];
+      long hash = imf.GetSample( x, y, color );
+      labelSample.Text = String.Format( "Sample at [{0},{1}] = [{2:f},{3:f},{4:f}], {5}",
+                                        x, y, color[ 0 ], color[ 1 ], color[ 2 ], hash );
+    }
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      redraw();
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      if ( outputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void pictureBox1_MouseDown ( object sender, MouseEventArgs e )
+    {
+      if ( e.Button == MouseButtons.Left )
+        singleSample( e.X, e.Y );
+    }
+
+    private void pictureBox1_MouseMove ( object sender, MouseEventArgs e )
+    {
+      if ( e.Button == MouseButtons.Left )
+        singleSample( e.X, e.Y );
+    }
+  }
+}
---
+++ common/RayTracingRender.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+
+namespace Rendering
+{
+  /// <summary>
+  /// Ray-tracing rendering (w all secondary rays).
+  /// </summary>
+  public class RayTracing : RayCasting
+  {
+    public RayTracing ( IRayScene sc ) : base( sc )
+    { }
+
+    /// <summary>
+    /// Computes one image sample.
+    /// </summary>
+    /// <param name="x">Horizontal coordinate.</param>
+    /// <param name="y">Vertical coordinate.</param>
+    /// <param name="color">Computed sample color.</param>
+    /// <returns>Hash-value used for adaptive subsampling.</returns>
+    public override long GetSample ( double x, double y, double[] color )
+    {
+      Vector3d p0, p1;
+      if ( !scene.Camera.GetRay( x, y, out p0, out p1 ) )
+      {
+        Array.Clear( color, 0, color.Length );                  // invalid ray -> black color
+        return 1L;
+      }
+
+      LinkedList<Intersection> intersections = scene.Intersectable.Intersect( p0, p1 );
+      if ( intersections == null || intersections.Count == 0 )  // no intersection -> background color
+      {
+        Array.Copy( scene.BackgroundColor, color, Math.Min( scene.BackgroundColor.Length, color.Length ) );
+        return 0L;
+      }
+
+      // there was at least one intersection
+      Intersection i = intersections.First.Value;
+      i.Complete();
+
+      // hash code for adaptive supersampling:
+      long hash = i.Solid.GetHashCode();
+
+      // apply all the textures fist..
+      if ( i.Textures != null )
+        foreach ( ITexture tex in i.Textures )
+          hash = hash * HASH_TEXTURE + tex.Apply( i );
+
+      // terminate if light sources are missing
+      if ( scene.Sources == null || scene.Sources.Count < 1 )
+      {
+        Array.Copy( i.SurfaceColor, color, Math.Min( i.SurfaceColor.Length, color.Length ) );
+        return hash;
+      }
+
+      // .. else apply the reflectance model for each source
+      p1 = -p1;
+      p1.Normalize();
+      double[] colorBak = i.Material.Color;
+      i.Material.Color = i.SurfaceColor;
+      Array.Clear( color, 0, color.Length );
+
+      foreach ( ILightSource source in scene.Sources )
+      {
+        Vector3d dir;
+        double[] intensity = source.GetIntensity( i, out dir );
+        if ( intensity != null )
+        {
+          double[] reflection = i.ReflectanceModel.ColorReflection( i.Material, i, dir, p1 );
+          if ( reflection != null )
+          {
+            int bands = Math.Min( color.Length, Math.Min( reflection.Length, intensity.Length ) );
+            for ( int b = 0; b < bands; b++ )
+              color[ b ] += intensity[ b ] * reflection[ b ];
+            hash = hash * HASH_LIGHT + source.GetHashCode();
+          }
+        }
+      }
+
+      i.Material.Color = colorBak;
+      return hash;
+    }
+  }
+
+}
