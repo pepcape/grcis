3cda9a492d01de4adcba7bf06239a8d4cc71dc25
pepcape <pepcape@gmail.com>
2011-10-10 13:17:04 +0000

    033colormap: 1st version of the project,   refactoring 001colormap a little bit (generate() function separated from the rest of the code).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@80 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 033colormap/Colormap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
+namespace _033colormap
+{
+  class Colormap
+  {
+    public static void generate ( Color baseColor1, Color baseColor2, int numCol, out Color[] colors )
+    {
+      // !!!{{ TODO - generate custom palette based on numCol, baseColor1 [and baseColor2]
+
+      colors = new Color[ numCol ];
+      colors[ 0 ] = baseColor1;
+      float r = baseColor1.R;
+      float g = baseColor1.G;
+      float b = baseColor1.B;
+      float dr = (baseColor2.R - r) / (numCol - 1.0f);
+      float dg = (baseColor2.G - g) / (numCol - 1.0f);
+      float db = (baseColor2.B - b) / (numCol - 1.0f);
+      for ( int i = 1; i < numCol; i++ )
+      {
+        r += dr; g += dg; b += db;
+        colors[ i ] = Color.FromArgb( (int)r, (int)g, (int)b );
+      }
+
+      // !!!}}
+
+    }
+  }
+}
---
+++ 033colormap/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _033colormap
+{
+  public partial class Form1 : Form
+  {
+    public static Color[] colors;
+
+    public static Color baseColor1 = Color.FromArgb( 180,  60,   0 );
+    public static Color baseColor2 = Color.FromArgb( 255, 240, 220 );
+    public static int numCol = 4;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+      baseBox1.color = baseColor1;
+      label1.Text = color2string( baseColor1 );
+      baseBox2.color = baseColor2;
+      label2.Text = color2string( baseColor2 );
+      Colormap.generate( baseColor1, baseColor2, numCol, out colors );
+      pictureBox1.Invalidate();
+    }
+
+    private void button1_Click ( object sender, EventArgs e )
+    {
+      this.Close();
+    }
+
+    private void buttonColor1_Click ( object sender, EventArgs e )
+    {
+      DialogResult res = colorDialog1.ShowDialog();
+      if ( res == DialogResult.Cancel ) return;
+      baseColor1 = colorDialog1.Color;
+      baseBox1.color = baseColor1;
+      label1.Text = color2string( baseColor1 );
+      baseBox1.Invalidate();
+      label1.Invalidate();
+      Colormap.generate( baseColor1, baseColor2, numCol, out colors );
+      pictureBox1.Invalidate();
+    }
+
+    private void buttonColor2_Click ( object sender, EventArgs e )
+    {
+      DialogResult res = colorDialog1.ShowDialog();
+      if ( res == DialogResult.Cancel ) return;
+      baseColor2 = colorDialog1.Color;
+      baseBox2.color = baseColor2;
+      label2.Text = color2string( baseColor2 );
+      baseBox2.Invalidate();
+      label2.Invalidate();
+      Colormap.generate( baseColor1, baseColor2, numCol, out colors );
+      pictureBox1.Invalidate();
+    }
+
+    private void numColors_ValueChanged ( object sender, EventArgs e )
+    {
+      numCol = (int)numColors.Value;
+      if ( numCol > 9 ) numCol = 9;
+      if ( numCol < 3 ) numCol = 3;
+      Colormap.generate( baseColor1, baseColor2, numCol, out colors );
+      pictureBox1.Invalidate();
+    }
+
+    public static string color2string ( Color col )
+    {
+      return "[" + col.R + ',' + col.G + ',' + col.B + ']';
+    }
+
+  }
+}
