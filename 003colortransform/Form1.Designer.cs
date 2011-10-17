075516792d4dc5458d1a209948ababcb6687cb05
pepcape <pepcape@gmail.com>
2011-10-17 13:08:49 +0000

    003colortransform: aktualizace projektu (scrolling obrazku, Recompute tlacitko).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@82 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 003colortransform/Form1.Designer.cs
+++ 003colortransform/Form1.Designer.cs
@@ -28,30 +28,46 @@
     /// </summary>
     private void InitializeComponent ()
     {
-      this.pictureResult = new System.Windows.Forms.PictureBox();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
       this.buttonOpen = new System.Windows.Forms.Button();
       this.buttonSave = new System.Windows.Forms.Button();
       this.numericParam = new System.Windows.Forms.NumericUpDown();
       this.label1 = new System.Windows.Forms.Label();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureResult)).BeginInit();
+      this.buttonRedraw = new System.Windows.Forms.Button();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericParam)).BeginInit();
       this.SuspendLayout();
       // 
-      // pictureResult
+      // panel1
       // 
-      this.pictureResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
-      this.pictureResult.Location = new System.Drawing.Point( 13, 13 );
-      this.pictureResult.Name = "pictureResult";
-      this.pictureResult.Size = new System.Drawing.Size( 714, 366 );
-      this.pictureResult.TabIndex = 0;
-      this.pictureResult.TabStop = false;
+      this.panel1.AutoScroll = true;
+      this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.panel1.Controls.Add( this.pictureBox1 );
+      this.panel1.Location = new System.Drawing.Point( 13, 13 );
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size( 680, 380 );
+      this.panel1.TabIndex = 0;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point( 0, 0 );
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size( 680, 380 );
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
       // 
       // buttonOpen
       // 
-      this.buttonOpen.Location = new System.Drawing.Point( 13, 392 );
+      this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonOpen.Location = new System.Drawing.Point( 13, 408 );
       this.buttonOpen.Name = "buttonOpen";
-      this.buttonOpen.Size = new System.Drawing.Size( 172, 23 );
+      this.buttonOpen.Size = new System.Drawing.Size( 133, 23 );
       this.buttonOpen.TabIndex = 1;
       this.buttonOpen.Text = "Load image";
       this.buttonOpen.UseVisualStyleBackColor = true;
@@ -59,56 +75,72 @@
       // 
       // buttonSave
       // 
-      this.buttonSave.Location = new System.Drawing.Point( 534, 392 );
+      this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.buttonSave.Location = new System.Drawing.Point( 573, 408 );
       this.buttonSave.Name = "buttonSave";
-      this.buttonSave.Size = new System.Drawing.Size( 192, 23 );
-      this.buttonSave.TabIndex = 2;
+      this.buttonSave.Size = new System.Drawing.Size( 120, 23 );
+      this.buttonSave.TabIndex = 3;
       this.buttonSave.Text = "Save image";
       this.buttonSave.UseVisualStyleBackColor = true;
       this.buttonSave.Click += new System.EventHandler( this.buttonSave_Click );
       // 
       // numericParam
       // 
+      this.numericParam.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.numericParam.DecimalPlaces = 2;
       this.numericParam.Increment = new decimal( new int[] {
             1,
             0,
             0,
             65536} );
-      this.numericParam.Location = new System.Drawing.Point( 336, 392 );
+      this.numericParam.Location = new System.Drawing.Point( 239, 410 );
       this.numericParam.Name = "numericParam";
-      this.numericParam.Size = new System.Drawing.Size( 131, 20 );
-      this.numericParam.TabIndex = 3;
+      this.numericParam.Size = new System.Drawing.Size( 102, 20 );
+      this.numericParam.TabIndex = 4;
       this.numericParam.Value = new decimal( new int[] {
             1,
             0,
             0,
             0} );
-      this.numericParam.ValueChanged += new System.EventHandler( this.numericParam_ValueChanged );
       // 
       // label1
       // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point( 263, 394 );
+      this.label1.Location = new System.Drawing.Point( 171, 413 );
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size( 58, 13 );
-      this.label1.TabIndex = 4;
+      this.label1.TabIndex = 5;
       this.label1.Text = "Parameter:";
+      // 
+      // buttonRedraw
+      // 
+      this.buttonRedraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonRedraw.Location = new System.Drawing.Point( 374, 408 );
+      this.buttonRedraw.Name = "buttonRedraw";
+      this.buttonRedraw.Size = new System.Drawing.Size( 105, 23 );
+      this.buttonRedraw.TabIndex = 6;
+      this.buttonRedraw.Text = "Redraw";
+      this.buttonRedraw.UseVisualStyleBackColor = true;
+      this.buttonRedraw.Click += new System.EventHandler( this.buttonRedraw_Click );
       // 
       // FormColorTransform
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.ClientSize = new System.Drawing.Size( 748, 427 );
+      this.ClientSize = new System.Drawing.Size( 710, 443 );
+      this.Controls.Add( this.buttonRedraw );
       this.Controls.Add( this.label1 );
       this.Controls.Add( this.numericParam );
       this.Controls.Add( this.buttonSave );
       this.Controls.Add( this.buttonOpen );
-      this.Controls.Add( this.pictureResult );
+      this.Controls.Add( this.panel1 );
+      this.MinimumSize = new System.Drawing.Size( 650, 200 );
       this.Name = "FormColorTransform";
       this.Text = "003 color transform";
-      ((System.ComponentModel.ISupportInitialize)(this.pictureResult)).EndInit();
+      this.panel1.ResumeLayout( false );
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericParam)).EndInit();
       this.ResumeLayout( false );
       this.PerformLayout();
@@ -117,11 +149,13 @@
 
     #endregion
 
-    public System.Windows.Forms.PictureBox pictureResult;
-    public System.Windows.Forms.Button buttonOpen;
-    public System.Windows.Forms.Button buttonSave;
-    public System.Windows.Forms.NumericUpDown numericParam;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button buttonOpen;
+    private System.Windows.Forms.Button buttonSave;
+    private System.Windows.Forms.NumericUpDown numericParam;
     private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button buttonRedraw;
   }
 }
 
