06cd952d0d0b27988b38b7b4cecb6780da898e68
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2011-10-23 22:03:55 +0000

    035plasma: initial project (cloned from 012animation), full-speed frame-based animation.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@85 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 035plasma/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Scene3D;
+
+namespace _035plasma
+{
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Global thread variable. Valid while the simulation is in progress..
+    /// </summary>
+    protected Thread aThread = null;
+
+    /// <summary>
+    /// Global simulation object. Re-entrant simulation of one frame.
+    /// </summary>
+    protected Simulation sim = null;
+
+    /// <summary>
+    /// Break-variable. Has to be checked on regular basis.
+    /// </summary>
+    volatile protected bool cont = true;
+
+    protected FpsMeter fps = new FpsMeter();
+
+    delegate void SetImageCallback ( Bitmap newImage );
+
+    protected void SetImage ( Bitmap newImage )
+    {
+      if ( pictureBox1.InvokeRequired )
+      {
+        SetImageCallback si = new SetImageCallback( SetImage );
+        BeginInvoke( si, new object[] { newImage } );
+      }
+      else
+        if ( true )
+        {
+          pictureBox1.Image = newImage;
+          pictureBox1.Invalidate();
+        }
+    }
+
+    delegate void SetTextCallback ( string text );
+
+    protected void SetText ( string text )
+    {
+      if ( labelElapsed.InvokeRequired )
+      {
+        SetTextCallback st = new SetTextCallback( SetText );
+        BeginInvoke( st, new object[] { text } );
+      }
+      else
+        labelElapsed.Text = text;
+    }
+
+    delegate void EndSimulationCallback ();
+
+    protected void EndSimulation ()
+    {
+      if ( buttonStart.InvokeRequired )
+      {
+        EndSimulationCallback ea = new EndSimulationCallback( EndSimulation );
+        BeginInvoke( ea );
+      }
+      else
+      {
+        buttonStart.Enabled = true;
+        buttonReset.Enabled = true;
+        buttonStop.Enabled = false;
+        aThread = null;
+      }
+    }
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    public void RenderSimulation ()
+    {
+      int width  = (int)numericXres.Value;
+      int height = (int)numericYres.Value;
+
+      if ( sim == null || sim.Width != width || sim.Height != height )
+        sim = new Simulation( width, height );
+
+      fps.Start();
+      float fp = 0.0f;
+
+      while ( cont )
+      {
+        Bitmap newImage = sim.Simulate();
+        SetImage( newImage );
+
+        float newFp = fps.Frame();
+        if ( sim.Frame % 32 == 0 ) fp = newFp;
+        SetText( String.Format( "Frame: {0} (FPS = {1:0.0})", sim.Frame, fp ) );
+
+        //string fileName = String.Format( "out{0:0000}.png", i );
+        //newImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
+      }
+
+      fps.Stop();
+    }
+
+    private void buttonStart_Click ( object sender, EventArgs e )
+    {
+      if ( aThread != null ) return;
+
+      buttonStart.Enabled = false;
+      buttonReset.Enabled = false;
+      buttonStop.Enabled  = true;
+      cont = true;
+
+      aThread = new Thread( new ThreadStart( this.RenderSimulation ) );
+      aThread.Start();
+    }
+
+    private void buttonStop_Click ( object sender, EventArgs e )
+    {
+      if ( aThread == null ) return;
+
+      cont = false;
+      aThread.Join();
+
+      EndSimulation();
+    }
+
+    private void buttonReset_Click ( object sender, EventArgs e )
+    {
+      if ( sim == null || aThread != null )
+        return;
+
+      sim.Reset();
+      SetText( "Frame: 0" );
+    }
+  }
+}
---
+++ 035plasma/Simulation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Threading;
+
+namespace _035plasma
+{
+  public class Simulation
+  {
+    public Simulation ( int width, int height )
+    {
+      Width  = width;
+      Height = height;
+      Frame  = 0;
+    }
+
+    /// <summary>
+    /// Width of the visualization bitmap in pixels.
+    /// </summary>
+    public int Width
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Height of the visualization bitmap in pixels.
+    /// </summary>
+    public int Height
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Frame number starting from 0.
+    /// </summary>
+    public long Frame
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Simulation reset.
+    /// </summary>
+    public void Reset ()
+    {
+      Frame = 0;
+    }
+
+    /// <summary>
+    /// Simulation of single timestep.
+    /// </summary>
+    /// <returns>Visualization bitmap.</returns>
+    public Bitmap Simulate ()
+    {
+      // !!!{{ TODO: put your simulation code here
+
+      Bitmap result = new Bitmap( Width, Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+      Graphics gr = Graphics.FromImage( result );
+
+      int x0 = Width / 2;
+      int y0 = Height / 2;
+      int maxWid = x0 - 2;
+      int maxHei = y0 - 2;
+
+      Pen p1 = new Pen( Color.FromArgb( 255, 255,  20 ), 1.0f );
+      Pen p2 = new Pen( Color.FromArgb(  60, 120, 255 ), 2.0f );
+
+      double tim = 0.5 + 0.5 * Math.Sin( Frame * 0.01 );
+      int wid = (int)(tim * maxWid);
+      int hei = (int)(tim * maxHei);
+      gr.DrawEllipse( p2, x0 - hei, y0 - wid, hei + hei, wid + wid );
+      gr.DrawEllipse( p1, x0 - wid, y0 - hei, wid + wid, hei + hei );
+
+      Frame++;
+      Thread.Sleep( 2 );
+
+      return result;
+
+      // !!!}}
+    }
+
+  }
+}
