7cbd4846b9404e11dbbc6ad7c9d605e2a478f170
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2011-11-04 18:55:30 +0000

    036circles: 1st version (non-functioning).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@92 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 036circles/Circles.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace _036circles
+{
+  public class Animation
+  {
+    public Animation ()
+    {
+    }
+
+    public Bitmap RenderFrame ( int width, int height, int currentFrame, int totalFrames )
+    {
+      // !!!{{ TODO: put your frame-rendering code here
+
+      Bitmap result = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+      Graphics gr = Graphics.FromImage( result );
+
+      int x0 = width / 2;
+      int y0 = height / 2;
+      int maxWid = x0 - 2;
+      int maxHei = y0 - 2;
+
+      Pen p1 = new Pen( Color.FromArgb( 255, 255,  20 ), 1.0f );
+      Pen p2 = new Pen( Color.FromArgb(  60, 120, 255 ), 2.0f );
+
+      double tim = currentFrame / (totalFrames - 1.0);
+      int wid = (int)(tim * maxWid);
+      int hei = (int)(tim * maxHei);
+      gr.DrawEllipse( p2, x0 - hei, y0 - wid, hei + hei, wid + wid );
+      gr.DrawEllipse( p1, x0 - wid, y0 - hei, wid + wid, hei + hei );
+
+      return result;
+
+      // !!!}}
+    }
+
+  }
+}
