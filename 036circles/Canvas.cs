a975d91f54945381293d18b282c4c24505251083
pepcape <pepcape@gmail.com>
2011-11-04 18:55:30 +0000

    036circles: 1st version (non-functioning).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@92 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 036circles/Canvas.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace _036circles
+{
+  public class Canvas
+  {
+    /// <summary>
+    /// Canvas width in pixels.
+    /// </summary>
+    public int Width
+    {
+      get; set;
+    }
+
+    /// <summary>
+    /// Canvas height in pixels.
+    /// </summary>
+    public int Height
+    {
+      get; set;
+    }
+
+    protected Bitmap bmp;
+
+    protected Graphics gr;
+
+    protected Color currColor = Color.White;
+
+    protected Pen currPen;
+
+    protected SolidBrush currBrush;
+
+    protected bool currAntiAlias = false;
+
+    public Canvas ()
+    {
+      Width = 800;
+      Height = 600;
+      bmp = new Bitmap( Width, Height, PixelFormat.Format24bppRgb );
+      gr = Graphics.FromImage( bmp );
+      currPen = new Pen( Color.White );
+      currBrush = new SolidBrush( Color.White );
+    }
+
+    public void Clear ( Color bg )
+    {
+      if ( bmp.Width  != Width ||
+           bmp.Height != Height )
+      {
+        bmp = new Bitmap( Width, Height, PixelFormat.Format24bppRgb );
+        gr = Graphics.FromImage( bmp );
+        gr.SmoothingMode = currAntiAlias;
+      }
+      gr.Clear( bg );
+    }
+
+    public void SetAntiAlias ( bool aa )
+    {
+      if ( currAntiAlias == aa ) return;
+      currAntiAlias = aa;
+      gr.SmoothingMode = currAntiAlias;
+    }
+
+    public void SetColor ( Color col )
+    {
+      if ( currColor.Equals( col ) ) return;
+      currColor = col;
+      currPen.Color = col;
+      currBrush.Color = col;
+    }
+
+    public void FillDisk ( float x, float y, float r )
+    {
+      gr.FillEllipse( currBrush, x - r, y - r, r + r, r + r );
+    }
+
+    public void DrawCircle ( float x, float y, float r )
+    {
+      gr.DrawEllipse( currPen, x - r, y - r, r + r, r + r );
+    }
+
+    public Bitmap Finish ()
+    {
+      gr.Dispose();
+      Bitmap result = bmp;
+      bmp = new Bitmap( Width, Height, PixelFormat.Format24bppRgb );
+      gr = Graphics.FromImage( bmp );
+      gr.SmoothingMode = currAntiAlias;
+      return result;
+    }
+
+  }
+}
---
+++ 036circles/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace _036circles
+{
+  public partial class Form1 : Form
+  {
+    protected Thread aThread = null;
+
+    volatile protected bool cont = true;
+
+    delegate void SetImageCallback ( Bitmap newImage );
+
+    protected void SetImage ( Bitmap newImage )
+    {
+      if ( pictureBox1.InvokeRequired )
+      {
+        SetImageCallback si = new SetImageCallback( SetImage );
+        BeginInvoke( si, new object[] { newImage } );
+      }
+      else
+        if ( checkShow.Checked )
+        {
+          pictureBox1.Image = newImage;
+          pictureBox1.Invalidate();
+        }
+    }
+
+    delegate void SetTextCallback ( string text );
+
+    protected void SetText ( string text )
+    {
+      if ( labelImageName.InvokeRequired )
+      {
+        SetTextCallback st = new SetTextCallback( SetText );
+        BeginInvoke( st, new object[] { text } );
+      }
+      else
+        labelImageName.Text = text;
+    }
+
+    delegate void StopAnimationCallback ();
+
+    protected void StopAnimation ()
+    {
+      if ( aThread == null ) return;
+
+      if ( buttonRedraw.InvokeRequired )
+      {
+        StopAnimationCallback ea = new StopAnimationCallback( StopAnimation );
+        BeginInvoke( ea );
+      }
+      else
+      {
+        // actually stop the animation:
+        cont = false;
+        aThread.Join();
+        aThread = null;
+
+        // GUI stuff:
+        buttonRedraw.Enabled = true;
+        buttonLoadImage.Enabled = false;
+      }
+    }
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    public void RenderAnimation ()
+    {
+      int maxFrame = (int)numericParam.Value;
+      int width    = (int)numericXres.Value;
+      int height   = (int)numericYres.Value;
+
+      Animation anim = new Animation();
+
+      for ( int i = 0; i < maxFrame; i++ )
+      {
+        if ( !cont ) return;
+
+        Bitmap newImage = anim.RenderFrame( width, height, i, maxFrame );
+        SetImage( (Bitmap)newImage.Clone() );
+        SetText( String.Format( "Frame: {0}", i ) );
+        string fileName = String.Format( "out{0:0000}.png", i );
+        newImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
+      }
+
+      StopAnimation();
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      if ( aThread != null ) return;
+
+      buttonRedraw.Enabled = false;
+      buttonLoadImage.Enabled  = true;
+      cont = true;
+
+      aThread = new Thread( new ThreadStart( this.RenderAnimation ) );
+      aThread.Start();
+    }
+
+    private void buttonLoadImage_Click ( object sender, EventArgs e )
+    {
+      StopAnimation();
+    }
+  }
+}
