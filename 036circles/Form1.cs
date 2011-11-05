8e79b43f4ddce65bba4174f172c255b094752c6d
pepcape <pepcape@gmail.com>
2011-11-05 13:09:01 +0000

    036circles: finished, needs approval.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@93 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 036circles/Form1.cs
+++ 036circles/Form1.cs
@@ -1,119 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Diagnostics;
-using System.IO;
-using System.Threading;
 
 namespace _036circles
 {
   public partial class Form1 : Form
   {
-    protected Thread aThread = null;
+    protected Image inputImage = null;
 
-    volatile protected bool cont = true;
-
-    delegate void SetImageCallback ( Bitmap newImage );
-
-    protected void SetImage ( Bitmap newImage )
-    {
-      if ( pictureBox1.InvokeRequired )
-      {
-        SetImageCallback si = new SetImageCallback( SetImage );
-        BeginInvoke( si, new object[] { newImage } );
-      }
-      else
-        if ( checkShow.Checked )
-        {
-          pictureBox1.Image = newImage;
-          pictureBox1.Invalidate();
-        }
-    }
-
-    delegate void SetTextCallback ( string text );
-
-    protected void SetText ( string text )
-    {
-      if ( labelImageName.InvokeRequired )
-      {
-        SetTextCallback st = new SetTextCallback( SetText );
-        BeginInvoke( st, new object[] { text } );
-      }
-      else
-        labelImageName.Text = text;
-    }
-
-    delegate void StopAnimationCallback ();
-
-    protected void StopAnimation ()
-    {
-      if ( aThread == null ) return;
-
-      if ( buttonRedraw.InvokeRequired )
-      {
-        StopAnimationCallback ea = new StopAnimationCallback( StopAnimation );
-        BeginInvoke( ea );
-      }
-      else
-      {
-        // actually stop the animation:
-        cont = false;
-        aThread.Join();
-        aThread = null;
-
-        // GUI stuff:
-        buttonRedraw.Enabled = true;
-        buttonLoadImage.Enabled = false;
-      }
-    }
+    protected Image outputImage = null;
 
     public Form1 ()
     {
       InitializeComponent();
     }
 
-    public void RenderAnimation ()
+    private void buttonLoadImage_Click ( object sender, EventArgs e )
     {
-      int maxFrame = (int)numericParam.Value;
-      int width    = (int)numericXres.Value;
-      int height   = (int)numericYres.Value;
+      OpenFileDialog ofd = new OpenFileDialog();
 
-      Animation anim = new Animation();
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
 
-      for ( int i = 0; i < maxFrame; i++ )
-      {
-        if ( !cont ) return;
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+        return;
 
-        Bitmap newImage = anim.RenderFrame( width, height, i, maxFrame );
-        SetImage( (Bitmap)newImage.Clone() );
-        SetText( String.Format( "Frame: {0}", i ) );
-        string fileName = String.Format( "out{0:0000}.png", i );
-        newImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
-      }
-
-      StopAnimation();
+      inputImage = Image.FromFile( ofd.FileName );
+      labelImageName.Text = "Input: " + ofd.FileName;
     }
 
     private void buttonRedraw_Click ( object sender, EventArgs e )
     {
-      if ( aThread != null ) return;
+      double param = (double)numericParam.Value;
+      int width    = (int)numericXres.Value;
+      int height   = (int)numericYres.Value;
 
-      buttonRedraw.Enabled = false;
-      buttonLoadImage.Enabled  = true;
-      cont = true;
+      Canvas c = new Canvas( width, height );
 
-      aThread = new Thread( new ThreadStart( this.RenderAnimation ) );
-      aThread.Start();
+      Circles.Draw( c, (Bitmap)inputImage, param );
+
+      pictureBox1.Image = outputImage = c.Finish();
+      buttonSave.Enabled = true;
     }
 
-    private void buttonLoadImage_Click ( object sender, EventArgs e )
+    private void buttonSave_Click ( object sender, EventArgs e )
     {
-      StopAnimation();
+      if ( outputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
     }
   }
 }
