1288e754ec08c83158e81dd7b7d1a5c4a838db50
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2012-04-05 10:00:18 +0000

    New: 046cameranim project, New: ITimeDependent interface.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@128 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 046cameranim/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Rendering;
+using System.Threading;
+
+namespace _046cameranim
+{
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Output raster image.
+    /// </summary>
+    protected Bitmap outputImage = null;
+
+    /// <summary>
+    /// Scene to be rendered.
+    /// </summary>
+    protected IRayScene scene = null;
+
+    /// <summary>
+    /// Ray-based renderer in form of image function.
+    /// </summary>
+    protected IImageFunction imf = null;
+
+    /// <summary>
+    /// Image synthesizer used to compute raster images.
+    /// </summary>
+    protected IRenderer rend = null;
+
+    /// <summary>
+    /// Redraws the whole image.
+    /// </summary>
+    private void redraw ()
+    {
+      Cursor.Current = Cursors.WaitCursor;
+
+      int width   = panel1.Width;
+      int height  = panel1.Height;
+      outputImage = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+
+      if ( imf == null )
+        imf = getImageFunction();
+      imf.Width  = width;
+      imf.Height = height;
+
+      if ( rend == null )
+        rend = getRenderer();
+      rend.Width  = width;
+      rend.Height = height;
+
+      // animation:
+      ((ITimeDependent)scene).Time = (double)numTime.Value;
+
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+
+      rend.RenderRectangle( outputImage, 0, 0, width, height );
+
+      sw.Stop();
+      labelElapsed.Text = String.Format( "Elapsed: {0:f}s", 1.0e-3 * sw.ElapsedMilliseconds );
+
+      pictureBox1.Image = outputImage;
+
+      Cursor.Current = Cursors.Default;
+    }
+
+    /// <summary>
+    /// Rendering thread.
+    /// </summary>
+    protected Thread aThread = null;
+
+    volatile protected bool cont = true;
+
+    delegate void SetImageCallback ( Bitmap newImage );
+
+    protected void SetImage ( Bitmap newImage )
+    {
+      if ( pictureBox1.InvokeRequired )
+      {
+        SetImageCallback si = new SetImageCallback( SetImage );
+        BeginInvoke( si, new object[] { newImage } );
+      }
+      else
+      {
+        pictureBox1.Image = newImage;
+        pictureBox1.Invalidate();
+      }
+    }
+
+    delegate void SetTextCallback ( string text );
+
+    protected void SetText ( string text )
+    {
+      if ( labelElapsed.InvokeRequired )
+      {
+        SetTextCallback st = new SetTextCallback( SetText );
+        BeginInvoke( st, new object[] { text } );
+      }
+      else
+        labelElapsed.Text = text;
+    }
+
+    delegate void StopAnimationCallback ();
+
+    protected void StopAnimation ()
+    {
+      if ( aThread == null ) return;
+
+      if ( buttonRenderAnim.InvokeRequired )
+      {
+        StopAnimationCallback ea = new StopAnimationCallback( StopAnimation );
+        BeginInvoke( ea );
+      }
+      else
+      {
+        // actually stop the animation:
+        cont = false;
+        aThread.Join();
+        aThread = null;
+
+        // GUI stuff:
+        buttonRenderAnim.Enabled = true;
+        buttonStop.Enabled = false;
+      }
+    }
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      redraw();
+    }
+
+    private void buttonRenderAnim_Click ( object sender, EventArgs e )
+    {
+      if ( aThread != null ) return;
+
+      buttonRenderAnim.Enabled = false;
+      buttonStop.Enabled = true;
+      cont = true;
+
+      aThread = new Thread( new ThreadStart( this.RenderAnimation ) );
+      aThread.Start();
+    }
+
+    private void buttonStop_Click ( object sender, EventArgs e )
+    {
+      StopAnimation();
+    }
+
+    private Bitmap RenderFrame ( int width, int height, double time )
+    {
+      Bitmap result = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+
+      if ( imf == null )
+        imf = getImageFunction();
+      imf.Width = width;
+      imf.Height = height;
+
+      if ( rend == null )
+        rend = getRenderer();
+      rend.Width = width;
+      rend.Height = height;
+
+      // animation:
+      ((ITimeDependent)scene).Time = time;
+
+      rend.RenderRectangle( result, 0, 0, width, height );
+
+      return result;
+    }
+
+    public void RenderAnimation ()
+    {
+      double time = (double)numFrom.Value;
+      double end  = (double)numTo.Value;
+      if ( end <= time ) end = time + 1.0;
+      double fps = (double)numFps.Value;
+      double dt = (fps > 0.0) ? 1.0 / fps : 25.0;
+      int width = panel1.Width;
+      int height = panel1.Height;
+
+      Cursor.Current = Cursors.WaitCursor;
+
+      Stopwatch sw = new Stopwatch();
+
+      for ( int fr = 0; time < end; fr++, time += dt )
+      {
+        if ( !cont ) return;
+
+        sw.Start();
+        Bitmap newImage = RenderFrame( width, height, time );
+        SetImage( (Bitmap)newImage.Clone() );
+        sw.Stop();
+
+        SetText( String.Format( "Frame: {0}, elapsed: {1:f}s", fr, 1.0e-3 * sw.ElapsedMilliseconds ) );
+        string fileName = String.Format( "out{0:0000}.png", fr );
+        newImage.Save( fileName, System.Drawing.Imaging.ImageFormat.Png );
+      }
+
+      Cursor.Current = Cursors.Default;
+
+      StopAnimation();
+    }
+
+    private void Form1_FormClosing ( object sender, FormClosingEventArgs e )
+    {
+      StopAnimation();
+    }
+  }
+}
---
+++ 046cameranim/RayTracingAnim.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using OpenTK;
+using Rendering;
+
+namespace _046cameranim
+{
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Initialize ray-scene and image function (good enough for single samples).
+    /// </summary>
+    private IImageFunction getImageFunction ()
+    {
+      // default constructor of the RayScene .. custom scene
+      scene = new RayScene();
+      return new RayTracing( scene );
+    }
+
+    /// <summary>
+    /// Initialize image synthesizer (responsible for raster image computation).
+    /// The 'imf' member has been already initialized..
+    /// </summary>
+    private IRenderer getRenderer ()
+    {
+      SimpleImageSynthesizer sis = new SimpleImageSynthesizer();
+      sis.ImageFunction = imf;
+      return sis;
+    }
+  }
+}
+
+namespace Rendering
+{
+  /// <summary>
+  /// Test scene for ray-based rendering.
+  /// </summary>
+  public class RayScene : IRayScene, ITimeDependent
+  {
+    public IIntersectable Intersectable
+    {
+      get;
+      set;
+    }
+
+    public double[] BackgroundColor
+    {
+      get;
+      set;
+    }
+
+    public ICamera Camera
+    {
+      get;
+      set;
+    }
+
+    public ICollection<ILightSource> Sources
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Starting (minimal) time in seconds.
+    /// </summary>
+    public double Start
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Ending (maximal) time in seconds.
+    /// </summary>
+    public double End
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Current time in seconds.
+    /// </summary>
+    public double Time
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Creates default ray-rendering scene.
+    /// </summary>
+    public RayScene ()
+    {
+      // scene:
+      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
+      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.5, 0.5, 0.5 }, 0.2, 0.5, 0.4, 12 ) );
+      Intersectable = root;
+      // sphere 1:
+      Sphere s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 1.0, 0.6, 0.0 } );
+      root.InsertChild( s, Matrix4d.Identity );
+      // sphere 2:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 0.2, 0.9, 0.5 } );
+      root.InsertChild( s, Matrix4d.CreateTranslation( -2.2, 0.0, 0.0 ) );
+      // sphere 3:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.3, 1.0 } );
+      root.InsertChild( s, Matrix4d.CreateTranslation( -4.4, 0.0, 0.0 ) );
+      // sphere 4:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 1.0, 0.2, 0.2 } );
+      root.InsertChild( s, Matrix4d.CreateTranslation( 2.2, 0.0, 0.0 ) );
+      // sphere 5:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.4, 0.0 } );
+      s.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 80.0, 40.0, new double[] { 1.0, 0.8, 0.2 } ) );
+      root.InsertChild( s, Matrix4d.CreateTranslation( 4.4, 0.0, 0.0 ) );
+
+      // background color:
+      BackgroundColor = new double[] { 0.0, 0.15, 0.2 };
+
+      // camera:
+      Camera = new StaticCamera( new Vector3d( 0.0, 0.0, -10.0 ),
+                                 new Vector3d( 0.0, 0.0, 1.0 ), 60.0 );
+
+      // light sources:
+      Sources = new LinkedList<ILightSource>();
+      Sources.Add( new AmbientLightSource( 0.8 ) );
+      Sources.Add( new PointLightSource( new Vector3d( -5.0, 3.0, -3.0 ), 1.0 ) );
+
+      // animation support:
+      Start = 0.0;
+      End   = 10.0;
+      Time  = 0.0;
+    }
+  }
+
+  /// <summary>
+  /// Correctly implemented set operations..
+  /// </summary>
+  public class CSGInnerNode : InnerNode
+  {
+    public CSGInnerNode ( SetOperation op )
+      : base( op )
+    { }
+
+    /// <summary>
+    /// Computes the complete intersection of the given ray with the object.
+    /// </summary>
+    /// <param name="p0">Ray origin.</param>
+    /// <param name="p1">Ray direction vector.</param>
+    /// <returns>Sorted list of intersection records.</returns>
+    public override LinkedList<Intersection> Intersect ( Vector3d p0, Vector3d p1 )
+    {
+      // !!!{{ TODO: put correct set operation implementation here
+
+      LinkedList<Intersection> result = new LinkedList<Intersection>();
+      foreach ( ISceneNode n in children )
+      {
+        Vector3d origin = Vector3d.TransformPosition( p0, n.FromParent );
+        Vector3d dir = Vector3d.TransformVector( p1, n.FromParent );
+        // ray in local child's coords: [ origin, dir ]
+
+        LinkedList<Intersection> partial = n.Intersect( origin, dir );
+        if ( partial == null || partial.Count == 0 ) continue;
+
+        Intersection i = Intersection.FirstIntersection( partial, ref p1 );
+
+        if ( i != null )
+          if ( result.First == null )
+            result.AddFirst( i );
+          else
+            if ( i.T < result.First.Value.T )
+              result.AddFirst( i );
+      }
+      return result;
+
+      // !!!}}
+    }
+  }
+}
