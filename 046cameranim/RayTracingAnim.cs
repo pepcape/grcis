2ddc5f4cde5140bc0dc33e477d7b2c1b0721f91a
pepcape <pepcape@gmail.com>
2012-04-06 23:18:44 +0000

    New: little more interesting RT scenes will be found in RayCastingScenes, New: DefaultRayScene (attributes), New: correct implementation of set operators in CSGInnerNode (not thoroughly tested yet).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@129 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 046cameranim/RayTracingAnim.cs
+++ 046cameranim/RayTracingAnim.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Forms;
-using OpenTK;
 using Rendering;
 
 namespace _046cameranim
@@ -35,32 +34,8 @@ namespace Rendering
   /// <summary>
   /// Test scene for ray-based rendering.
   /// </summary>
-  public class RayScene : IRayScene, ITimeDependent
+  public class RayScene : DefaultRayScene, ITimeDependent
   {
-    public IIntersectable Intersectable
-    {
-      get;
-      set;
-    }
-
-    public double[] BackgroundColor
-    {
-      get;
-      set;
-    }
-
-    public ICamera Camera
-    {
-      get;
-      set;
-    }
-
-    public ICollection<ILightSource> Sources
-    {
-      get;
-      set;
-    }
-
     /// <summary>
     /// Starting (minimal) time in seconds.
     /// </summary>
@@ -93,93 +68,19 @@ namespace Rendering
     /// </summary>
     public RayScene ()
     {
-      // scene:
-      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
-      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
-      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.5, 0.5, 0.5 }, 0.2, 0.5, 0.4, 12 ) );
-      Intersectable = root;
-      // sphere 1:
-      Sphere s = new Sphere();
-      s.SetAttribute( PropertyName.COLOR, new double[] { 1.0, 0.6, 0.0 } );
-      root.InsertChild( s, Matrix4d.Identity );
-      // sphere 2:
-      s = new Sphere();
-      s.SetAttribute( PropertyName.COLOR, new double[] { 0.2, 0.9, 0.5 } );
-      root.InsertChild( s, Matrix4d.CreateTranslation( -2.2, 0.0, 0.0 ) );
-      // sphere 3:
-      s = new Sphere();
-      s.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.3, 1.0 } );
-      root.InsertChild( s, Matrix4d.CreateTranslation( -4.4, 0.0, 0.0 ) );
-      // sphere 4:
-      s = new Sphere();
-      s.SetAttribute( PropertyName.COLOR, new double[] { 1.0, 0.2, 0.2 } );
-      root.InsertChild( s, Matrix4d.CreateTranslation( 2.2, 0.0, 0.0 ) );
-      // sphere 5:
-      s = new Sphere();
-      s.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.4, 0.0 } );
-      s.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 80.0, 40.0, new double[] { 1.0, 0.8, 0.2 } ) );
-      root.InsertChild( s, Matrix4d.CreateTranslation( 4.4, 0.0, 0.0 ) );
+      //Scenes.FiveBalls( this );
+      Scenes.HedgehogInTheCage( this );
 
-      // background color:
-      BackgroundColor = new double[] { 0.0, 0.15, 0.2 };
+      // !!!{{ TODO: put your camera setup code here
 
-      // camera:
-      Camera = new StaticCamera( new Vector3d( 0.0, 0.0, -10.0 ),
-                                 new Vector3d( 0.0, 0.0, 1.0 ), 60.0 );
+      //Camera = new MyAnimationCamera( ... );
 
-      // light sources:
-      Sources = new LinkedList<ILightSource>();
-      Sources.Add( new AmbientLightSource( 0.8 ) );
-      Sources.Add( new PointLightSource( new Vector3d( -5.0, 3.0, -3.0 ), 1.0 ) );
+      // !!!}}
 
       // animation support:
       Start = 0.0;
       End   = 10.0;
       Time  = 0.0;
-    }
-  }
-
-  /// <summary>
-  /// Correctly implemented set operations..
-  /// </summary>
-  public class CSGInnerNode : InnerNode
-  {
-    public CSGInnerNode ( SetOperation op )
-      : base( op )
-    { }
-
-    /// <summary>
-    /// Computes the complete intersection of the given ray with the object.
-    /// </summary>
-    /// <param name="p0">Ray origin.</param>
-    /// <param name="p1">Ray direction vector.</param>
-    /// <returns>Sorted list of intersection records.</returns>
-    public override LinkedList<Intersection> Intersect ( Vector3d p0, Vector3d p1 )
-    {
-      // !!!{{ TODO: put correct set operation implementation here
-
-      LinkedList<Intersection> result = new LinkedList<Intersection>();
-      foreach ( ISceneNode n in children )
-      {
-        Vector3d origin = Vector3d.TransformPosition( p0, n.FromParent );
-        Vector3d dir = Vector3d.TransformVector( p1, n.FromParent );
-        // ray in local child's coords: [ origin, dir ]
-
-        LinkedList<Intersection> partial = n.Intersect( origin, dir );
-        if ( partial == null || partial.Count == 0 ) continue;
-
-        Intersection i = Intersection.FirstIntersection( partial, ref p1 );
-
-        if ( i != null )
-          if ( result.First == null )
-            result.AddFirst( i );
-          else
-            if ( i.T < result.First.Value.T )
-              result.AddFirst( i );
-      }
-      return result;
-
-      // !!!}}
     }
   }
 }
---
+++ common/RayCastingScenes.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using OpenTK;
+
+namespace Rendering
+{
+  /// <summary>
+  /// Some interesting scenes created mostly by MFF UK students.
+  /// http://cgg.mff.cuni.cz/~pepca/lectures/npgr004.current.html
+  /// </summary>
+  public class Scenes
+  {
+    /// <summary>
+    /// Simple scene containing five colored spheres.
+    /// </summary>
+    public static void FiveBalls ( IRayScene sc )
+    {
+      Debug.Assert( sc != null );
+
+      // CSG scene:
+      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
+      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.5, 0.5, 0.5 }, 0.2, 0.5, 0.4, 12 ) );
+      sc.Intersectable = root;
+
+      // Background color:
+      sc.BackgroundColor = new double[] { 0.0, 0.15, 0.2 };
+
+      // Camera:
+      sc.Camera = new StaticCamera( new Vector3d( 0.0, 0.0, -10.0 ),
+                                    new Vector3d( 0.0, 0.0, 1.0 ),
+                                    60.0 );
+
+      // Light sources:
+      sc.Sources = new LinkedList<ILightSource>();
+      sc.Sources.Add( new AmbientLightSource( 0.8 ) );
+      sc.Sources.Add( new PointLightSource( new Vector3d( -5.0, 3.0, -3.0 ), 1.0 ) );
+
+      // --- NODE DEFINITIONS ----------------------------------------------------
+
+      // sphere 1:
+      Sphere s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 1.0, 0.6, 0.0 } );
+      root.InsertChild( s, Matrix4d.Identity );
+
+      // sphere 2:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 0.2, 0.9, 0.5 } );
+      root.InsertChild( s, Matrix4d.CreateTranslation( -2.2, 0.0, 0.0 ) );
+
+      // sphere 3:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.3, 1.0 } );
+      root.InsertChild( s, Matrix4d.CreateTranslation( -4.4, 0.0, 0.0 ) );
+
+      // sphere 4:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 1.0, 0.2, 0.2 } );
+      root.InsertChild( s, Matrix4d.CreateTranslation( 2.2, 0.0, 0.0 ) );
+
+      // sphere 5:
+      s = new Sphere();
+      s.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.4, 0.0 } );
+      s.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 80.0, 40.0, new double[] { 1.0, 0.8, 0.2 } ) );
+      root.InsertChild( s, Matrix4d.CreateTranslation( 4.4, 0.0, 0.0 ) );
+    }
+
+    /// <summary>
+    /// Hedgehog in the Cage - by Michal Wirth, (c) 2012
+    /// </summary>
+    public static void HedgehogInTheCage ( IRayScene sc )
+    {
+      Debug.Assert( sc != null );
+
+      // CSG scene:
+      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
+      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.5, 0.5, 0.5 }, 0.4, 0.4, 0.3, 12 ) );
+      sc.Intersectable = root;
+
+      // Background color:
+      sc.BackgroundColor = new double[] { 0.0, 0.15, 0.2 };
+
+      // Camera:
+      sc.Camera = new StaticCamera( new Vector3d( 0.0, 2.0, -7.0 ),
+                                    new Vector3d( 0.0, -0.32, 1.0 ),
+                                    40.0 );
+
+      // Light sources:
+      sc.Sources = new LinkedList<ILightSource>();
+      sc.Sources.Add( new AmbientLightSource( 0.8 ) );
+      sc.Sources.Add( new PointLightSource( new Vector3d( -8.0, 5.0, -3.0 ), 1.0 ) );
+
+      // --- NODE DEFINITIONS ----------------------------------------------------
+
+      // cage
+      CSGInnerNode cage = new CSGInnerNode( SetOperation.Difference );
+      cage.SetAttribute( PropertyName.COLOR, new double[] { 0.70, 0.93, 0.20 } );
+
+      // cylinder1
+      CSGInnerNode cylinder1 = new CSGInnerNode( SetOperation.Intersection );
+      Sphere s = new Sphere();
+      cylinder1.InsertChild( s, Matrix4d.Scale( 1.0, 1000.0, 1.0 ) );
+      s = new Sphere();
+      cylinder1.InsertChild( s, Matrix4d.Scale( 1000.0, 1.5, 1000.0 ) );
+      cage.InsertChild( cylinder1, Matrix4d.Identity );
+
+      // cylinder2
+      CSGInnerNode cylinder2 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      cylinder2.InsertChild( s, Matrix4d.Scale( 1.0, 1000.0, 1.0 ) );
+      s = new Sphere();
+      cylinder2.InsertChild( s, Matrix4d.Scale( 1000.0, 1.5, 1000.0 ) );
+      cage.InsertChild( cylinder2, Matrix4d.Scale( 0.9 ) );
+
+      // holeUpDown
+      Sphere holeUpDown = new Sphere();
+      cage.InsertChild( holeUpDown, Matrix4d.Scale( 0.5, 1000.0, 0.5 ) );
+
+      // hole1
+      CSGInnerNode hole1 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      hole1.InsertChild( s, Matrix4d.Scale( 1000.0, 1.1, 1000.0 ) );
+      s = new Sphere();
+      hole1.InsertChild( s, Matrix4d.Scale( 0.4, 1000.0, 1000.0 ) );
+      cage.InsertChild( hole1, Matrix4d.Identity );
+
+      // hole2
+      CSGInnerNode hole2 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      hole2.InsertChild( s, Matrix4d.Scale( 1000.0, 1.1, 1000.0 ) );
+      s = new Sphere();
+      hole2.InsertChild( s, Matrix4d.Scale( 0.4, 1000.0, 1000.0 ) );
+      cage.InsertChild( hole2, Matrix4d.RotateY( Math.PI / 3 ) );
+
+      // hole3
+      CSGInnerNode hole3 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      hole3.InsertChild( s, Matrix4d.Scale( 1000.0, 1.1, 1000.0 ) );
+      s = new Sphere();
+      hole3.InsertChild( s, Matrix4d.Scale( 0.4, 1000.0, 1000.0 ) );
+      cage.InsertChild( hole3, Matrix4d.RotateY( Math.PI / -3 ) );
+
+      // hedgehog
+      CSGInnerNode hedgehog = new CSGInnerNode( SetOperation.Union );
+      hedgehog.SetAttribute( PropertyName.COLOR, new double[] { 0.62, 0.1, 0.1 } );
+
+      s = new Sphere();
+      hedgehog.InsertChild( s, Matrix4d.Scale( 0.48 ) );
+
+      // spine1
+      CSGInnerNode spine1 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      spine1.InsertChild( s, Matrix4d.Scale( 0.06, 1000.0, 0.06 ) );
+      s = new Sphere();
+      spine1.InsertChild( s, Matrix4d.Scale( 1.2 ) );
+      hedgehog.InsertChild( spine1, Matrix4d.Identity );
+
+      // spine2
+      CSGInnerNode spine2 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      spine2.InsertChild( s, Matrix4d.Scale( 0.06, 1000.0, 0.06 ) );
+      s = new Sphere();
+      spine2.InsertChild( s, Matrix4d.Scale( 1.2 ) * Matrix4d.CreateTranslation( 0.0, 0.1, 0.0 ) );
+      hedgehog.InsertChild( spine2, Matrix4d.RotateX( Math.PI / -3 ) );
+
+      // spine3
+      CSGInnerNode spine3 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      spine3.InsertChild( s, Matrix4d.Scale( 0.06, 1000.0, 0.06 ) );
+      s = new Sphere();
+      spine3.InsertChild( s, Matrix4d.Scale( 1.2 ) * Matrix4d.CreateTranslation( 0.0, -0.25, 0.0 ) );
+      hedgehog.InsertChild( spine3, Matrix4d.RotateX( Math.PI / -3 ) * Matrix4d.RotateY( Math.PI * -0.4 ) );
+
+      // spine4
+      CSGInnerNode spine4 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      spine4.InsertChild( s, Matrix4d.Scale( 0.06, 1000.0, 0.06 ) );
+      s = new Sphere();
+      spine4.InsertChild( s, Matrix4d.Scale( 1.2 ) * Matrix4d.CreateTranslation( 0.0, 0.2, 0.0 ) );
+      hedgehog.InsertChild( spine4, Matrix4d.RotateX( Math.PI / -3 ) * Matrix4d.RotateY( Math.PI * -0.8 ) );
+
+      // spine5
+      CSGInnerNode spine5 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      spine5.InsertChild( s, Matrix4d.Scale( 0.06, 1000.0, 0.06 ) );
+      s = new Sphere();
+      spine5.InsertChild( s, Matrix4d.Scale( 1.2 ) * Matrix4d.CreateTranslation( 0.0, -0.2, 0.0 ) );
+      hedgehog.InsertChild( spine5, Matrix4d.RotateX( Math.PI / -3 ) * Matrix4d.RotateY( Math.PI * 0.4 ) );
+
+      // spine6
+      CSGInnerNode spine6 = new CSGInnerNode( SetOperation.Intersection );
+      s = new Sphere();
+      spine6.InsertChild( s, Matrix4d.Scale( 0.06, 1000.0, 0.06 ) );
+      s = new Sphere();
+      spine6.InsertChild( s, Matrix4d.Scale( 1.2 ) * Matrix4d.CreateTranslation( 0.0, -0.25, 0.0 ) );
+      hedgehog.InsertChild( spine6, Matrix4d.RotateX( Math.PI / -3 ) * Matrix4d.RotateY( Math.PI * 0.8 ) );
+
+      // all
+      CSGInnerNode all = new CSGInnerNode( SetOperation.Union );
+      all.InsertChild( cage, Matrix4d.RotateY( 0.25 ) );
+      all.InsertChild( hedgehog, Matrix4d.Rotate( new Vector3d( 0.0, 1.0, 1.0 ), 0.1 ) * Matrix4d.CreateTranslation( 0.0, -0.1, 0.0 ) );
+
+      root.InsertChild( all, Matrix4d.RotateZ( 0.1 ) );
+    }
+  }
+}
