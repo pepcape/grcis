086bbec273430bbfed71712ea09925e026d3aad2
pepcape <pepcape@gmail.com>
2012-04-07 01:59:20 +0000

    New: Cube solid, New: Flags scene for RT.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@130 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- common/RayCastingBasic.cs
+++ common/RayCastingBasic.cs
@@ -465,6 +465,67 @@ namespace Rendering
   }
 
   /// <summary>
+  /// Simple texture able to modulate surface color.
+  /// </summary>
+  public class CheckerTexture : ITexture
+  {
+    /// <summary>
+    /// Alternative color.
+    /// </summary>
+    public double[] Color2;
+
+    /// <summary>
+    /// Frequency in the u-direction.
+    /// </summary>
+    public double Fu;
+
+    /// <summary>
+    /// Frequency in the v-direction.
+    /// </summary>
+    public double Fv;
+
+    public CheckerTexture ( double fu, double fv, double[] color )
+    {
+      Fu = fu;
+      Fv = fv;
+      Color2 = (double[])color.Clone();
+    }
+
+    /// <summary>
+    /// Apply the relevant value-modulation in the given Intersection instance.
+    /// Simple variant, w/o an integration support.
+    /// </summary>
+    /// <param name="inter">Data object to modify.</param>
+    /// <returns>Hash value (texture signature) for adaptive subsampling.</returns>
+    public long Apply ( Intersection inter )
+    {
+      double u = inter.TextureCoord.X * Fu;
+      double v = inter.TextureCoord.Y * Fv;
+
+      long ui = (long)Math.Floor( u );
+      long vi = (long)Math.Floor( v );
+
+      if ( ((ui + vi) & 1) != 0 )
+        Array.Copy( Color2, inter.SurfaceColor, Math.Min( Color2.Length, inter.SurfaceColor.Length ) );
+
+      return (ui + RandomStatic.numericRecipes( vi ));
+    }
+
+    /// <summary>
+    /// Apply the relevant value-modulation in the given Intersection instance.
+    /// Internal integration support.
+    /// </summary>
+    /// <param name="inter">Data object to modify.</param>
+    /// <param name="rank">Rank of this sample, 0 <= rank < total (for integration).</param>
+    /// <param name="total">Total number of samples (for integration).</param>
+    /// <returns>Hash value (texture signature) for adaptive subsampling.</returns>
+    public long Apply ( Intersection inter, int rank, int total )
+    {
+      return Apply( inter );
+    }
+  }
+
+  /// <summary>
   /// Unit sphere as a simple solid able to compute ray-intersection, normal vector
   /// and 2D texture coordinates.
   /// </summary>
@@ -534,63 +595,228 @@ namespace Rendering
   }
 
   /// <summary>
-  /// Simple texture able to modulate surface color.
+  /// Normalized (unit) cube as a simple solid able to compute ray-intersection, normal vector
+  /// and 2D texture coordinates. [0,1]^3
   /// </summary>
-  public class CheckerTexture : ITexture
+  public class Cube : DefaultSceneNode, ISolid
   {
-    /// <summary>
-    /// Alternative color.
-    /// </summary>
-    public double[] Color2;
-
-    /// <summary>
-    /// Frequency in the u-direction.
-    /// </summary>
-    public double Fu;
-
-    /// <summary>
-    /// Frequency in the v-direction.
-    /// </summary>
-    public double Fv;
-
-    public CheckerTexture ( double fu, double fv, double[] color )
+    protected enum CubeFaces
     {
-      Fu = fu;
-      Fv = fv;
-      Color2 = (double[])color.Clone();
+      PositiveX,
+      NegativeX,
+      PositiveY,
+      NegativeY,
+      PositiveZ,
+      NegativeZ
+    };
+
+    protected static Vector3d[] Normals =
+    {
+      new Vector3d(  1,  0,  0 ),
+      new Vector3d( -1,  0,  0 ),
+      new Vector3d(  0,  1,  0 ),
+      new Vector3d(  0, -1,  0 ),
+      new Vector3d(  0,  0,  1 ),
+      new Vector3d(  0,  0, -1 )
+    };
+
+    /// <summary>
+    /// Computes the complete intersection of the given ray with the object. 
+    /// </summary>
+    /// <param name="p0">Ray origin.</param>
+    /// <param name="p1">Ray direction vector.</param>
+    /// <returns>Sorted list of intersection records.</returns>
+    public override LinkedList<Intersection> Intersect ( Vector3d p0, Vector3d p1 )
+    {
+      double tMin = Double.MinValue;
+      double tMax = Double.MaxValue;
+      CubeFaces fMin = CubeFaces.PositiveX;
+      CubeFaces fMax = CubeFaces.PositiveX;
+      double mul, t1, t2;
+
+      // normal = X axis:
+      if ( Geometry.IsZero( p1.X ) )
+      {
+        if ( p0.X <= 0.0 || p0.X >= 1.0 )
+          return null;
+      }
+      else
+      {
+        mul = 1.0 / p1.X;
+        t1 = -p0.X * mul;
+        t2 = t1 + mul;
+
+        if ( mul > 0.0 )
+        {
+          if ( t1 > tMin )
+          {
+            tMin = t1;
+            fMin = CubeFaces.NegativeX;
+          }
+          if ( t2 < tMax )
+          {
+            tMax = t2;
+            fMax = CubeFaces.PositiveX;
+          }
+        }
+        else
+        {
+          if ( t2 > tMin )
+          {
+            tMin = t2;
+            fMin = CubeFaces.NegativeX;
+          }
+          if ( t1 < tMax )
+          {
+            tMax = t1;
+            fMax = CubeFaces.PositiveX;
+          }
+        }
+
+        if ( tMin >= tMax )
+          return null;
+      }
+
+      // normal = Y axis:
+      if ( Geometry.IsZero( p1.Y ) )
+      {
+        if ( p0.Y <= 0.0 || p0.Y >= 1.0 )
+          return null;
+      }
+      else
+      {
+        mul = 1.0 / p1.Y;
+        t1 = -p0.Y * mul;
+        t2 = t1 + mul;
+
+        if ( mul > 0.0 )
+        {
+          if ( t1 > tMin )
+          {
+            tMin = t1;
+            fMin = CubeFaces.NegativeY;
+          }
+          if ( t2 < tMax )
+          {
+            tMax = t2;
+            fMax = CubeFaces.PositiveY;
+          }
+        }
+        else
+        {
+          if ( t2 > tMin )
+          {
+            tMin = t2;
+            fMin = CubeFaces.NegativeY;
+          }
+          if ( t1 < tMax )
+          {
+            tMax = t1;
+            fMax = CubeFaces.PositiveY;
+          }
+        }
+
+        if ( tMin >= tMax )
+          return null;
+      }
+
+      // normal = Z axis:
+      if ( Geometry.IsZero( p1.Z ) )
+      {
+        if ( p0.Z <= 0.0 || p0.Z >= 1.0 )
+          return null;
+      }
+      else
+      {
+        mul = 1.0 / p1.Z;
+        t1 = -p0.Z * mul;
+        t2 = t1 + mul;
+
+        if ( mul > 0.0 )
+        {
+          if ( t1 > tMin )
+          {
+            tMin = t1;
+            fMin = CubeFaces.NegativeZ;
+          }
+          if ( t2 < tMax )
+          {
+            tMax = t2;
+            fMax = CubeFaces.PositiveZ;
+          }
+        }
+        else
+        {
+          if ( t2 > tMin )
+          {
+            tMin = t2;
+            fMin = CubeFaces.NegativeZ;
+          }
+          if ( t1 < tMax )
+          {
+            tMax = t1;
+            fMax = CubeFaces.PositiveZ;
+          }
+        }
+
+        if ( tMin >= tMax )
+          return null;
+      }
+
+      // Finally assemble the two intersections:
+      LinkedList<Intersection> result = new LinkedList<Intersection>();
+
+      Intersection i;
+      i = new Intersection( this );
+      i.T = tMin;
+      i.Enter =
+      i.Front = true;
+      i.CoordLocal = p0 + tMin * p1;
+      i.SolidData = fMin;
+      result.AddLast( i );
+
+      i = new Intersection( this );
+      i.T = tMax;
+      i.Enter =
+      i.Front = false;
+      i.CoordLocal = p0 + tMax * p1;
+      i.SolidData = fMax;
+      result.AddLast( i );
+
+      return result;
     }
 
     /// <summary>
-    /// Apply the relevant value-modulation in the given Intersection instance.
-    /// Simple variant, w/o an integration support.
+    /// Complete all relevant items in the given Intersection object.
     /// </summary>
-    /// <param name="inter">Data object to modify.</param>
-    /// <returns>Hash value (texture signature) for adaptive subsampling.</returns>
-    public long Apply ( Intersection inter )
+    /// <param name="inter">Intersection instance to complete.</param>
+    public override void CompleteIntersection ( Intersection inter )
     {
-      double u = inter.TextureCoord.X * Fu;
-      double v = inter.TextureCoord.Y * Fv;
+      // normal vector:
+      Vector3d tu, tv;
+      Geometry.GetAxes( ref Normals[ (int)inter.SolidData ], out tu, out tv );
+      tu = Vector3d.TransformVector( tu, inter.LocalToWorld );
+      tv = Vector3d.TransformVector( tv, inter.LocalToWorld );
+      Vector3d.Cross( ref tu, ref tv, out inter.Normal );
 
-      long ui = (long)Math.Floor( u );
-      long vi = (long)Math.Floor( v );
-
-      if ( ((ui + vi) & 1) != 0 )
-        Array.Copy( Color2, inter.SurfaceColor, Math.Min( Color2.Length, inter.SurfaceColor.Length ) );
-
-      return( ui + RandomStatic.numericRecipes( vi ) );
-    }
-
-    /// <summary>
-    /// Apply the relevant value-modulation in the given Intersection instance.
-    /// Internal integration support.
-    /// </summary>
-    /// <param name="inter">Data object to modify.</param>
-    /// <param name="rank">Rank of this sample, 0 <= rank < total (for integration).</param>
-    /// <param name="total">Total number of samples (for integration).</param>
-    /// <returns>Hash value (texture signature) for adaptive subsampling.</returns>
-    public long Apply ( Intersection inter, int rank, int total )
-    {
-      return Apply( inter );
+      // 2D texture coordinates:
+      switch ( (CubeFaces)inter.SolidData )
+      {
+        case CubeFaces.NegativeX:
+        case CubeFaces.PositiveX:
+          inter.TextureCoord.X = inter.CoordLocal.Y;
+          inter.TextureCoord.Y = inter.CoordLocal.Z;
+          break;
+        case CubeFaces.NegativeY:
+        case CubeFaces.PositiveY:
+          inter.TextureCoord.X = inter.CoordLocal.X;
+          inter.TextureCoord.Y = inter.CoordLocal.Z;
+          break;
+        default:
+          inter.TextureCoord.X = inter.CoordLocal.X;
+          inter.TextureCoord.Y = inter.CoordLocal.Y;
+          break;
+      }
     }
   }
 }
