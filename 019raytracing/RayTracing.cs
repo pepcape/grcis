c6c5a4d5fb05219a77db7199cb52b666fdc74c62
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2012-04-14 23:47:18 +0000

    New: test scene with cubes (still in main), Fix: normals of Cube.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@141 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 019raytracing/RayTracing.cs
+++ 019raytracing/RayTracing.cs
@@ -70,8 +70,8 @@ namespace Rendering
       sc.BackgroundColor = new double[] { 0.0, 0.05, 0.07 };
 
       // Camera:
-      sc.Camera = new StaticCamera( new Vector3d( 0.7, 0.5, -5.0 ),
-                                    new Vector3d( 0.0, -0.18, 1.0 ),
+      sc.Camera = new StaticCamera( new Vector3d( 0.7, 3.0, -10.0 ),
+                                    new Vector3d( 0.0, -0.3, 1.0 ),
                                     50.0 );
 
       // Light sources:
@@ -81,15 +81,40 @@ namespace Rendering
 
       // --- NODE DEFINITIONS ----------------------------------------------------
 
-      // Sphere
-      Sphere s = new Sphere();
-      root.InsertChild( s, Matrix4d.Identity );
-
-      // Plane
-      Plane pl = new Plane( 3.0, 2.0 ); // right but not isosceles triangle
-      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.5, 0.0, 0.0 } );
-      pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 6.0, 4.0, new double[] { 1.0, 1.0, 1.0 } ) );
+      // Base plane
+      Plane pl = new Plane();
+      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.6, 0.0, 0.0 } );
+      pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 0.5, 0.5, new double[] { 1.0, 1.0, 1.0 } ) );
       root.InsertChild( pl, Matrix4d.RotateX( -MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( 0.0, -1.0, 0.0 ) );
+
+      // Cubes
+      Cube c;
+      // front row:
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateY( 0.6 ) * Matrix4d.CreateTranslation( -3.5, -0.8, 0.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateY( 1.2 ) * Matrix4d.CreateTranslation( -1.5, -0.8, 0.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateY( 1.8 ) * Matrix4d.CreateTranslation( 0.5, -0.8, 0.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateY( 2.4 ) * Matrix4d.CreateTranslation( 2.5, -0.8, 0.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateY( 3.0 ) * Matrix4d.CreateTranslation( 4.5, -0.8, 0.0 ) );
+      // back row:
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 3.5 ) * Matrix4d.CreateTranslation( -4.0, 1.0, 2.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 3.0 ) * Matrix4d.CreateTranslation( -2.5, 1.0, 2.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 2.5 ) * Matrix4d.CreateTranslation( -1.0, 1.0, 2.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 2.0 ) * Matrix4d.CreateTranslation( 0.5, 1.0, 2.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 1.5 ) * Matrix4d.CreateTranslation( 2.0, 1.0, 2.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 1.0 ) * Matrix4d.CreateTranslation( 3.5, 1.0, 2.0 ) );
+      c = new Cube();
+      root.InsertChild( c, Matrix4d.RotateX( 0.5 ) * Matrix4d.CreateTranslation( 5.0, 1.0, 2.0 ) );
     }
   }
 }
