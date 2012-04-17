77975b41da3f64909fffafd1658d568dc13a6c61
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2012-04-17 00:08:48 +0000

    New: Cylinder solid, test scene, Fix: shadow casting.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@144 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 019raytracing/RayTracing.cs
+++ 019raytracing/RayTracing.cs
@@ -44,7 +44,7 @@ namespace _019raytracing
       comboScene.Items.Add( "Test scene" );
 
       // .. and set your favorite scene here:
-      comboScene.SelectedIndex = comboScene.Items.IndexOf( "Cubes" );
+      comboScene.SelectedIndex = comboScene.Items.IndexOf( "Test scene" );
     }
   }
 }
@@ -63,7 +63,7 @@ namespace Rendering
       // CSG scene:
       CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
       root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
-      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.7, 0.1, 0.0 }, 0.1, 0.8, 0.2, 16 ) );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.6, 0.0, 0.0 }, 0.15, 0.8, 0.15, 16 ) );
       sc.Intersectable = root;
 
       // Background color:
@@ -71,7 +71,7 @@ namespace Rendering
 
       // Camera:
       sc.Camera = new StaticCamera( new Vector3d( 0.7, 3.0, -10.0 ),
-                                    new Vector3d( 0.0, -0.3, 1.0 ),
+                                    new Vector3d( 0.0, -0.2, 1.0 ),
                                     50.0 );
 
       // Light sources:
@@ -83,38 +83,23 @@ namespace Rendering
 
       // Base plane
       Plane pl = new Plane();
-      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.4, 0.0 } );
+      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.0, 0.2, 0.0 } );
       pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 0.5, 0.5, new double[] { 1.0, 1.0, 1.0 } ) );
       root.InsertChild( pl, Matrix4d.RotateX( -MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( 0.0, -1.0, 0.0 ) );
 
-      // Cubes
-      Cube c;
-      // front row:
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateY( 0.6 ) * Matrix4d.CreateTranslation( -3.5, -0.8, 0.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateY( 1.2 ) * Matrix4d.CreateTranslation( -1.5, -0.8, 0.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateY( 1.8 ) * Matrix4d.CreateTranslation( 0.5, -0.8, 0.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateY( 2.4 ) * Matrix4d.CreateTranslation( 2.5, -0.8, 0.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateY( 3.0 ) * Matrix4d.CreateTranslation( 4.5, -0.8, 0.0 ) );
-      // back row:
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 3.5 ) * Matrix4d.CreateTranslation( -4.0, 1.0, 2.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 3.0 ) * Matrix4d.CreateTranslation( -2.5, 1.0, 2.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 2.5 ) * Matrix4d.CreateTranslation( -1.0, 1.0, 2.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 2.0 ) * Matrix4d.CreateTranslation( 0.5, 1.0, 2.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 1.5 ) * Matrix4d.CreateTranslation( 2.0, 1.0, 2.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 1.0 ) * Matrix4d.CreateTranslation( 3.5, 1.0, 2.0 ) );
-      c = new Cube();
-      root.InsertChild( c, Matrix4d.RotateX( 0.5 ) * Matrix4d.CreateTranslation( 5.0, 1.0, 2.0 ) );
+      // Cylinders
+      Cylinder c = new Cylinder();
+      root.InsertChild( c, Matrix4d.RotateX( MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( -2.1, 0.0, 1.0 ) );
+      c = new Cylinder();
+      c.SetAttribute( PropertyName.COLOR, new double[] { 0.2, 0.0, 0.7 } );
+      c.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 12.0, 1.0, new double[] { 0.0, 0.0, 0.3 } ) );
+      root.InsertChild( c, Matrix4d.RotateY( -0.4 ) * Matrix4d.CreateTranslation( 1.0, 0.0, 1.0 ) );
+      c = new Cylinder( 0.0, 100.0 );
+      c.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.7, 0.0 } );
+      root.InsertChild( c, Matrix4d.RotateY( 0.2 ) * Matrix4d.CreateTranslation( 5.0, 0.3, 4.0 ) );
+      c = new Cylinder( -0.5, 0.5 );
+      c.SetAttribute( PropertyName.COLOR, new double[] { 0.8, 0.6, 0.0 } );
+      root.InsertChild( c, Matrix4d.Scale( 2.0 ) * Matrix4d.RotateX( 1.2 ) * Matrix4d.CreateTranslation( 2.0, 1.8, 16.0 ) );
     }
   }
 }
--- common/RayCastingScenes.cs
+++ common/RayCastingScenes.cs
@@ -26,6 +26,7 @@ namespace Rendering
       "Flags",
       "Sphere on the plane",
       "Cubes",
+      "Cylinders",
     };
 
     /// <summary>
@@ -38,6 +39,7 @@ namespace Rendering
       new InitSceneDelegate( Flags ),
       new InitSceneDelegate( SpherePlane ),
       new InitSceneDelegate( Cubes ),
+      new InitSceneDelegate( Cylinders ),
     };
 
     /// <summary>
@@ -480,6 +482,55 @@ namespace Rendering
       root.InsertChild( c, Matrix4d.RotateX( 1.0 ) * Matrix4d.CreateTranslation( 3.5, 1.0, 2.0 ) );
       c = new Cube();
       root.InsertChild( c, Matrix4d.RotateX( 0.5 ) * Matrix4d.CreateTranslation( 5.0, 1.0, 2.0 ) );
+    }
+
+    /// <summary>
+    /// Test scene for cylinders
+    /// </summary>
+    public static void Cylinders ( IRayScene sc )
+    {
+      Debug.Assert( sc != null );
+
+      // CSG scene:
+      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
+      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 0.6, 0.0, 0.0 }, 0.15, 0.8, 0.15, 16 ) );
+      sc.Intersectable = root;
+
+      // Background color:
+      sc.BackgroundColor = new double[] { 0.0, 0.05, 0.07 };
+
+      // Camera:
+      sc.Camera = new StaticCamera( new Vector3d( 0.7, 3.0, -10.0 ),
+                                    new Vector3d( 0.0, -0.2, 1.0 ),
+                                    50.0 );
+
+      // Light sources:
+      sc.Sources = new LinkedList<ILightSource>();
+      sc.Sources.Add( new AmbientLightSource( 0.8 ) );
+      sc.Sources.Add( new PointLightSource( new Vector3d( -5.0, 3.0, -3.0 ), 1.0 ) );
+
+      // --- NODE DEFINITIONS ----------------------------------------------------
+
+      // Base plane
+      Plane pl = new Plane();
+      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.0, 0.2, 0.0 } );
+      pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 0.5, 0.5, new double[] { 1.0, 1.0, 1.0 } ) );
+      root.InsertChild( pl, Matrix4d.RotateX( -MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( 0.0, -1.0, 0.0 ) );
+
+      // Cylinders
+      Cylinder c = new Cylinder();
+      root.InsertChild( c, Matrix4d.RotateX( MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( -2.1, 0.0, 1.0 ) );
+      c = new Cylinder();
+      c.SetAttribute( PropertyName.COLOR, new double[] { 0.2, 0.0, 0.7 } );
+      c.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 12.0, 1.0, new double[] { 0.0, 0.0, 0.3 } ) );
+      root.InsertChild( c, Matrix4d.RotateY( -0.4 ) * Matrix4d.CreateTranslation( 1.0, 0.0, 1.0 ) );
+      c = new Cylinder( 0.0, 100.0 );
+      c.SetAttribute( PropertyName.COLOR, new double[] { 0.1, 0.7, 0.0 } );
+      root.InsertChild( c, Matrix4d.RotateY( 0.2 ) * Matrix4d.CreateTranslation( 5.0, 0.3, 4.0 ) );
+      c = new Cylinder( -0.5, 0.5 );
+      c.SetAttribute( PropertyName.COLOR, new double[] { 0.8, 0.6, 0.0 } );
+      root.InsertChild( c, Matrix4d.Scale( 2.0 ) * Matrix4d.RotateX( 1.2 ) * Matrix4d.CreateTranslation( 2.0, 1.8, 16.0 ) );
     }
   }
 }
