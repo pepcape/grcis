97719a7f244811f9747ee6c6a3653a9d1fe880ab
pepcape <pepcape@gmail.com>
2012-05-10 08:49:20 +0000

    Updt: 049distributedrt template is finished.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@175 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 049distributedrt/DistributedRT.cs
+++ 049distributedrt/DistributedRT.cs
@@ -23,7 +23,7 @@ namespace _049distributedrt
     /// </summary>
     private IImageFunction getImageFunction ( IRayScene scene )
     {
-      return new RayTracing( scene );
+      return new DistributedRayTracing( scene );
     }
 
     /// <summary>
@@ -90,30 +90,30 @@ namespace Rendering
 
       // Camera:
       sc.Camera = new StaticCamera( new Vector3d( 0.7, 0.5, -5.0 ),
-                                    new Vector3d( 0.0, -0.26, 1.0 ),
+                                    new Vector3d( 0.0, -0.28, 1.0 ),
                                     50.0 );
 
       // Light sources:
       sc.Sources = new LinkedList<ILightSource>();
       sc.Sources.Add( new AmbientLightSource( 0.8 ) );
-      sc.Sources.Add( new PointLightSource( new Vector3d( -5.0, 4.0, -3.0 ), 1.2 ) );
+      //sc.Sources.Add( new PointLightSource( new Vector3d( -5.0, 4.0, -3.0 ), 1.2 ) );
 
-      /*
+      /* */
       // horizontal stick source:
       //RectangleLightSource rls = new RectangleLightSource( new Vector3d( -5.0, 4.0, -6.0 ),
       //                                                     new Vector3d( 0.0, 0.0, 6.0 ),
-      //                                                     new Vector3d( 0.0, 0.1, 0.0 ), 2.2 );
+      //                                                     new Vector3d( 0.0, 0.1, 0.0 ), 1.8 );
       // vertical stick source:
       RectangleLightSource rls = new RectangleLightSource( new Vector3d( -5.0, 1.0, -3.0 ),
                                                            new Vector3d( 0.0, 0.0, 0.1 ),
-                                                           new Vector3d( 0.0, 6.0, 0.0 ), 2.2 );
+                                                           new Vector3d( 0.0, 6.0, 0.0 ), 1.8 );
       // rectangle source:
       //RectangleLightSource rls = new RectangleLightSource( new Vector3d( -5.0, 1.0, -6.0 ),
       //                                                     new Vector3d( 0.0, 0.0, 6.0 ),
-      //                                                     new Vector3d( 0.0, 6.0, 0.0 ), 2.2 );
-      rls.Dim = new double[] { 1.0, 0.04, 0.0 };
+      //                                                     new Vector3d( 0.0, 6.0, 0.0 ), 1.8 );
+      rls.Dim = new double[] { 1.0, 0.05, 0.0 };
       sc.Sources.Add( rls );
-      */
+      /* */
 
       // --- NODE DEFINITIONS ----------------------------------------------------
 
@@ -127,6 +127,136 @@ namespace Rendering
       pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 0.6, 0.6, new double[] { 1.0, 1.0, 1.0 } ) );
       root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 1.0, 0.8, 0.1 }, 0.1, 0.3, 0.7, 16 ) );
       root.InsertChild( pl, Matrix4d.RotateX( -MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( 0.0, -1.0, 0.0 ) );
+    }
+  }
+
+  /// <summary>
+  /// Distributed Ray-tracing.
+  /// Feel free to override the shade() function.
+  /// </summary>
+  public class DistributedRayTracing : RayTracing
+  {
+    public DistributedRayTracing ( IRayScene sc ) : base( sc )
+    {
+    }
+
+    /// <summary>
+    /// Recursive shading function - computes color contribution of the given ray (shot from the
+    /// origin 'p0' into direction vector 'p1''). Recursion is stopped
+    /// by a hybrid method: 'importance' and 'level' are checked.
+    /// Internal integration support.
+    /// </summary>
+    /// <param name="level">Current recursion depth.</param>
+    /// <param name="importance">Importance of the current ray.</param>
+    /// <param name="p0">Ray origin.</param>
+    /// <param name="p1">Ray direction vector.</param>
+    /// <param name="rank">Rank of this sample, 0 <= rank < total (for integration).</param>
+    /// <param name="total">Total number of samples (for integration).</param>
+    /// <param name="rnd">Global (per-thread) instance of the random generator.</param>
+    /// <param name="color">Result color.</param>
+    /// <returns>Hash-value (ray sub-signature) used for adaptive subsampling.</returns>
+    protected override long shade ( int level, double importance, ref Vector3d p0, ref Vector3d p1,
+                                    int rank, int total, RandomJames rnd, double[] color )
+    {
+      int bands = color.Length;
+      LinkedList<Intersection> intersections = scene.Intersectable.Intersect( p0, p1 );
+      Intersection.countRays++;
+      Intersection i = Intersection.FirstIntersection( intersections, ref p1 );
+
+      if ( i == null )          // no intersection -> background color
+      {
+        Array.Copy( scene.BackgroundColor, color, bands );
+        return 1L;
+      }
+
+      // there was at least one intersection
+      i.Complete();
+
+      // hash code for adaptive supersampling:
+      long hash = i.Solid.GetHashCode();
+
+      // apply all the textures fist..
+      if ( i.Textures != null )
+        foreach ( ITexture tex in i.Textures )
+          hash = hash * HASH_TEXTURE + tex.Apply( i, rank, total, rnd );
+
+      p1 = -p1;   // viewing vector
+      p1.Normalize();
+
+      if ( scene.Sources == null || scene.Sources.Count < 1 )
+        // no light sources at all
+        Array.Copy( i.SurfaceColor, color, bands );
+      else
+      {
+        // apply the reflectance model for each source
+        i.Material = (IMaterial)i.Material.Clone();
+        i.Material.Color = i.SurfaceColor;
+        Array.Clear( color, 0, bands );
+
+        foreach ( ILightSource source in scene.Sources )
+        {
+          Vector3d dir;
+          double[] intensity = source.GetIntensity( i, rank, total, rnd, out dir );
+          if ( intensity != null )
+          {
+            if ( DoShadows && !dir.Equals( Vector3d.Zero ) )
+            {
+              intersections = scene.Intersectable.Intersect( i.CoordWorld, dir );
+              Intersection.countRays++;
+              Intersection si = Intersection.FirstIntersection( intersections, ref dir );
+              // Better shadow testing: intersection between 0.0 and 1.0 kills the lighting
+              if ( si != null && !si.Far( 1.0, ref dir ) )
+                continue;
+            }
+
+            double[] reflection = i.ReflectanceModel.ColorReflection( i, dir, p1, ReflectionComponent.ALL );
+            if ( reflection != null )
+            {
+              for ( int b = 0; b < bands; b++ )
+                color[ b ] += intensity[ b ] * reflection[ b ];
+              hash = hash * HASH_LIGHT + source.GetHashCode();
+            }
+          }
+        }
+      }
+
+      // check the recursion depth:
+      if ( level++ >= MaxLevel ||
+           !DoReflections && !DoRefractions )
+        return hash;                        // no further recursion
+
+      Vector3d r;
+      double maxK;
+      double[] comp = new double[ bands ];
+      double newImportance;
+
+      if ( DoReflections )
+      {
+        // !!!{{ TODO: cast a reflected ray to a bit random direction (variance should be based on Phong material's exponent H)..
+
+        Geometry.SpecularReflection( ref i.Normal, ref p1, out r );
+        double[] ks = i.ReflectanceModel.ColorReflection( i, p1, r, ReflectionComponent.SPECULAR_REFLECTION );
+
+        // !!!}}
+
+        if ( ks != null )
+        {
+          maxK = ks[ 0 ];
+          for ( int b = 1; b < bands; b++ )
+            if ( ks[ b ] > maxK )
+              maxK = ks[ b ];
+
+          newImportance = importance * maxK;
+          if ( newImportance >= MinImportance ) // do compute the reflected ray
+          {
+            hash += HASH_REFLECT * shade( level, newImportance, ref i.CoordWorld, ref r, rank, total, rnd, comp );
+            for ( int b = 0; b < bands; b++ )
+              color[ b ] += ks[ b ] * comp[ b ];
+          }
+        }
+      }
+
+      return hash;
     }
   }
 }
