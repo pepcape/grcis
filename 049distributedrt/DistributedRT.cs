46a9598db6124bfb3acccf9c66d6d70d6965bd00
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2012-05-10 08:14:35 +0000

    New: 049distributedrt project (with exposed shade() function ready to overrides).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@174 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 049distributedrt/DistributedRT.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using MathSupport;
+using OpenTK;
+using Rendering;
+
+namespace _049distributedrt
+{
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Initialize the ray-scene.
+    /// </summary>
+    private IRayScene getScene ()
+    {
+      return SceneByComboBox();
+    }
+
+    /// <summary>
+    /// Initialize ray-scene and image function (good enough for simple samples).
+    /// </summary>
+    private IImageFunction getImageFunction ( IRayScene scene )
+    {
+      return new RayTracing( scene );
+    }
+
+    /// <summary>
+    /// Global instance of a random generator.
+    /// </summary>
+    private static RandomJames rnd = new RandomJames();
+
+    /// <summary>
+    /// Initialize image synthesizer (responsible for raster image computation).
+    /// </summary>
+    private IRenderer getRenderer ( IImageFunction imf )
+    {
+      SupersamplingImageSynthesizer sis = new SupersamplingImageSynthesizer();
+      sis.ImageFunction = imf;
+      return sis;
+    }
+
+    /// <summary>
+    /// Prepare combo-box of available scenes.
+    /// </summary>
+    private void InitializeScenes ()
+    {
+      sceneInitFunctions = new List<InitSceneDelegate>( Scenes.InitFunctions );
+
+      // 1. default scenes from RayCastingScenes
+      foreach ( string name in Scenes.Names )
+        comboScene.Items.Add( name );
+
+      // 2. eventually add custom scenes
+      sceneInitFunctions.Add( new InitSceneDelegate( CustomScene.TestScene ) );
+      comboScene.Items.Add( "Test scene" );
+
+      // .. and set your favorite scene here:
+      comboScene.SelectedIndex = comboScene.Items.IndexOf( "Test scene" );
+
+      // default image parameters?
+      ImageWidth  = 800;
+      ImageHeight = 540;
+      numericSupersampling.Value = 16;
+      checkMultithreading.Checked = true;
+    }
+  }
+}
+
+namespace Rendering
+{
+  /// <summary>
+  /// Custom scene for adaptive super-sampling (derived from "Sphere on the Plane").
+  /// </summary>
+  public class CustomScene
+  {
+    public static void TestScene ( IRayScene sc )
+    {
+      Debug.Assert( sc != null );
+
+      // CSG scene:
+      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
+      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 1.0, 0.8, 0.1 }, 0.1, 0.6, 0.4, 128 ) );
+      sc.Intersectable = root;
+
+      // Background color:
+      sc.BackgroundColor = new double[] { 0.0, 0.05, 0.07 };
+
+      // Camera:
+      sc.Camera = new StaticCamera( new Vector3d( 0.7, 0.5, -5.0 ),
+                                    new Vector3d( 0.0, -0.26, 1.0 ),
+                                    50.0 );
+
+      // Light sources:
+      sc.Sources = new LinkedList<ILightSource>();
+      sc.Sources.Add( new AmbientLightSource( 0.8 ) );
+      sc.Sources.Add( new PointLightSource( new Vector3d( -5.0, 4.0, -3.0 ), 1.2 ) );
+
+      /*
+      // horizontal stick source:
+      //RectangleLightSource rls = new RectangleLightSource( new Vector3d( -5.0, 4.0, -6.0 ),
+      //                                                     new Vector3d( 0.0, 0.0, 6.0 ),
+      //                                                     new Vector3d( 0.0, 0.1, 0.0 ), 2.2 );
+      // vertical stick source:
+      RectangleLightSource rls = new RectangleLightSource( new Vector3d( -5.0, 1.0, -3.0 ),
+                                                           new Vector3d( 0.0, 0.0, 0.1 ),
+                                                           new Vector3d( 0.0, 6.0, 0.0 ), 2.2 );
+      // rectangle source:
+      //RectangleLightSource rls = new RectangleLightSource( new Vector3d( -5.0, 1.0, -6.0 ),
+      //                                                     new Vector3d( 0.0, 0.0, 6.0 ),
+      //                                                     new Vector3d( 0.0, 6.0, 0.0 ), 2.2 );
+      rls.Dim = new double[] { 1.0, 0.04, 0.0 };
+      sc.Sources.Add( rls );
+      */
+
+      // --- NODE DEFINITIONS ----------------------------------------------------
+
+      // Sphere:
+      Sphere s = new Sphere();
+      root.InsertChild( s, Matrix4d.Identity );
+
+      // Infinite plane with checker:
+      Plane pl = new Plane();
+      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.3, 0.0, 0.0 } );
+      pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 0.6, 0.6, new double[] { 1.0, 1.0, 1.0 } ) );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 1.0, 0.8, 0.1 }, 0.1, 0.3, 0.7, 16 ) );
+      root.InsertChild( pl, Matrix4d.RotateX( -MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( 0.0, -1.0, 0.0 ) );
+    }
+  }
+}
