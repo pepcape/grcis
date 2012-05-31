30a5d6bd0c978d11a91e9f092b06811c1054e11d
pepcape <pepcape@gmail.com>
2012-05-31 14:00:06 +0000

    New: stat logging into 048rtmontecarlo.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@183 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 048rtmontecarlo/Form1.cs
+++ 048rtmontecarlo/Form1.cs
@@ -224,12 +224,15 @@ namespace _048rtmontecarlo
         elapsed = sw.ElapsedMilliseconds;
       }
 
-      SetText( String.Format( CultureInfo.InvariantCulture, "{0:f1}s  [ {1}x{2}, mt{3}, r{4:#,#}k, i{5:#,#}k, bb{6:#,#}k, t{7:#,#}k ]",
-                              1.0e-3 * elapsed, width, height, checkMultithreading.Checked ? Environment.ProcessorCount : 1,
-                              (Intersection.countRays + 500L) / 1000L,
-                              (Intersection.countIntersections + 500L) / 1000L,
-                              (CSGInnerNode.countBoundingBoxes + 500L) / 1000L,
-                              (CSGInnerNode.countTriangles + 500L) / 1000L ) );
+      String msg = String.Format( CultureInfo.InvariantCulture,
+                                  "{0:f1}s  [ {1}x{2}, mt{3}, r{4:#,#}k, i{5:#,#}k, bb{6:#,#}k, t{7:#,#}k ]",
+                                  1.0e-3 * elapsed, width, height, checkMultithreading.Checked ? Environment.ProcessorCount : 1,
+                                  (Intersection.countRays + 500L) / 1000L,
+                                  (Intersection.countIntersections + 500L) / 1000L,
+                                  (CSGInnerNode.countBoundingBoxes + 500L) / 1000L,
+                                  (CSGInnerNode.countTriangles + 500L) / 1000L );
+      SetText( msg );
+      Console.WriteLine( "Rendering finished: " + msg );
       SetImage( (Bitmap)outputImage.Clone() );
 
       Cursor.Current = Cursors.Default;
