cdb04b1a938bf26d1fbe6d0e4000e6db3733054a
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2012-10-07 23:23:52 +0000

    051colormap: 1st version of the project.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@184 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 051colormap/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _051colormap
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose( bool disposing )
+    {
+      if ( disposing && (components != null) )
+      {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.buttonClose = new System.Windows.Forms.Button();
+      this.numColors = new System.Windows.Forms.NumericUpDown();
+      this.label3 = new System.Windows.Forms.Label();
+      this.pictureBox1 = new _051colormap.MyBox();
+      this.buttonLoad = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.numColors)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // buttonClose
+      // 
+      this.buttonClose.Location = new System.Drawing.Point(692, 338);
+      this.buttonClose.Name = "buttonClose";
+      this.buttonClose.Size = new System.Drawing.Size(90, 24);
+      this.buttonClose.TabIndex = 7;
+      this.buttonClose.Text = "Done";
+      this.buttonClose.UseVisualStyleBackColor = true;
+      this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+      // 
+      // numColors
+      // 
+      this.numColors.Location = new System.Drawing.Point(500, 340);
+      this.numColors.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+      this.numColors.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+      this.numColors.Name = "numColors";
+      this.numColors.Size = new System.Drawing.Size(43, 20);
+      this.numColors.TabIndex = 8;
+      this.numColors.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+      this.numColors.ValueChanged += new System.EventHandler(this.numColors_ValueChanged);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(430, 342);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(64, 13);
+      this.label3.TabIndex = 9;
+      this.label3.Text = "Palette size:";
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(400, 348);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // buttonLoad
+      // 
+      this.buttonLoad.Location = new System.Drawing.Point(564, 338);
+      this.buttonLoad.Name = "buttonLoad";
+      this.buttonLoad.Size = new System.Drawing.Size(113, 23);
+      this.buttonLoad.TabIndex = 10;
+      this.buttonLoad.Text = "Load image";
+      this.buttonLoad.UseVisualStyleBackColor = true;
+      this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(794, 372);
+      this.Controls.Add(this.buttonLoad);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.numColors);
+      this.Controls.Add(this.buttonClose);
+      this.Controls.Add(this.pictureBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "Form1";
+      this.Text = "051 Paleta";
+      ((System.ComponentModel.ISupportInitialize)(this.numColors)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    public MyBox pictureBox1;
+    public System.Windows.Forms.Button buttonClose;
+    public System.Windows.Forms.NumericUpDown numColors;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button buttonLoad;
+  }
+}
