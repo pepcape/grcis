6c530da54d0892ea5743479c0b79360e87df76c8
pepcape <pepcape@gmail.com>
2012-10-07 23:23:52 +0000

    051colormap: 1st version of the project.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@184 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 051colormap/Colormap.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace _051colormap
+{
+  class Colormap
+  {
+    public static void Generate ( Bitmap input, int numCol, out Color[] colors )
+    {
+      // !!!{{ TODO - generate custom palette based on the given image
+
+      int width  = input.Width;
+      int height = input.Height;
+
+      colors = new Color[ numCol ];
+      colors[ 0 ]          = input.GetPixel( 0, 0 );
+      colors[ numCol - 1 ] = input.GetPixel( width/2, height/2 );
+
+      float r = colors[ 0 ].R;
+      float g = colors[ 0 ].G;
+      float b = colors[ 0 ].B;
+      float dr = (colors[ numCol - 1 ].R - r) / (numCol - 1.0f);
+      float dg = (colors[ numCol - 1 ].G - g) / (numCol - 1.0f);
+      float db = (colors[ numCol - 1 ].B - b) / (numCol - 1.0f);
+
+      for ( int i = 1; i < numCol; i++ )
+      {
+        r += dr; g += dg; b += db;
+        colors[ i ] = Color.FromArgb( (int)r, (int)g, (int)b );
+      }
+
+      // !!!}}
+    }
+  }
+}
---
+++ 051colormap/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _051colormap
+{
+  public partial class Form1 : Form
+  {
+    public static Color[] colors;
+
+    private static Bitmap inputImage = null;
+
+    public static int numCol = 3;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+      colors = new Color[ 3 ];
+      colors[ 0 ] = Color.FromArgb(   0, 100, 127 );
+      colors[ 1 ] = Color.FromArgb( 150, 150,  80 );
+      colors[ 2 ] = Color.FromArgb( 255, 100,  20 );
+      pictureBox1.Invalidate();
+    }
+
+    private void buttonClose_Click ( object sender, EventArgs e )
+    {
+      this.Close();
+    }
+
+    private void numColors_ValueChanged ( object sender, EventArgs e )
+    {
+      numCol = (int)numColors.Value;
+      if ( numCol > 8 ) numCol = 8;
+      if ( numCol < 3 ) numCol = 3;
+
+      if ( inputImage != null )
+      {
+        Colormap.Generate( inputImage, numCol, out colors );
+        pictureBox1.Invalidate();
+      }
+    }
+
+    public static string color2string ( Color col )
+    {
+      return "[" + col.R + ',' + col.G + ',' + col.B + ']';
+    }
+
+    private void buttonLoad_Click ( object sender, EventArgs e )
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
+
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+        return;
+
+      Image inp = Image.FromFile( ofd.FileName );
+      inputImage = new Bitmap( inp );
+      inp.Dispose();
+
+      Colormap.Generate( inputImage, numCol, out colors );
+      pictureBox1.Invalidate();
+    }
+  }
+}
