52a77ecafa320e5b2dcb28beb4fecd80d4466674
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2012-12-10 07:07:30 +0000

    057scene: exercise template.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@196 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 057scene/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _057scene
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main ()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault( false );
+      Application.Run( new Form1() );
+    }
+  }
+}
