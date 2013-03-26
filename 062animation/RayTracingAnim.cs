0efe84f9daed0472ef5342798c9608c60ba6f623
pepcape <pepcape@gmail.com>
2013-03-26 00:10:40 +0000

    062: final tuning - camera-animation example, comments, stc.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@224 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 062animation/RayTracingAnim.cs
+++ 062animation/RayTracingAnim.cs
@@ -15,7 +15,7 @@ namespace _062animation
     /// </summary>
     private IRayScene getScene ()
     {
-      return new RayScene();
+      return new AnimatedScene();
     }
 
     /// <summary>
@@ -50,6 +50,7 @@ namespace Rendering
 {
   /// <summary>
   /// Pilot implementation of time-dependent camera.
+  /// It simply goes round the central point (look-at point).
   /// </summary>
   public class AnimatedCamera : StaticCamera, ITimeDependent
   {
@@ -81,9 +82,6 @@ namespace Rendering
     {
       Debug.Assert( !Double.Equals( Start, End ) );
 
-      // !!!{{ TODO: put your camera time-dependency code here
-
-      //time = Arith.Clamp( newTime, Start, End );
       time = newTime;    // Here Start & End define a periodicity, not bounds!
 
       // change the camera position:
@@ -93,8 +91,6 @@ namespace Rendering
       direction = -radial;
       direction.Normalize();
       prepare();
-
-      // !!!}}
     }
 
     /// <summary>
@@ -146,30 +142,69 @@ namespace Rendering
     }
   }
 
+  // !!!{{ TODO: put your new time-dependent scene-construction classes here
+
+  // !!!}}
+
   /// <summary>
   /// Animated Ray-scene.
   /// </summary>
-  public class RayScene : AnimatedRayScene
+  public class AnimatedScene : AnimatedRayScene
   {
     /// <summary>
     /// Creates default ray-rendering scene.
     /// </summary>
-    public RayScene ()
+    public AnimatedScene ()
     {
-      // !!!{{ TODO: put your scene and camera setup code here
+      // !!!{{ TODO: .. and use your time-dependent objects to construct the scene
 
-      Scenes.FiveBalls( this );
-      AnimatedCamera cam = new AnimatedCamera( new Vector3d( 0.0, 0.0, 1.0 ),
-                                               new Vector3d( 0.0, 0.0, -9.0 ),
-                                               60.0 );
+      // This code is based on Scenes.TwoSpheres():
 
-      //Scenes.Bezier( this );
-      //AnimatedCamera cam = new AnimatedCamera( new Vector3d( 0.7, -1.0, 3.0 ),
-      //                                         new Vector3d( 0.7, 0.5, -5.0 ),
-      //                                         55.0 );
+      // CSG scene:
+      CSGInnerNode root = new CSGInnerNode( SetOperation.Union );
+      root.SetAttribute( PropertyName.REFLECTANCE_MODEL, new PhongModel() );
+      root.SetAttribute( PropertyName.MATERIAL, new PhongMaterial( new double[] { 1.0, 0.8, 0.1 }, 0.1, 0.6, 0.4, 128 ) );
+      Intersectable = root;
 
-      Camera = cam;
-      cam.End = 5.0;   // one complete turn takes 5.0 seconds
+      // Background color:
+      BackgroundColor = new double[] { 0.0, 0.05, 0.07 };
+
+      // Camera:
+      AnimatedCamera cam = new AnimatedCamera( new Vector3d( 0.7, -0.4,  0.0 ),
+                                               new Vector3d( 0.7,  0.5, -6.0 ),
+                                               50.0 );
+      cam.End = 20.0;   // one complete turn takes 20.0 seconds
+      Camera  = cam;
+
+      //Camera = new StaticCamera( new Vector3d( 0.7,  0.5, -5.0 ),
+      //                           new Vector3d( 0.0, -0.18, 1.0 ),
+      //                           50.0 );
+
+      // Light sources:
+      Sources = new LinkedList<ILightSource>();
+      Sources.Add( new AmbientLightSource( 0.8 ) );
+      Sources.Add( new PointLightSource( new Vector3d( -5.0, 4.0, -3.0 ), 1.2 ) );
+
+      // --- NODE DEFINITIONS ----------------------------------------------------
+
+      // Transparent sphere:
+      Sphere s;
+      s = new Sphere();
+      PhongMaterial pm = new PhongMaterial( new double[] { 0.0, 0.2, 0.1 }, 0.05, 0.05, 0.1, 128 );
+      pm.n = 1.6;
+      pm.Kt = 0.9;
+      s.SetAttribute( PropertyName.MATERIAL, pm );
+      root.InsertChild( s, Matrix4d.Identity );
+
+      // Opaque sphere:
+      s = new Sphere();
+      root.InsertChild( s, Matrix4d.Scale( 1.2 ) * Matrix4d.CreateTranslation( 1.5, 0.2, 2.4 ) );
+
+      // Infinite plane with checker:
+      Plane pl = new Plane();
+      pl.SetAttribute( PropertyName.COLOR, new double[] { 0.3, 0.0, 0.0 } );
+      pl.SetAttribute( PropertyName.TEXTURE, new CheckerTexture( 0.6, 0.6, new double[] { 1.0, 1.0, 1.0 } ) );
+      root.InsertChild( pl, Matrix4d.RotateX( -MathHelper.PiOver2 ) * Matrix4d.CreateTranslation( 0.0, -1.0, 0.0 ) );
 
       // !!!}}
     }
