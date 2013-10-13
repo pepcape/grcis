f3404a129fd3296e934d16533752ac34bf64af13
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2013-10-13 21:25:47 +0000

    066histogram: first version cloned from 052.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@232 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 066histogram/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _066histogram
+{
+  public partial class Form1 : Form
+  {
+    protected Image inputImage = null;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonOpen_Click ( object sender, EventArgs e )
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
+
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+        return;
+
+      Image inp = Image.FromFile( ofd.FileName );
+      inputImage = new Bitmap( inp );
+      inp.Dispose();
+
+      recompute();
+    }
+
+    private void recompute ()
+    {
+      if ( inputImage == null ) return;
+
+      pictureBox1.Image = (Bitmap)inputImage;
+    }
+
+    private void buttonHistogram_Click ( object sender, EventArgs e )
+    {
+      recompute();
+    }
+  }
+}
