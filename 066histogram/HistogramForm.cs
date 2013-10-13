da220d4148c177e92baa2c32b9e49be2b101081d
pepcape <pepcape@gmail.com>
2013-10-13 22:05:09 +0000

    066histogram: modeless histogram form called from the main form..
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@233 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 066histogram/HistogramForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace _066histogram
+{
+  public partial class HistogramForm : Form
+  {
+    protected Form1 parent;
+
+    public HistogramForm ( Form1 par )
+    {
+      parent = par;
+      InitializeComponent();
+    }
+
+    private void HistogramForm_FormClosed ( object sender, FormClosedEventArgs e )
+    {
+      parent.histogramForm = null;
+    }
+  }
+}
