5f73177c1515a281b9c5931ded1eb7a8c8764187
pepcape <pepcape@gmail.com>
2013-10-14 01:23:49 +0000

    066histogram: final version before the 1st lab.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@234 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 066histogram/Form1.cs
+++ 066histogram/Form1.cs
@@ -6,13 +6,26 @@ namespace _066histogram
 {
   public partial class Form1 : Form
   {
-    protected Image inputImage = null;
+    /// <summary>
+    /// Source image.
+    /// </summary>
+    public Image inputImage = null;
 
+    /// <summary>
+    /// Input file-name.
+    /// </summary>
+    public string fileName = null;
+
+    /// <summary>
+    /// Modeless histogram window.
+    /// </summary>
     public HistogramForm histogramForm = null;
 
     public Form1 ()
     {
       InitializeComponent();
+      String[] tok = "$Rev$".Split( ' ' );
+      Text += " (rev: " + tok[ 1 ] + ')';
     }
 
     private void buttonOpen_Click ( object sender, EventArgs e )
@@ -32,17 +45,21 @@ namespace _066histogram
       if ( ofd.ShowDialog() != DialogResult.OK )
         return;
 
-      Image inp = Image.FromFile( ofd.FileName );
+      Image inp = Image.FromFile( fileName = ofd.FileName );
       inputImage = new Bitmap( inp );
       inp.Dispose();
 
       recompute();
     }
 
+    /// <summary>
+    /// Recomputes the histogram window (forces input image pass).
+    /// </summary>
     private void recompute ()
     {
       if ( inputImage == null ) return;
 
+      dirty = true;
       pictureBox1.Image = (Bitmap)inputImage;
 
       if ( histogramForm == null )
@@ -50,8 +67,10 @@ namespace _066histogram
         histogramForm = new HistogramForm( this );
         histogramForm.Show();
       }
+      else
+        histogramForm.Invalidate();
 
-      // TODO: redraw histogram
+      histogramForm.Text = "Histogram (" + fileName + ')';
     }
 
     private void buttonHistogram_Click ( object sender, EventArgs e )
---
+++ 066histogram/Histogram.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using MathSupport;
+
+namespace _066histogram
+{
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Has the source image changed?
+    /// </summary>
+    public bool dirty = false;
+
+    /// <summary>
+    /// Cached histogram data.
+    /// </summary>
+    protected int[] histArray = null;
+
+    /// <summary>
+    /// Recomputes image histogram and draws the result in the given raster image.
+    /// </summary>
+    /// <param name="input">Input image.</param>
+    /// <param name="graph">Result image (for the graph).</param>
+    /// <param name="param">Textual parameter.</param>
+    public void ComputeHistogram ( Bitmap input, Bitmap graph, string param )
+    {
+      // !!!{{ TODO: write your own histogram computation and drawing code here
+
+      // Text parameter = histogram mode (0 .. red, 1 .. green, 2 .. blue, 3 .. gray)
+      int mode = 0;
+      if ( param.ToLower().IndexOf( "gray" ) >= 0 )
+      {
+        mode = 3;
+      }
+
+      int x, y;
+
+      // 1. Histogram recomputation:
+      if ( dirty )
+      {
+        dirty = false;
+
+        // recompute histogram data:
+        histArray = new int[ 256 ];
+
+        int width = input.Width;
+        int height = input.Height;
+        for ( y = 0; y < height; y++ )
+          for ( x = 0; x < width; x++ )
+          {
+            Color col = input.GetPixel( x, y );
+
+            //double H, S, V;
+            //Arith.ColorToHSV( col, out H, out S, out V );
+
+            histArray[ col.R ]++;
+          }
+      }
+
+      // 2. Graph drawing:
+      float max = 0.0f;
+      foreach ( var f in histArray )
+        if ( f > max ) max = f;
+
+      Graphics gfx = Graphics.FromImage( graph );
+      gfx.Clear( Color.White );
+
+      // Graph scaling:
+      float x0 = graph.Width * 0.05f;
+      float y0 = graph.Height * 0.95f;
+      float kx = graph.Width * 0.9f / histArray.Length;
+      float ky = -graph.Height * 0.9f / max;
+
+      // Pens:
+      Pen graphPen = new Pen( Color.Red );
+      Brush graphBrush = new SolidBrush( Color.Red );
+      Pen axisPen = new Pen( Color.Black );
+
+      // Histogram:
+      for ( x = 0; x < histArray.Length; x++ )
+        gfx.FillRectangle( graphBrush, x0 + x * kx, y0 + histArray[ x ] * ky, kx, -histArray[ x ] * ky );
+
+      // Axes:
+      gfx.DrawLine( axisPen, x0, y0, x0 + histArray.Length * kx, y0 );
+      gfx.DrawLine( axisPen, x0, y0, x0, y0 + max * ky );
+
+      gfx.Dispose();
+
+      // !!!}}
+    }
+  }
+}
