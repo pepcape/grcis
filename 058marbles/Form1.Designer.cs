e890dd494de813eef02d62c5ccce26c141b7a28f
pepcape <pepcape@gmail.com>
2013-12-16 09:22:26 +0000

    058marbles: small Form changes.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@259 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 058marbles/Form1.Designer.cs
+++ 058marbles/Form1.Designer.cs
@@ -41,54 +41,54 @@ namespace _058marbles
       // buttonInit
       // 
       this.buttonInit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.buttonInit.Location = new System.Drawing.Point( 13, 372 );
+      this.buttonInit.Location = new System.Drawing.Point(13, 372);
       this.buttonInit.Name = "buttonInit";
-      this.buttonInit.Size = new System.Drawing.Size( 73, 23 );
+      this.buttonInit.Size = new System.Drawing.Size(73, 23);
       this.buttonInit.TabIndex = 11;
       this.buttonInit.Text = "Init";
       this.buttonInit.UseVisualStyleBackColor = true;
-      this.buttonInit.Click += new System.EventHandler( this.buttonInit_Click );
+      this.buttonInit.Click += new System.EventHandler(this.buttonInit_Click);
       // 
       // label3
       // 
       this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point( 99, 378 );
+      this.label3.Location = new System.Drawing.Point(99, 378);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size( 40, 13 );
+      this.label3.Size = new System.Drawing.Size(40, 13);
       this.label3.TabIndex = 13;
       this.label3.Text = "Param:";
       // 
       // glControl1
       // 
-      this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                  | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
       this.glControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
       this.glControl1.BackColor = System.Drawing.Color.Black;
-      this.glControl1.Location = new System.Drawing.Point( 13, 12 );
+      this.glControl1.Location = new System.Drawing.Point(13, 12);
       this.glControl1.Name = "glControl1";
-      this.glControl1.Size = new System.Drawing.Size( 680, 343 );
+      this.glControl1.Size = new System.Drawing.Size(680, 343);
       this.glControl1.TabIndex = 17;
       this.glControl1.VSync = false;
-      this.glControl1.Load += new System.EventHandler( this.glControl1_Load );
-      this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler( this.glControl1_Paint );
-      this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler( this.glControl1_KeyDown );
-      this.glControl1.KeyUp += new System.Windows.Forms.KeyEventHandler( this.glControl1_KeyUp );
-      this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler( this.glControl1_MouseDown );
-      this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler( this.glControl1_MouseMove );
-      this.glControl1.MouseUp += new System.Windows.Forms.MouseEventHandler( this.glControl1_MouseUp );
-      this.glControl1.MouseWheel += new System.Windows.Forms.MouseEventHandler( this.glControl1_MouseWheel );
-      this.glControl1.Resize += new System.EventHandler( this.glControl1_Resize );
-      this.glControl1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler( this.control_PreviewKeyDown );
+      this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+      this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+      this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+      this.glControl1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyUp);
+      this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseDown);
+      this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+      this.glControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseUp);
+      this.glControl1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseWheel);
+      this.glControl1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.control_PreviewKeyDown);
+      this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
       // 
       // labelFps
       // 
-      this.labelFps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.labelFps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.labelFps.AutoSize = true;
-      this.labelFps.Location = new System.Drawing.Point( 574, 376 );
+      this.labelFps.Location = new System.Drawing.Point(346, 377);
       this.labelFps.Name = "labelFps";
-      this.labelFps.Size = new System.Drawing.Size( 27, 13 );
+      this.labelFps.Size = new System.Drawing.Size(27, 13);
       this.labelFps.TabIndex = 18;
       this.labelFps.Text = "Fps:";
       // 
@@ -96,9 +96,9 @@ namespace _058marbles
       // 
       this.checkSmooth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.checkSmooth.AutoSize = true;
-      this.checkSmooth.Location = new System.Drawing.Point( 322, 376 );
+      this.checkSmooth.Location = new System.Drawing.Point(222, 404);
       this.checkSmooth.Name = "checkSmooth";
-      this.checkSmooth.Size = new System.Drawing.Size( 102, 17 );
+      this.checkSmooth.Size = new System.Drawing.Size(102, 17);
       this.checkSmooth.TabIndex = 21;
       this.checkSmooth.Text = "Smooth shading";
       this.checkSmooth.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -108,9 +108,9 @@ namespace _058marbles
       // 
       this.checkWireframe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.checkWireframe.AutoSize = true;
-      this.checkWireframe.Location = new System.Drawing.Point( 433, 376 );
+      this.checkWireframe.Location = new System.Drawing.Point(330, 404);
       this.checkWireframe.Name = "checkWireframe";
-      this.checkWireframe.Size = new System.Drawing.Size( 74, 17 );
+      this.checkWireframe.Size = new System.Drawing.Size(74, 17);
       this.checkWireframe.TabIndex = 22;
       this.checkWireframe.Text = "Wireframe";
       this.checkWireframe.UseVisualStyleBackColor = true;
@@ -118,9 +118,9 @@ namespace _058marbles
       // textParam
       // 
       this.textParam.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.textParam.Location = new System.Drawing.Point( 146, 374 );
+      this.textParam.Location = new System.Drawing.Point(146, 374);
       this.textParam.Name = "textParam";
-      this.textParam.Size = new System.Drawing.Size( 148, 20 );
+      this.textParam.Size = new System.Drawing.Size(170, 20);
       this.textParam.TabIndex = 23;
       this.textParam.Text = "200";
       // 
@@ -130,9 +130,9 @@ namespace _058marbles
       this.checkCamera.AutoSize = true;
       this.checkCamera.Checked = true;
       this.checkCamera.CheckState = System.Windows.Forms.CheckState.Checked;
-      this.checkCamera.Location = new System.Drawing.Point( 322, 401 );
+      this.checkCamera.Location = new System.Drawing.Point(102, 404);
       this.checkCamera.Name = "checkCamera";
-      this.checkCamera.Size = new System.Drawing.Size( 114, 17 );
+      this.checkCamera.Size = new System.Drawing.Size(114, 17);
       this.checkCamera.TabIndex = 24;
       this.checkCamera.Text = "Camera movement";
       this.checkCamera.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -140,21 +140,21 @@ namespace _058marbles
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size( 712, 439 );
-      this.Controls.Add( this.checkCamera );
-      this.Controls.Add( this.textParam );
-      this.Controls.Add( this.checkWireframe );
-      this.Controls.Add( this.checkSmooth );
-      this.Controls.Add( this.labelFps );
-      this.Controls.Add( this.glControl1 );
-      this.Controls.Add( this.label3 );
-      this.Controls.Add( this.buttonInit );
-      this.MinimumSize = new System.Drawing.Size( 660, 200 );
+      this.ClientSize = new System.Drawing.Size(712, 439);
+      this.Controls.Add(this.checkCamera);
+      this.Controls.Add(this.textParam);
+      this.Controls.Add(this.checkWireframe);
+      this.Controls.Add(this.checkSmooth);
+      this.Controls.Add(this.labelFps);
+      this.Controls.Add(this.glControl1);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.buttonInit);
+      this.MinimumSize = new System.Drawing.Size(660, 200);
       this.Name = "Form1";
       this.Text = "058 marbles";
-      this.ResumeLayout( false );
+      this.ResumeLayout(false);
       this.PerformLayout();
 
     }
