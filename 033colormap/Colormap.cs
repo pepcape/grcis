0bcddeeee50e5c3a30f76772756c9e3ed616e66c
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2015-10-05 13:38:55 +0000

    033colormap: VS2010 conversion,   string param added,   couple of small improvements.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@307 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 033colormap/Colormap.cs
+++ 033colormap/Colormap.cs
@@ -1,29 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Author: Josef Pelikan
+
 using System.Drawing;
-using System.Text;
+using MathSupport;
+using System.Globalization;
 
 namespace _033colormap
 {
+  public partial class Form1
+  {
+    /// <summary>
+    /// Initialize the input data.
+    /// </summary>
+    void InitPaletteData ()
+    {
+      baseColor1 = Color.FromArgb( 180,  60,   0 );
+      baseColor2 = Color.FromArgb( 255, 240, 220 );
+      numCol = 4;
+      param = "180.0";
+    }
+  }
+
   class Colormap
   {
-    public static void generate ( Color baseColor1, Color baseColor2, int numCol, out Color[] colors )
+    /// <summary>
+    /// Generate custom palette based on 1-2 input colors.
+    /// </summary>
+    /// <param name="baseColor1">Mandatory primary input color.</param>
+    /// <param name="baseColor2">Optional secondary input color.</param>
+    /// <param name="numCol">Suggested palette size (could be ignored).</param>
+    /// <param name="param">Optional string parameter.</param>
+    /// <param name="colors">Output color array.</param>
+    public static void Generate ( Color baseColor1, Color baseColor2, int numCol, string param, out Color[] colors )
     {
-      // !!!{{ TODO - generate custom palette based on numCol, baseColor1 [and baseColor2]
+      // !!!{{ TODO - insert your custom palette generation code here
+
+      //--------------------------------------------------
+      // Sample code for HSV<->RGB conversion:
+      //double H, S, V;
+      //Arith.ColorToHSV( baseColor1, out H, out S, out V );
+      //Color col = Arith.HSVToColor( H, S, V );
 
       colors = new Color[ numCol ];
-      colors[ 0 ] = baseColor1;
-      float r = baseColor1.R;
+
+      float r;
+      //--------------------------------------------------
+      // Sample code for parsing float from param string:
+      if ( !float.TryParse( param, NumberStyles.Number, CultureInfo.InvariantCulture, out r ) )
+        r = baseColor1.R;
+
+      r = Arith.Clamp( r, 0, 255 );
       float g = baseColor1.G;
       float b = baseColor1.B;
+
       float dr = (baseColor2.R - r) / (numCol - 1.0f);
       float dg = (baseColor2.G - g) / (numCol - 1.0f);
       float db = (baseColor2.B - b) / (numCol - 1.0f);
-      for ( int i = 1; i < numCol; i++ )
+
+      for ( int i = 0; i < numCol; i++ )
       {
-        r += dr; g += dg; b += db;
         colors[ i ] = Color.FromArgb( (int)r, (int)g, (int)b );
+        r += dr; g += dg; b += db;
       }
 
       // !!!}}
