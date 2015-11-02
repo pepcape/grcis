75db8df6e41f2643073114f3affdea2d3a1b6c60
pepcape <pepcape@gmail.com>
2015-11-02 02:28:28 +0000

    084filter: 1st version.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@323 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 084filter/Filter.cs
@@ -0,0 +1,125 @@
+﻿// Author: Josef Pelikan
+
+using System.Drawing;
+using MathSupport;
+using System.Drawing.Imaging;
+using System;
+
+namespace _084filter
+{
+  class Filter
+  {
+    /// <summary>
+    /// Optional data initialization.
+    /// </summary>
+    public static void InitParams ( out string param )
+    {
+      param = "";
+    }
+
+    /// <summary>
+    /// Separator for string parameter.
+    /// </summary>
+    static readonly char COMMA = ',';
+
+    /// <summary>
+    /// Recompute the output image.
+    /// </summary>
+    /// <param name="input">Input image.</param>
+    /// <param name="param">Optional string parameter (its content and format is entierely up to you).</param>
+    public static Bitmap Recompute ( Bitmap input, string param )
+    {
+      // !!!{{ TODO: write your own image transform code here
+
+      if ( input == null )
+        return null;
+
+      // Text parameter = image size
+      int wid = input.Width;
+      int hei = input.Height;
+      int cellW = 8;
+      int cellH = 8;
+      if ( param.Length > 0 )
+      {
+        string[] size = param.Split( COMMA );
+        if ( size.Length > 0 )
+        {
+          if ( !int.TryParse( size[ 0 ], out cellW ) )
+            cellW = 1;
+          if ( cellW < 1 ) cellW = 1;
+          cellH = cellW;
+
+          if ( size.Length > 1 )
+          {
+            if ( !int.TryParse( size[ 1 ], out cellH ) )
+              cellH = cellW;
+            if ( cellH < 1 ) cellH = 1;
+          }
+        }
+      }
+
+      Bitmap output = new Bitmap( wid, hei, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+
+      // convert pixel data (fast memory-mapped code):
+      int x0, y0;
+      int x, y;
+      int x1, y1;
+      BitmapData dataIn  = input.LockBits( new Rectangle( 0, 0, wid, hei ), ImageLockMode.ReadOnly, input.PixelFormat );
+      BitmapData dataOut = output.LockBits( new Rectangle( 0, 0, wid, hei ), ImageLockMode.WriteOnly, output.PixelFormat );
+      unsafe
+      {
+        byte* iptr, optr;
+        byte r, g, b;
+        int sR, sG, sB;
+        int pixels;
+        int dI = Image.GetPixelFormatSize( input.PixelFormat ) / 8;
+        int dO = Image.GetPixelFormatSize( output.PixelFormat ) / 8;
+
+        for ( y0 = 0; y0 < hei; y0 += cellH )
+        {
+          if ( !Form1.cont ) break;
+
+          for ( x0 = 0; x0 < wid; x0 += cellW )     // one output cell
+          {
+            sR = sG = sB = 0;
+            x1 = Math.Min( x0 + cellW, wid );
+            y1 = Math.Min( y0 + cellH, hei );
+
+            for ( y = y0; y < y1; y++ )
+            {
+              iptr = (byte*)dataIn.Scan0 + y * dataIn.Stride + x0 * dI;
+              for ( x = x0; x < x1; x++, iptr += dI )
+              {
+                sB += (int)iptr[ 0 ];
+                sG += (int)iptr[ 1 ];
+                sR += (int)iptr[ 2 ];
+              }
+            }
+
+            pixels = (x1 - x0) * (y1 - y0);
+            r = (byte)((sR + pixels / 2) / pixels);
+            g = (byte)((sG + pixels / 2) / pixels);
+            b = (byte)((sB + pixels / 2) / pixels);
+
+            for ( y = y0; y < y1; y++ )
+            {
+              optr = (byte*)dataOut.Scan0 + y * dataOut.Stride + x0 * dO;
+              for ( x = x0; x < x1; x++, optr += dO )
+              {
+                optr[ 0 ] = b;
+                optr[ 1 ] = g;
+                optr[ 2 ] = r;
+              }
+            }
+          }
+        }
+      }
+      output.UnlockBits( dataOut );
+      input.UnlockBits( dataIn );
+
+      return output;
+
+      // !!!}}
+    }
+  }
+}
---
+++ 084filter/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _084filter
+{
+  public partial class Form1 : Form
+  {
+    protected Bitmap inputImage  = null;
+    protected Bitmap outputImage = null;
+
+    public Form1 ()
+    {
+      InitializeComponent();
+      string par;
+      Filter.InitParams( out par );
+      textParam.Text = par;
+
+      String[] tok = "$Rev$".Split( ' ' );
+      Text += " (rev: " + tok[ 1 ] + ')';
+    }
+
+    protected Thread aThread = null;
+
+    volatile public static bool cont = true;
+
+    delegate void SetImageCallback ( Bitmap newImage );
+
+    protected void SetImage ( Bitmap newImage )
+    {
+      if ( pictureBox1.InvokeRequired )
+      {
+        SetImageCallback si = new SetImageCallback( SetImage );
+        BeginInvoke( si, new object[] { newImage } );
+      }
+      else
+      {
+        pictureBox1.Image = newImage;
+        pictureBox1.Invalidate();
+      }
+    }
+
+    delegate void SetTextCallback ( string text );
+
+    protected void SetText ( string text )
+    {
+      if ( labelElapsed.InvokeRequired )
+      {
+        SetTextCallback st = new SetTextCallback( SetText );
+        BeginInvoke( st, new object[] { text } );
+      }
+      else
+        labelElapsed.Text = text;
+    }
+
+    private void buttonOpen_Click ( object sender, EventArgs e )
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+
+      ofd.Title = "Open Image File";
+      ofd.Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif";
+
+      ofd.FilterIndex = 6;
+      ofd.FileName = "";
+      if ( ofd.ShowDialog() != DialogResult.OK )
+      {
+        if ( inputImage != null )
+          inputImage.Dispose();
+
+        inputImage = null;
+        recompute();
+        return;
+      }
+
+      newImage( ofd.FileName );
+    }
+
+    /// <summary>
+    /// Sets the new image defined by its file-name. Does all the checks.
+    /// </summary>
+    private bool newImage ( string fn )
+    {
+      Image inp = null;
+      try
+      {
+        inp = Image.FromFile( fn );
+      }
+      catch ( Exception )
+      {
+        return false;
+      }
+
+      if ( inputImage != null )
+        inputImage.Dispose();
+      inputImage = new Bitmap( inp );
+
+      if ( outputImage != null )
+        outputImage.Dispose();
+      outputImage = null;
+
+      inp.Dispose();
+      recompute();
+
+      return true;
+    }
+
+    protected void EnableGUI ( bool enable )
+    {
+      buttonRedraw.Enabled =
+      buttonOpen.Enabled   =
+      buttonSave.Enabled   =
+      textParam.Enabled    = enable;
+      buttonStop.Enabled   = !enable;
+    }
+
+    delegate void StopComputationCallback ();
+
+    protected void StopComputation ()
+    {
+      if ( aThread == null )
+        return;
+
+      if ( buttonRedraw.InvokeRequired )
+      {
+        StopComputationCallback ea = new StopComputationCallback( StopComputation );
+        BeginInvoke( ea );
+      }
+      else
+      {
+        // actually stop the computation:
+        cont = false;
+        aThread.Join();
+        aThread = null;
+
+        // GUI stuff:
+        EnableGUI( true );
+      }
+    }
+
+    private void transform ()
+    {
+      Bitmap ibmp = inputImage;
+      Bitmap bmp;
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+
+      bmp = Filter.Recompute( ibmp, textParam.Text );
+
+      sw.Stop();
+      float elapsed = 1.0e-3f * sw.ElapsedMilliseconds;
+
+      SetImage( bmp );
+      if ( outputImage != null )
+        outputImage.Dispose();
+      outputImage = bmp;
+
+      SetText( string.Format( CultureInfo.InvariantCulture, "Elapsed: {0:f3}s", elapsed ) );
+
+      StopComputation();
+    }
+
+    private void recompute ()
+    {
+      if ( aThread != null )
+        return;
+
+      EnableGUI( false );
+      cont = true;
+
+      aThread = new Thread( new ThreadStart( this.transform ) );
+      aThread.Start();
+    }
+
+    private void buttonSave_Click ( object sender, EventArgs e )
+    {
+      if ( outputImage == null ) return;
+
+      SaveFileDialog sfd = new SaveFileDialog();
+      sfd.Title = "Save PNG file";
+      sfd.Filter = "PNG Files|*.png";
+      sfd.AddExtension = true;
+      sfd.FileName = "";
+
+      if ( sfd.ShowDialog() != DialogResult.OK )
+        return;
+
+      outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      recompute();
+    }
+
+    private void buttonStop_Click ( object sender, EventArgs e )
+    {
+      StopComputation();
+    }
+
+    private void Form1_DragDrop ( object sender, DragEventArgs e )
+    {
+      string[] strFiles = (string[])e.Data.GetData( DataFormats.FileDrop );
+      newImage( strFiles[ 0 ] );
+    }
+
+    private void Form1_DragEnter ( object sender, DragEventArgs e )
+    {
+      if ( e.Data.GetDataPresent( DataFormats.FileDrop ) )
+        e.Effect = DragDropEffects.Copy;
+    }
+  }
+}
