0ef58536fb9531635845b83047cd0721ecc31bb2
pepcape <pepcape@gmail.com>
2015-12-03 08:58:50 +0000

    011compressionbw: form tuning.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@345 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 011compressionbw/Form1.Designer.cs
+++ 011compressionbw/Form1.Designer.cs
@@ -42,22 +42,22 @@
       // 
       // panel1
       // 
-      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                  | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
       this.panel1.AutoScroll = true;
       this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.panel1.Controls.Add( this.pictureBox1 );
-      this.panel1.Location = new System.Drawing.Point( 13, 13 );
+      this.panel1.Controls.Add(this.pictureBox1);
+      this.panel1.Location = new System.Drawing.Point(13, 13);
       this.panel1.Name = "panel1";
-      this.panel1.Size = new System.Drawing.Size( 680, 410 );
+      this.panel1.Size = new System.Drawing.Size(680, 410);
       this.panel1.TabIndex = 0;
       // 
       // pictureBox1
       // 
-      this.pictureBox1.Location = new System.Drawing.Point( 0, 0 );
+      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
       this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size( 680, 410 );
+      this.pictureBox1.Size = new System.Drawing.Size(680, 410);
       this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
       this.pictureBox1.TabIndex = 2;
       this.pictureBox1.TabStop = false;
@@ -65,32 +65,32 @@
       // buttonSave
       // 
       this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.buttonSave.Location = new System.Drawing.Point( 585, 439 );
+      this.buttonSave.Location = new System.Drawing.Point(585, 439);
       this.buttonSave.Name = "buttonSave";
-      this.buttonSave.Size = new System.Drawing.Size( 108, 23 );
+      this.buttonSave.Size = new System.Drawing.Size(108, 23);
       this.buttonSave.TabIndex = 2;
       this.buttonSave.Text = "Save image";
       this.buttonSave.UseVisualStyleBackColor = true;
-      this.buttonSave.Click += new System.EventHandler( this.buttonSave_Click );
+      this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
       // 
       // buttonRecode
       // 
       this.buttonRecode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.buttonRecode.Location = new System.Drawing.Point( 140, 439 );
+      this.buttonRecode.Location = new System.Drawing.Point(140, 439);
       this.buttonRecode.Name = "buttonRecode";
-      this.buttonRecode.Size = new System.Drawing.Size( 97, 23 );
+      this.buttonRecode.Size = new System.Drawing.Size(97, 23);
       this.buttonRecode.TabIndex = 5;
       this.buttonRecode.Text = "Recode";
       this.buttonRecode.UseVisualStyleBackColor = true;
-      this.buttonRecode.Click += new System.EventHandler( this.buttonRecode_Click );
+      this.buttonRecode.Click += new System.EventHandler(this.buttonRecode_Click);
       // 
       // labelElapsed
       // 
       this.labelElapsed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.labelElapsed.AutoSize = true;
-      this.labelElapsed.Location = new System.Drawing.Point( 254, 444 );
+      this.labelElapsed.Location = new System.Drawing.Point(251, 445);
       this.labelElapsed.Name = "labelElapsed";
-      this.labelElapsed.Size = new System.Drawing.Size( 48, 13 );
+      this.labelElapsed.Size = new System.Drawing.Size(48, 13);
       this.labelElapsed.TabIndex = 8;
       this.labelElapsed.Text = "Elapsed:";
       // 
@@ -98,54 +98,54 @@
       // 
       this.labelResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.labelResult.AutoSize = true;
-      this.labelResult.Location = new System.Drawing.Point( 390, 445 );
+      this.labelResult.Location = new System.Drawing.Point(414, 446);
       this.labelResult.Name = "labelResult";
-      this.labelResult.Size = new System.Drawing.Size( 32, 13 );
+      this.labelResult.Size = new System.Drawing.Size(32, 13);
       this.labelResult.TabIndex = 9;
       this.labelResult.Text = "result";
       // 
       // buttonLoad
       // 
       this.buttonLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.buttonLoad.Location = new System.Drawing.Point( 13, 439 );
+      this.buttonLoad.Location = new System.Drawing.Point(13, 439);
       this.buttonLoad.Name = "buttonLoad";
-      this.buttonLoad.Size = new System.Drawing.Size( 110, 23 );
+      this.buttonLoad.Size = new System.Drawing.Size(110, 23);
       this.buttonLoad.TabIndex = 13;
       this.buttonLoad.Text = "Load image";
       this.buttonLoad.UseVisualStyleBackColor = true;
-      this.buttonLoad.Click += new System.EventHandler( this.buttonLoad_Click );
+      this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
       // 
       // checkDiff
       // 
       this.checkDiff.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.checkDiff.AutoSize = true;
-      this.checkDiff.Location = new System.Drawing.Point( 501, 442 );
+      this.checkDiff.Location = new System.Drawing.Point(501, 444);
       this.checkDiff.Name = "checkDiff";
-      this.checkDiff.Size = new System.Drawing.Size( 76, 17 );
+      this.checkDiff.Size = new System.Drawing.Size(76, 17);
       this.checkDiff.TabIndex = 14;
       this.checkDiff.Text = "Show diff?";
       this.checkDiff.UseVisualStyleBackColor = true;
-      this.checkDiff.CheckedChanged += new System.EventHandler( this.checkDiff_CheckedChanged );
+      this.checkDiff.CheckedChanged += new System.EventHandler(this.checkDiff_CheckedChanged);
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size( 712, 474 );
-      this.Controls.Add( this.checkDiff );
-      this.Controls.Add( this.buttonLoad );
-      this.Controls.Add( this.labelResult );
-      this.Controls.Add( this.labelElapsed );
-      this.Controls.Add( this.buttonRecode );
-      this.Controls.Add( this.buttonSave );
-      this.Controls.Add( this.panel1 );
-      this.MinimumSize = new System.Drawing.Size( 720, 200 );
+      this.ClientSize = new System.Drawing.Size(712, 474);
+      this.Controls.Add(this.checkDiff);
+      this.Controls.Add(this.buttonLoad);
+      this.Controls.Add(this.labelResult);
+      this.Controls.Add(this.labelElapsed);
+      this.Controls.Add(this.buttonRecode);
+      this.Controls.Add(this.buttonSave);
+      this.Controls.Add(this.panel1);
+      this.MinimumSize = new System.Drawing.Size(728, 200);
       this.Name = "Form1";
       this.Text = "011 compress B/W";
-      this.panel1.ResumeLayout( false );
+      this.panel1.ResumeLayout(false);
       this.panel1.PerformLayout();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-      this.ResumeLayout( false );
+      this.ResumeLayout(false);
       this.PerformLayout();
 
     }
