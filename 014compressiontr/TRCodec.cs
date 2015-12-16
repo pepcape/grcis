2ce1a9f25035a62e093fcf9fda1e8336b3c1663a
pepcape <pepcape@gmail.com>
2015-12-16 23:34:19 +0000

    014compressiontr: channel codec update.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@356 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 014compressiontr/TRCodec.cs
+++ 014compressiontr/TRCodec.cs
@@ -1,10 +1,11 @@
 ﻿// Author: Josef Pelikan
 
-using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
-using Compression;
+using System.IO.Compression;
 using Raster;
+using Support;
 
 namespace _014compressiontr
 {
@@ -37,30 +38,30 @@ namespace _014compressiontr
 
       // !!!{{ TODO: add the encoding code here
 
-      IEntropyCodec c = new DeflateCodec();
-      c.MaxSymbol = 255;
-      if ( c.MaxSymbol < 255 )
-        throw new Exception( "Unappropriate codec used (alphabet too small)!" );
-
-      c.BinaryStream = outs;
-      c.Open( true );
-
-      // file header: [ MAGIC, width, height ]
-      c.PutBits( MAGIC, 32 );
-      c.PutBits( width, 16 );
-      c.PutBits( height, 16 );
-
-      for ( int y = 0; y < height; y++ )
+      using ( DeflateStream ds = new BufferedDeflateStream( 16384, outs, CompressionMode.Compress, true ) )
       {
-        for ( int x = 0; x < width; x++ )
+        // file header: [ MAGIC, width, height ]
+        ds.WriteByte( (byte)((MAGIC >> 24) & 0xff) );
+        ds.WriteByte( (byte)((MAGIC >> 16) & 0xff) );
+        ds.WriteByte( (byte)((MAGIC >>  8) & 0xff) );
+        ds.WriteByte( (byte)( MAGIC        & 0xff) );
+
+        ds.WriteByte( (byte)((width >> 8) & 0xff) );
+        ds.WriteByte( (byte)( width       & 0xff) );
+
+        ds.WriteByte( (byte)((height >> 8) & 0xff) );
+        ds.WriteByte( (byte)( height       & 0xff) );
+
+        for ( int y = 0; y < height; y++ )
         {
-          Color col = inp.GetPixel( x, y );
-          int gr = Draw.RgbToGray( col.R, col.G, col.B );
-          c.Put( gr & 0xff );
+          for ( int x = 0; x < width; x++ )
+          {
+            Color col = inp.GetPixel( x, y );
+            int gr = Draw.RgbToGray( col.R, col.G, col.B );
+            ds.WriteByte( (byte)(gr & 0xff) );
+          }
         }
       }
-
-      c.Close();
 
       // !!!}}
     }
@@ -71,36 +72,47 @@ namespace _014compressiontr
 
       // !!!{{ TODO: add the decoding code here
 
-      IEntropyCodec c = new DeflateCodec();
-      c.BinaryStream = inps;
-      c.Open( false );
-
-      uint magic = (uint)c.GetBits( 32 );
-      if ( magic != MAGIC )
-        return null;
-
-      int width, height;
-      width = (int)c.GetBits( 16 );
-      height = (int)c.GetBits( 16 );
-
-      if ( width < 1 || height < 1 )
-        return null;
-
-      Bitmap result = new Bitmap( width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
-
-      for ( int y = 0; y < height; y++ )
+      Bitmap result;
+      using ( DeflateStream ds = new DeflateStream( inps, CompressionMode.Decompress, true ) )
       {
-        for ( int x = 0; x < width; x++ )
+        int buffer;
+        buffer = ds.ReadByte();
+        if ( buffer < 0 || buffer != ((MAGIC >> 24) & 0xff) ) return null;
+        buffer = ds.ReadByte();
+        if ( buffer < 0 || buffer != ((MAGIC >> 16) & 0xff) ) return null;
+        buffer = ds.ReadByte();
+        if ( buffer < 0 || buffer != ((MAGIC >>  8) & 0xff) ) return null;
+        buffer = ds.ReadByte();
+        if ( buffer < 0 || buffer != ( MAGIC        & 0xff) ) return null;
+
+        int width, height;
+        width = ds.ReadByte();
+        if ( width < 0 ) return null;
+        buffer = ds.ReadByte();
+        if ( buffer < 0 ) return null;
+        width = (width << 8) + buffer;
+        height = ds.ReadByte();
+        if ( height < 0 ) return null;
+        buffer = ds.ReadByte();
+        if ( buffer < 0 ) return null;
+        height = (height << 8) + buffer;
+
+        if ( width < 1 || height < 1 )
+          return null;
+
+        result = new Bitmap( width, height, PixelFormat.Format24bppRgb );
+
+        for ( int y = 0; y < height; y++ )
         {
-          int gr = c.Get();
-          if ( gr < 0 )
-            goto fin;
-          result.SetPixel( x, y, Color.FromArgb( gr, gr, gr ) );
+          for ( int x = 0; x < width; x++ )
+          {
+            int gr = ds.ReadByte();
+            if ( gr < 0 )
+              return result;
+            result.SetPixel( x, y, Color.FromArgb( gr, gr, gr ) );
+          }
         }
       }
-
-    fin:
-      c.Close();
 
       return result;
 
@@ -108,7 +120,5 @@ namespace _014compressiontr
     }
 
     #endregion
-
   }
-
 }
