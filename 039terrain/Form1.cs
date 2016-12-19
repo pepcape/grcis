b3d633b2c1fd32b046e6a849b9c5ca1fea02f8b3
pepcape <pepcape@gmail.com>
2016-12-19 05:36:46 +0000

    039terrain: functional upgraded version.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@458 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 039terrain/Form1.cs
+++ 039terrain/Form1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Windows.Forms;
 using Scene3D;
+using Utilities;
 
 namespace _039terrain
 {
   public partial class Form1 : Form
   {
-    static readonly string rev = "$Rev$".Split( ' ' )[ 1 ];
+    static readonly string rev = Util.SetVersion( "$Rev$" );
 
     protected SceneBrep scene = new SceneBrep();
 
@@ -28,7 +29,17 @@ namespace _039terrain
     public Form1 ()
     {
       InitializeComponent();
-      Text += " (rev: " + rev + ')';
+
+      int iter;
+      float rough;
+      string param;
+      string name;
+      InitParams( out iter, out rough, out param, out name );
+      textParam.Text = param ?? "";
+      upDownIterations.Value = iter;
+      upDownRoughness.Value = (decimal)rough;
+
+      Text += " (" + rev + ") '" + name + '\'';
     }
 
     private void buttonSave_Click ( object sender, EventArgs e )
@@ -56,14 +67,34 @@ namespace _039terrain
 
     private void glControl1_Paint ( object sender, PaintEventArgs e )
     {
+      Simulate( DateTime.Now.Ticks * 1.0e-7 );
       Render();
     }
 
     private void upDownIterations_ValueChanged ( object sender, EventArgs e )
     {
-      int iterations = (int)upDownIterations.Value;
+      if ( !loaded ) return;
+
+      int iterations  = (int)upDownIterations.Value;
       float roughness = (float)upDownRoughness.Value;
-      Regenerate( iterations, roughness );
+      Regenerate( iterations, roughness, textParam.Text );
+      labelStatus.Text = "Triangles: " + scene.Triangles;
+    }
+
+    private void buttonResetCam_Click ( object sender, EventArgs e )
+    {
+      ResetCamera();
+    }
+
+    private void checkVsync_CheckedChanged ( object sender, EventArgs e )
+    {
+      glControl1.VSync = checkVsync.Checked;
+    }
+
+    private void checkAnim_CheckedChanged ( object sender, EventArgs e )
+    {
+      if ( checkAnim.Checked )
+        InitSimulation( false );
     }
   }
 }
