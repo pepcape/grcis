a8cfe24e4ee7f8940c7b859662e1eda11238d3ca
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2017-10-08 14:36:37 +0000

    051colormap: template improvement, VS2015.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@528 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 051colormap/Colormap.cs
+++ 051colormap/Colormap.cs
@@ -1,16 +1,23 @@
-﻿// Author: Josef Pelikan
-
-using System.Drawing;
+﻿using System.Drawing;
+using MathSupport;
 
 namespace _051colormap
 {
   class Colormap
   {
     /// <summary>
+    /// Form data initialization.
+    /// </summary>
+    public static void InitForm ( out string author )
+    {
+      author = "Josef Pelikán";
+    }
+
+    /// <summary>
     /// Generate a colormap based on input image.
     /// </summary>
     /// <param name="input">Input raster image.</param>
-    /// <param name="numCol">Required colormap size (ignore it if you must)</param>
+    /// <param name="numCol">Required colormap size (ignore it if you must).</param>
     /// <param name="colors">Output palette (array of colors).</param>
     public static void Generate ( Bitmap input, int numCol, out Color[] colors )
     {
@@ -19,10 +26,19 @@ namespace _051colormap
       int width  = input.Width;
       int height = input.Height;
 
-      colors = new Color[ numCol ];
-      colors[ 0 ]          = input.GetPixel( 0, 0 );
-      colors[ numCol - 1 ] = input.GetPixel( width/2, height/2 );
+      colors = new Color[ numCol ];            // accepting the required palette size..
 
+      colors[ 0 ] = input.GetPixel( 0, 0 );    // upper left image corner
+
+      double H, S, V;
+      Color center = input.GetPixel( width / 2, height / 2 ); // image center
+      Arith.ColorToHSV( center, out H, out S, out V );
+      if ( S > 1.0e-3 )
+        colors[ numCol - 1 ] = Arith.HSVToColor( H, 1.0, 1.0 ); // non-monochromatic color => using Hue only
+      else
+        colors[ numCol - 1 ] = center;                          // monochromatic color => using it directly
+
+      // color-ramp linear interpolation:
       float r = colors[ 0 ].R;
       float g = colors[ 0 ].G;
       float b = colors[ 0 ].B;
