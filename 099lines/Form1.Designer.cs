53a72ea1277576e6b7812f1ce7638d12fd21317e
pepcape <pepcape@gmail.com>
2017-10-28 20:17:26 +0000

    103clrt: refactor, 099lines: project template clone.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@532 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 099lines/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace _099lines
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose ( bool disposing )
+    {
+      if ( disposing && (components != null) )
+      {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent ()
+    {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.numericXres = new System.Windows.Forms.NumericUpDown();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.labelElapsed = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.numericYres = new System.Windows.Forms.NumericUpDown();
+      this.buttonRedraw = new System.Windows.Forms.Button();
+      this.buttonSave = new System.Windows.Forms.Button();
+      this.textParam = new System.Windows.Forms.TextBox();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericXres)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericYres)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.panel1.AutoScroll = true;
+      this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.panel1.Controls.Add(this.pictureBox1);
+      this.panel1.Location = new System.Drawing.Point(13, 13);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(680, 380);
+      this.panel1.TabIndex = 0;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(680, 380);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      // 
+      // numericXres
+      // 
+      this.numericXres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.numericXres.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+      this.numericXres.Location = new System.Drawing.Point(50, 406);
+      this.numericXres.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+      this.numericXres.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+      this.numericXres.Name = "numericXres";
+      this.numericXres.Size = new System.Drawing.Size(71, 20);
+      this.numericXres.TabIndex = 3;
+      this.numericXres.Value = new decimal(new int[] {
+            800,
+            0,
+            0,
+            0});
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(14, 408);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(31, 13);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Xres:";
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(142, 408);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(40, 13);
+      this.label2.TabIndex = 6;
+      this.label2.Text = "Param:";
+      // 
+      // labelElapsed
+      // 
+      this.labelElapsed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.labelElapsed.AutoSize = true;
+      this.labelElapsed.Location = new System.Drawing.Point(284, 440);
+      this.labelElapsed.Name = "labelElapsed";
+      this.labelElapsed.Size = new System.Drawing.Size(48, 13);
+      this.labelElapsed.TabIndex = 8;
+      this.labelElapsed.Text = "Elapsed:";
+      this.labelElapsed.MouseHover += new System.EventHandler(this.labelElapsed_MouseHover);
+      // 
+      // label3
+      // 
+      this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(14, 440);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(31, 13);
+      this.label3.TabIndex = 10;
+      this.label3.Text = "Yres:";
+      // 
+      // numericYres
+      // 
+      this.numericYres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.numericYres.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+      this.numericYres.Location = new System.Drawing.Point(50, 439);
+      this.numericYres.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+      this.numericYres.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+      this.numericYres.Name = "numericYres";
+      this.numericYres.Size = new System.Drawing.Size(71, 20);
+      this.numericYres.TabIndex = 11;
+      this.numericYres.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+      // 
+      // buttonRedraw
+      // 
+      this.buttonRedraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonRedraw.Location = new System.Drawing.Point(145, 435);
+      this.buttonRedraw.Name = "buttonRedraw";
+      this.buttonRedraw.Size = new System.Drawing.Size(121, 23);
+      this.buttonRedraw.TabIndex = 13;
+      this.buttonRedraw.Text = "Redraw";
+      this.buttonRedraw.UseVisualStyleBackColor = true;
+      this.buttonRedraw.Click += new System.EventHandler(this.buttonRedraw_Click);
+      // 
+      // buttonSave
+      // 
+      this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.buttonSave.Enabled = false;
+      this.buttonSave.Location = new System.Drawing.Point(598, 436);
+      this.buttonSave.Name = "buttonSave";
+      this.buttonSave.Size = new System.Drawing.Size(95, 23);
+      this.buttonSave.TabIndex = 14;
+      this.buttonSave.Text = "Save image";
+      this.buttonSave.UseVisualStyleBackColor = true;
+      this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+      // 
+      // textParam
+      // 
+      this.textParam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textParam.Location = new System.Drawing.Point(189, 406);
+      this.textParam.Name = "textParam";
+      this.textParam.Size = new System.Drawing.Size(504, 20);
+      this.textParam.TabIndex = 15;
+      this.textParam.Text = "1.0";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(712, 474);
+      this.Controls.Add(this.textParam);
+      this.Controls.Add(this.buttonSave);
+      this.Controls.Add(this.buttonRedraw);
+      this.Controls.Add(this.numericYres);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.labelElapsed);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.numericXres);
+      this.Controls.Add(this.panel1);
+      this.MinimumSize = new System.Drawing.Size(540, 240);
+      this.Name = "Form1";
+      this.Text = "099 lines";
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericXres)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericYres)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.NumericUpDown numericXres;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label labelElapsed;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.NumericUpDown numericYres;
+    private System.Windows.Forms.Button buttonRedraw;
+    private System.Windows.Forms.Button buttonSave;
+    private System.Windows.Forms.TextBox textParam;
+  }
+}
+
---
+++ 099lines/Lines.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using LineCanvas;
+using Utilities;
+
+namespace _099lines
+{
+  public class Lines
+  {
+    /// <summary>
+    /// Optional data initialization.
+    /// </summary>
+    public static void InitParams ( out int wid, out int hei, out string param, out string name )
+    {
+      wid   = 800;
+      hei   = 520;
+      param = "width=1.0,anti=true,objects=100,prob=0.95";
+      name  = "Josef Pelikán";
+    }
+
+    /// <summary>
+    /// Draw the image into the initialized Canvas object.
+    /// </summary>
+    /// <param name="c">Canvas ready for your drawing.</param>
+    /// <param name="param">Optional string parameter from the form.</param>
+    public static void Draw ( Canvas c, string param )
+    {
+      // !!!{{ TODO: put your drawing code here
+
+      // input params:
+      float penWidth = 1.0f;  // pen width
+      bool antialias = true;  // use anti-aliasing?
+      bool explicitAntialias = false;
+      int objects = 100;      // number of randomly generated objects (squares, stars, Brownian particles)
+      int hatches = 12;       // number of hatch-lines for the squares
+      double prob = 0.95;     // continue-probability for the Brownian motion simulator
+
+      Dictionary<string, string> p = Util.ParseKeyValueList( param );
+      if ( p.Count > 0 )
+      {
+        // with=<line-width>
+        if ( Util.TryParse( p, "width", ref penWidth ) )
+        {
+          if ( penWidth < 0.0f )
+            penWidth = 0.0f;
+        }
+
+        // anti=<bool>
+        if ( Util.TryParse( p, "anti", ref antialias ) )
+          explicitAntialias = true;
+
+        // squares=<number>
+        if ( Util.TryParse( p, "objects", ref objects ) &&
+             objects < 0 )
+          objects = 0;
+
+        // hatches=<number>
+        if ( Util.TryParse( p, "hatches", ref hatches ) &&
+             hatches < 1 )
+          hatches = 1;
+
+        // prob=<probability>
+        if ( Util.TryParse( p, "prob", ref prob ) &&
+             prob > 0.999 )
+          prob = 0.999;
+      }
+
+      int wq = c.Width / 4;
+      int hq = c.Height / 4;
+      int wh = wq + wq;
+      int hh = hq + hq;
+      int minh = Math.Min( wh, hh );
+      double t;
+      int i, j;
+      double cx, cy, angle, x, y;
+
+      c.Clear( Color.Black );
+
+      // 1st quadrant - star
+      c.SetPenWidth( penWidth );
+      c.SetAntiAlias( explicitAntialias && antialias );
+
+      const int MAX_LINES = 30;
+      for ( i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES )
+      {
+        c.SetColor( Color.FromArgb( (i * 255) / MAX_LINES, 255, 255 - (i * 255) / MAX_LINES ) ); // [0,255,255] -> [255,255,0]
+        c.Line( t * wh, 0, wh - t * wh, hh );
+      }
+      for ( i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES )
+      {
+        c.SetColor( Color.FromArgb( 255, 255 - (i * 255) / MAX_LINES, (i * 255) / MAX_LINES ) ); // [255,255,0] -> [255,0,255]
+        c.Line( 0, hh - t * hh, wh, t * hh );
+      }
+
+      // 2nd quadrant - random hatched squares
+      double size = minh / 10.0;
+      double padding = size * Math.Sqrt( 0.5 );
+      c.SetColor( Color.LemonChiffon );
+      c.SetPenWidth( 1.0f );
+      Random r = new Random( 12 );
+
+      for ( i = 0; i < objects; i++ )
+      {
+        do
+          cx = r.NextDouble() * wh;
+        while ( cx < padding ||
+                cx > wh - padding );
+        c.SetAntiAlias( cx > wq );
+        cx += wh;
+
+        do
+          cy = r.NextDouble() * hh;
+        while ( cy < padding ||
+                cy > hh - padding );
+
+        angle = r.NextDouble() * Math.PI;
+
+        double dirx = Math.Sin( angle ) * size * 0.5;
+        double diry = Math.Cos( angle ) * size * 0.5;
+        cx -= dirx - diry;
+        cy -= diry + dirx;
+        double dx = -diry * 2.0 / hatches;
+        double dy = dirx * 2.0 / hatches;
+        double linx = dirx + dirx;
+        double liny = diry + diry;
+
+        for ( j = 0; j++ < hatches; cx += dx, cy += dy )
+          c.Line( cx, cy, cx + linx, cy + liny );
+      }
+
+      // 3rd quadrant - random stars
+      c.SetColor( Color.LightCoral );
+      c.SetPenWidth( penWidth );
+      size = minh / 16.0;
+      padding = size;
+      const int MAX_SIDES = 30;
+      List<PointF> v = new List<PointF>( MAX_SIDES + 1 );
+
+      for ( i = 0; i < objects; i++ )
+      {
+        do
+          cx = r.NextDouble() * wh;
+        while ( cx < padding ||
+                cx > wh - padding );
+        c.SetAntiAlias( cx > wq );
+
+        do
+          cy = r.NextDouble() * hh;
+        while ( cy < padding ||
+                cy > hh - padding );
+        cy += hh;
+
+        int sides = r.Next( 3, MAX_SIDES );
+        double dAngle = Math.PI * 2.0 / sides;
+
+        v.Clear();
+        angle = 0.0;
+
+        for ( j = 0; j++ < sides; angle += dAngle )
+        {
+          double rad = size * (0.1 + 0.9 * r.NextDouble());
+          x = cx + rad * Math.Sin( angle );
+          y = cy + rad * Math.Cos( angle );
+          v.Add( new PointF( (float)x, (float)y ) );
+        }
+        v.Add( v[ 0 ] );
+        c.PolyLine( v );
+      }
+
+      // 4th quadrant - Brownian motion
+      c.SetPenWidth( penWidth );
+      c.SetAntiAlias( true );
+      size = minh / 10.0;
+      padding = size;
+
+      for ( i = 0; i < objects; i++ )
+      {
+        do
+          x = r.NextDouble() * wh;
+        while ( x < padding ||
+                x > wh - padding );
+
+        do
+          y = r.NextDouble() * hh;
+        while ( y < padding ||
+                y > hh - padding );
+
+        c.SetColor( Color.FromArgb( 127 + r.Next( 0, 128 ),
+                                    127 + r.Next( 0, 128 ),
+                                    127 + r.Next( 0, 128 ) ) );
+
+        for ( j = 0; j++ < 1000; )
+        {
+          angle = r.NextDouble() * Math.PI * 2.0;
+          double rad = size * r.NextDouble();
+          cx = x + rad * Math.Sin( angle );
+          cy = y + rad * Math.Cos( angle );
+          if ( cx < 0.0 || cx > wh ||
+               cy < 0.0 || cy > hh )
+            break;
+
+          c.Line( x + wh, y + hh, cx + wh, cy + hh );
+          x = cx; y = cy;
+          if ( r.NextDouble() > prob )
+            break;
+        }
+      }
+
+      // !!!}}
+    }
+  }
+}
