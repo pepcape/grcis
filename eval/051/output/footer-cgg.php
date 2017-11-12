3fa803babc028c1657110ffc2e2fe9af823091c5
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2017-11-12 23:31:02 +0000

    eval/051: colormap size.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@545 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- eval/051/output/footer-cgg.php
+++ eval/051/output/footer-cgg.php
@@ -1,5 +1,5 @@
 <hr class="h2" />
-<p>Copyright (C) 2016 <a href="http://cgg.mff.cuni.cz/~pepca/">J.Pelikán</a>,
+<p>Copyright (C) 2016-2017 <a href="http://cgg.mff.cuni.cz/~pepca/">J.Pelikán</a> (excluding images),
 <?php
   $SvnDate='$Date$';
   echo 'last change: '.substr($SvnDate,7,strlen($SvnDate)-9);
