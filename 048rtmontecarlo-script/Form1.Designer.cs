35e429783035229d0f2426acbeba1acdd17bcff0
vozarj <vozarj@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2018-04-02 00:08:10 +0000

    New form New class - AdvancedTools
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/branches/rt+@588 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 048rtmontecarlo-script/Form1.Designer.cs
+++ 048rtmontecarlo-script/Form1.Designer.cs
@@ -47,6 +47,7 @@
       this.buttonRes = new System.Windows.Forms.Button();
       this.label3 = new System.Windows.Forms.Label();
       this.textParam = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
       this.panel1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericSupersampling)).BeginInit();
@@ -283,11 +284,23 @@
       this.textParam.TabIndex = 44;
       this.textParam.TextChanged += new System.EventHandler(this.textParam_TextChanged);
       // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.Location = new System.Drawing.Point(505, 447);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(91, 23);
+      this.button1.TabIndex = 45;
+      this.button1.Text = "Advanced";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(712, 482);
+      this.Controls.Add(this.button1);
       this.Controls.Add(this.textParam);
       this.Controls.Add(this.label3);
       this.Controls.Add(this.buttonRes);
@@ -340,6 +353,7 @@
     private System.Windows.Forms.Button buttonRes;
     private System.Windows.Forms.Label label3;
     private System.Windows.Forms.TextBox textParam;
+    private System.Windows.Forms.Button button1;
   }
 }
 
