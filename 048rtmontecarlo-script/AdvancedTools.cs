52ed1ac9c96b09579183e79ff0a86e4a12eec9c6
jerry411 <jarkovozar@gmail.com>
2018-04-05 20:29:49 +0000

    Tried strong genericity for arithmetic operations - too slow
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/branches/rt+@597 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 048rtmontecarlo-script/AdvancedTools.cs
+++ 048rtmontecarlo-script/AdvancedTools.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using MathSupport;
@@ -81,16 +82,34 @@ namespace _048rtmontecarlo
 
       internal static double[,] depthMap;
 
+      private static Bitmap depthMapBitmap;
+
       public static void RenderDepthMap()
       {
-        
+        if (DepthMapImageWidth == 0 || DepthMapImageHeight == 0)
+        {
+          Initialize();
+        }
 
-        Bitmap depthMapBitmap = new Bitmap(DepthMapImageWidth, DepthMapImageHeight, PixelFormat.Format24bppRgb);
+        double maxDepth = Double.MinValue;
+        double minDepth = Double.MaxValue;
+
+        GetMinimumAndMaximum(ref minDepth, ref maxDepth, depthMap);
+
+        depthMapBitmap = new Bitmap(DepthMapImageWidth, DepthMapImageHeight, PixelFormat.Format24bppRgb);
+
+        MapAppropriateColors ( minDepth, maxDepth, depthMap, depthMapBitmap);
       }
+     
 
       public static Bitmap GetBitmap ()
       {
-        throw new NotImplementedException();
+        if (depthMapBitmap == null)
+        {
+          RenderDepthMap();
+        }
+
+        return depthMapBitmap;
       }
     }
 
@@ -114,9 +133,6 @@ namespace _048rtmontecarlo
       /// </summary>
       public static int IntensityMapImageHeight;
 
-      private static Color highIntensity = Color.Red;
-      private static Color lowIntensity = Color.Blue;
-
       internal static int[,] intensityMap;
 
       private static Bitmap intensityMapBitmap;
@@ -128,31 +144,22 @@ namespace _048rtmontecarlo
           Initialize ();
         }
 
-        int maxIntensity = 0;
-        int minIntensity = int.MaxValue;
+        int maxIntensity = Int32.MinValue;
+        int minIntensity = Int32.MaxValue;
 
-        // get maximal and minimal intensity - later used to get appropriate color for each intensity
-        for ( int x = 0; x < IntensityMapImageWidth; x++ )
-        {
-          for ( int y = 0; y < IntensityMapImageHeight; y++ )
-          {
-            if ( intensityMap[ x, y ] > maxIntensity )
-              maxIntensity = intensityMap[ x, y ];
-
-            if (intensityMap[x, y] < minIntensity)
-              minIntensity = intensityMap[x, y];
-          }
-        }
+        GetMinimumAndMaximum ( ref minIntensity, ref maxIntensity, intensityMap );
 
         intensityMapBitmap = new Bitmap(IntensityMapImageWidth, IntensityMapImageHeight, PixelFormat.Format24bppRgb);
 
-        for (int x = 0; x < IntensityMapImageWidth; x++)
+        MapAppropriateColors ( minIntensity, maxIntensity, intensityMap, intensityMapBitmap );
+
+        /*for (int x = 0; x < IntensityMapImageWidth; x++)
         {
           for (int y = 0; y < IntensityMapImageHeight; y++)
           {
             intensityMapBitmap.SetPixel ( x, y, GetAppropriateColor( minIntensity, maxIntensity, intensityMap[ x,y ] ));
           }
-        }              
+        }  */
       }      
 
       public static Bitmap GetBitmap()
@@ -166,9 +173,52 @@ namespace _048rtmontecarlo
       }
     }
 
-    private static Color GetAppropriateColor(double minValue, double maxValue, double newValue)
+
+    /// <summary>
+    /// Returns minimal and maximal values found in a specific map
+    /// Values are returned via reference and must be of type IComparable
+    /// </summary>
+    /// <typeparam name="T">Type IComparable</typeparam>
+    /// <param name="minValue">Must be initially set to max value of corresponding type</param>
+    /// <param name="maxValue">Must be initially set to min value of corresponding type</param>
+    /// <param name="map">2D array of values</param>
+    private static void GetMinimumAndMaximum<T> ( ref T minValue, ref T maxValue, T[,] map ) where T: IComparable
     {
-      double colorValue = (newValue - minValue) / (maxValue - minValue) * 240;
+      for (int x = 0; x < DepthMap.DepthMapImageWidth; x++)
+      {
+        for (int y = 0; y < DepthMap.DepthMapImageHeight; y++)
+        {
+          if ( map[ x, y ].CompareTo ( maxValue ) > 0 )
+            maxValue = map[ x, y ];
+
+          if ( map[ x, y ].CompareTo ( maxValue ) < 0 )
+            minValue = map[ x, y ];
+
+
+          /*if (map[x, y] > maxValue)
+            maxValue = map[x, y];
+
+          if (map[x, y] < minValue)
+            minValue = map[x, y];*/
+        }
+      }
+    }
+
+    /// <summary>
+    /// Returns color based on range
+    /// Returned color is either dark blue (close to minValue) or red (close to maxValue)
+    /// Between than color is lineary transited changing value in HSV model
+    /// Dark blue -> light blue -> turquoise -> green -> yellow -> orange -> red (does not go to purple)
+    /// </summary>
+    /// <param name="minValue">Start of range (dark blue color)</param>
+    /// <param name="maxValue">End of range (red color)</param>
+    /// <param name="newValue">Value for which we want color</param>
+    /// <returns></returns>
+    private static Color GetAppropriateColor<T> ( T minValue, T maxValue, T newValue )
+    {
+      dynamic colorValue = Divide ( Subtract ( newValue, minValue ), Subtract ( maxValue, minValue ) );
+      colorValue *= 240;
+      //double colorValue = (newValue - minValue) / (maxValue - minValue) * 240;
 
       if (double.IsNaN(colorValue) || double.IsInfinity(colorValue))
       {
@@ -178,13 +228,47 @@ namespace _048rtmontecarlo
       return Arith.HSVToColor(240 - colorValue, 1, 1);
     }
 
+    private static void MapAppropriateColors<T> ( T minValue, T maxValue, T[,] map, Bitmap bitmap )
+    {
+      for ( int x = 0; x < DepthMap.DepthMapImageWidth; x++ )
+      {
+        for ( int y = 0; y < DepthMap.DepthMapImageHeight; y++ )
+        {
+          bitmap.SetPixel(x, y, GetAppropriateColor( minValue, maxValue, map[x, y]));
+        }
+      }
+    }
 
+    static T Subtract<T>(T a, T b)
+    {
+      ParameterExpression paramA = Expression.Parameter(typeof(T), "a"),
+                          paramB = Expression.Parameter(typeof(T), "b");
+      BinaryExpression body = Expression.Subtract(paramA, paramB);
+      Func<T, T, T> subtract = Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+      return subtract(a, b);
+    }
+
+    static T Divide<T>(T a, T b)
+    {
+      ParameterExpression paramA = Expression.Parameter(typeof(T), "a"),
+                          paramB = Expression.Parameter(typeof(T), "b");
+      BinaryExpression body     = Expression.Divide(paramA, paramB);
+      Func<T, T, T> divide = Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+      return divide(a, b);
+    }
+
+    /// <summary>
+    /// Calls Initialize method of all subclasses in AdvancedTools
+    /// </summary>
     public static void SetNewDimensions ()
     {
       DepthMap.Initialize ();
       IntensityMap.Initialize ();
     }
 
+    /// <summary>
+    /// Removes all maps and unnecessary stuff
+    /// </summary>
     public static void NewRenderInitialization ()
     {
       DepthMap.depthMap = null;
