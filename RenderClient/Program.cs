39f0b961fba1e70575f55048b13beac6a30711e3
vozarj <vozarj@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2018-07-02 01:07:53 +0000

    Added a bunch of comments
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/branches/rt+@653 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- RenderClient/Program.cs
+++ RenderClient/Program.cs
@@ -25,7 +25,9 @@ namespace RenderClient
     }
   }
 
-
+  /// <summary>
+  /// Takes care of connection and communication with server
+  /// </summary>
   static class RenderClient
   {
     private static IPAddress     ipAdr;
@@ -36,6 +38,10 @@ namespace RenderClient
     private static IRayScene scene;
     private static IRenderer renderer;
 
+    /// <summary>
+    /// TcpListener is used even though this is in fact a client, not a server - this is done so that the real server can connect to the client
+    /// (and not otherwise as usually)
+    /// </summary>
     public static void ConnectToServer ()
     {
       Console.WriteLine ( "Waiting for remote server to connect to this client..." );
@@ -54,6 +60,32 @@ namespace RenderClient
       Console.WriteLine ( "Client succesfully connected." );
     }
 
+    /// <summary>
+    /// Serializes and sends object through NetworkStream
+    /// </summary>
+    /// <typeparam name="T">Type of object - must be marked as [Serializable], as well as all his recursive dependencies</typeparam>
+    /// <param name="objectToSend">Instance of actual object to send</param>
+    private static void SendObject<T> ( T objectToSend )
+    {
+      BinaryFormatter formatter    = new BinaryFormatter ();
+      MemoryStream    memoryStream = new MemoryStream ();
+
+      formatter.Serialize ( memoryStream, objectToSend );
+
+      byte[] dataBuffer = memoryStream.ToArray ();
+
+      client.SendBufferSize = dataBuffer.Length;
+
+      stream.Write ( dataBuffer, 0, dataBuffer.Length );
+
+      Console.WriteLine ( "Data for {0} serialized and sent to server.", typeof ( T ).Name );
+    }
+
+    /// <summary>
+    /// Receives object from NetworkStream and deserializes it
+    /// </summary>
+    /// <typeparam name="T">Type of object - must be marked as [Serializable], as well as all his recursive dependencies</typeparam>
+    /// <returns>Instance of actual received object</returns>
     public static T ReceiveObject<T> ()
     {
       byte[] dataBuffer = new byte[client.ReceiveBufferSize];
@@ -71,6 +103,9 @@ namespace RenderClient
       return receivedObject;
     }
 
+    /// <summary>
+    /// For DEBUG only
+    /// </summary>
     public static void TestSend ()
     {
       byte[] bytes = Encoding.ASCII.GetBytes ( "This is test message." );
@@ -83,6 +118,11 @@ namespace RenderClient
       client.Close ();
     }
 
+    /// <summary>
+    /// Receives all objects which are necessary from render server
+    /// IRayScene - scene representation like solids, textures, lights, camera, ...
+    /// IRenderer - renderer itself including IImageFunction; needed for RenderPixel method
+    /// </summary>
     public static void ReceiveNecessaryObjects ()
     {
       scene = ReceiveObject<IRayScene> ();
@@ -93,6 +133,10 @@ namespace RenderClient
       Console.ReadLine ();  // For Debug Only
     }
 
+    /// <summary>
+    /// For DEBUG purposes and to remove some problems with NetworkStream
+    /// Simply sends byte "1"
+    /// </summary>
     private static void SendConfirmation ()
     {
       stream.WriteByte ( 1 );
