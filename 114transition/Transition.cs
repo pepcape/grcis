7b4ada90f7205014972ee42946b9b308c6f98473
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2018-09-30 22:39:40 +0000

    114transition: 1st working version.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@674 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 114transition/Transition.cs
+++ 114transition/Transition.cs
@@ -1,102 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿using System.Collections.Generic;
 using MathSupport;
-using Raster;
 using Utilities;
 
 namespace _114transition
 {
-  class Transition
+  public class Transition
   {
     /// <summary>
-    /// Optional data initialization.
+    /// Form initialization.
     /// </summary>
-    /// <param name="param">Optinal text parameter from the form's text-field.</param>
     /// <param name="name">Your first-name and last-name.</param>
-    public static void InitParams ( out string param, out string name )
+    /// <param name="param">Optinal text to initialize the form's text-field.</param>
+    public static void InitParams ( out string name, out string param )
     {
-      param = "gamma=2.5";
-      name  = "pilot";
+      // {{
+      name = "Josef Pelikán";
+      param = "morph=false";
+      // }}
+    }
+
+    //==========================================================================
+    //  Transition instance
+
+    /// <summary>
+    /// Image width in pixels.
+    /// </summary>
+    protected int width;
+
+    /// <summary>
+    /// Image height in pixels.
+    /// </summary>
+    protected int height;
+
+    /// <summary>
+    /// True if the MorphingFunction should be used, false for the BlendingFunction.
+    /// </summary>
+    protected bool useMorphing;
+
+    public Transition ( int wid, int hei, string param )
+    {
+      Reset( wid, hei, param );
     }
 
     /// <summary>
-    /// Tone-mapping of the input HDR image.
+    /// Reset the Transition object instance.
     /// </summary>
-    /// <param name="input">Mandatory HDR image.</param>
-    /// <param name="result">Optional (pre-allocatied) output LDR image.</param>
-    /// <param name="param">Optional text parameters (from form's text-field).</param>
-    /// <returns>Output LDR image (use pre-allocated 'result' if possible).</returns>
-    public static unsafe Bitmap ToneMap ( FloatImage input, Bitmap result, string param )
+    /// <param name="wid">New image width in pixels.</param>
+    /// <param name="hei">New image height in pixels.</param>
+    /// <param name="param">Optional string parameter (its content and format is entierely up to you).</param>
+    public void Reset ( int wid, int hei, string param )
     {
-      if ( input == null )
-        return null;
+      width  = wid;
+      height = hei;
 
-      // !!!{{ TODO: write your own tone-mapping reduction code here
+      // {{
 
-      // custom parameters from the text-field:
+      // !!! TODO: process the param string if you need !!!
       Dictionary<string, string> p = Util.ParseKeyValueList( param );
-      double g = 0.0;
       if ( p.Count > 0 )
       {
-        // gamma=<float-number>
-        if ( Util.TryParse( p, "gamma", ref g ) &&
-             g > 0.001 &&
-             Math.Abs( g - 1.0 ) > 0.001 )
-          g = 1.0 / g;
-        else
-          g = 0.0;
+        // morph=<bool>
+        if ( !Util.TryParse( p, "morph", ref useMorphing ) )
+          useMorphing = false;
+
+        // ... you can add more parameters here ...
       }
 
-      int width  = input.Width;
-      int height = input.Height;
+      // }}
+    }
 
-      // re-allocate the output image if necessary:
-      if ( result == null ||
-           result.Width != width ||
-           result.Height != height ||
-           result.PixelFormat != PixelFormat.Format24bppRgb )
-        result = new Bitmap( width, height, PixelFormat.Format24bppRgb );
+    /// <summary>
+    /// Do we use morphing (true) or blending (false)?
+    /// </summary>
+    public bool UseMorphing ()
+    {
+      return useMorphing;
+    }
 
-      // input range:
-      double minY, maxY;
-      input.Contrast( out minY, out maxY );
-      double coef = 1.0 / maxY;   // the whole range is mapped into (0.0,1.0>
+    /// <summary>
+    /// Simple image blending w/o geometric transform.
+    /// </summary>
+    /// <param name="t">Time variable (0.0 to 1.0).</param>
+    /// <param name="x">Horizontal pixel coordinate.</param>
+    /// <param name="y">Vertical pixel coordinate.</param>
+    /// <returns>0.0 for the input1 value, 1.0 for the input2 (and anything in between).</returns>
+    public double BlendingFunction (
+      double t, int x, int y )
+    {
+      // {{
 
-      BitmapData dataOut = result.LockBits( new Rectangle( 0, 0, width, height ), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb );
-      fixed ( float* id = input.Data )
-      {
-        float* iptr;
-        byte* optr;
-        int dO = Image.GetPixelFormatSize( PixelFormat.Format24bppRgb ) / 8;    // BGR
+      // !!! TODO: modify this function if you will be using simple blending !!!
+      return Util.Clamp( t, 0.0, 1.0 );
 
-        for ( int y = 0; y < height; y++ )
-        {
-          iptr = id + input.Scan0 + y * input.Stride;
-          optr = (byte*)dataOut.Scan0 + y * dataOut.Stride;
+      // }}
+    }
 
-          if ( g > 0.0 )
-            for ( int x = 0; x++ < width; iptr += input.Channels, optr += dO )
-            {
-              optr[ 2 ] = (byte)Arith.Clamp( 255.999 * Math.Pow( iptr[ 0 ] * coef, g ), 0.0, 255.0 );
-              optr[ 1 ] = (byte)Arith.Clamp( 255.999 * Math.Pow( iptr[ 1 ] * coef, g ), 0.0, 255.0 );
-              optr[ 0 ] = (byte)Arith.Clamp( 255.999 * Math.Pow( iptr[ 2 ] * coef, g ), 0.0, 255.0 );
-            }
-          else
-            for ( int x = 0; x++ < width; iptr += input.Channels, optr += dO )
-            {
-              optr[ 2 ] = (byte)Arith.Clamp( 255.999 * iptr[ 0 ] * coef, 0.0, 255.0 );
-              optr[ 1 ] = (byte)Arith.Clamp( 255.999 * iptr[ 1 ] * coef, 0.0, 255.0 );
-              optr[ 0 ] = (byte)Arith.Clamp( 255.999 * iptr[ 2 ] * coef, 0.0, 255.0 );
-            }
-        }
-      }
-      result.UnlockBits( dataOut );
+    /// <summary>
+    /// Image morphing: every time a blend of arbitrary input1 and input2 pixels can be used.
+    /// </summary>
+    /// <param name="t">Time variable (0.0 to 1.0).</param>
+    /// <param name="x">Horizontal target pixel coordinate.</param>
+    /// <param name="y">Vertical target pixel coordinate.</param>
+    /// <param name="srcX1">Input1 source pixel horizontal coordinate.</param>
+    /// <param name="srcY1">Input1 source pixel vertical coordinate.</param>
+    /// <param name="srcX2">Input2 source pixel horizontal coordinate.</param>
+    /// <param name="srcY2">Input2 source pixel vertical coordinate.</param>
+    /// <param name="blend">Blend factor: 0.0 for input1, 1.0 for input2 (and anything in between).</param>
+    public void MorphingFunction (
+      double t, int x, int y,
+      out double srcX1, out double srcY1,
+      out double srcX2, out double srcY2,
+      out double blend )
+    {
+      // {{
 
-      // !!!}}
+      // !!! TODO: modify this function if you will be using morphing !!!
 
-      return result;
+      // Default implementation - blending.
+      srcX1 = srcX2 = x;
+      srcY1 = srcY2 = y;
+      blend = Util.Clamp( t, 0.0, 1.0 );
+
+      // }}
     }
   }
 }
