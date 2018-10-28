925df0c74fb1c81b4a957dcb79036612cced2671
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2018-10-28 23:24:54 +0000

    082circles task update (only discs could be used..).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@711 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 082circles/Form1.cs
+++ 082circles/Form1.cs
@@ -4,17 +4,28 @@ using System.Drawing;
 using System.Globalization;
 using System.Windows.Forms;
 using CircleCanvas;
+using Utilities;
 
 namespace _082circles
 {
   public partial class Form1 : Form
   {
-    static readonly string rev = "$Rev$".Split( ' ' )[ 1 ];
+    static readonly string rev = Util.SetVersion( "$Rev$" );
 
     /// <summary>
     /// Current computed image.
     /// </summary>
     protected Bitmap outputImage = null;
+
+    /// <summary>
+    /// Param string tooltip = help.
+    /// </summary>
+    string tooltip = "";
+
+    /// <summary>
+    /// Shared ToolTip instance.
+    /// </summary>
+    ToolTip tt = new ToolTip();
 
     public Form1 ()
     {
@@ -22,13 +33,13 @@ namespace _082circles
 
       // custom init:
       int width, height;
-      string param;
-      Circles.InitParams( out width, out height, out param );
+      string param, name;
+      Circles.InitParams( out name, out width, out height, out param, out tooltip );
       numericXres.Value = Math.Max( width, 10 );
       numericYres.Value = Math.Max( height, 10 );
       textParam.Text = param ?? "";
 
-      Text += " (rev: " + rev + ')';
+      Text += " (" + rev + ") '" + name + '\'';
     }
 
     private void setImage ( ref Bitmap bakImage, Bitmap newImage )
@@ -39,9 +50,9 @@ namespace _082circles
       bakImage = newImage;
     }
 
-    private void buttonRedraw_Click ( object sender, EventArgs e )
+    private void redraw ()
     {
-      int width  = (int)numericXres.Value;
+      int width = (int)numericXres.Value;
       int height = (int)numericYres.Value;
 
       buttonSave.Enabled = false;
@@ -56,10 +67,15 @@ namespace _082circles
       sw.Stop();
       float elapsed = 0.001f * sw.ElapsedMilliseconds;
 
-      labelElapsed.Text  = string.Format( CultureInfo.InvariantCulture, "Elapsed: {0:f3}s", elapsed );
+      labelElapsed.Text = string.Format( CultureInfo.InvariantCulture, "Elapsed: {0:f3}s", elapsed );
       setImage( ref outputImage, newImage );
 
       buttonSave.Enabled = true;
+    }
+
+    private void buttonRedraw_Click ( object sender, EventArgs e )
+    {
+      redraw();
     }
 
     private void buttonSave_Click ( object sender, EventArgs e )
@@ -76,6 +92,26 @@ namespace _082circles
         return;
 
       outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+    }
+
+    private void labelElapsed_MouseHover ( object sender, EventArgs e )
+    {
+      tt.Show( Util.TargetFramework + " (" + Util.RunningFramework + ')',
+               (IWin32Window)sender, 10, -25, 2000 );
+    }
+
+    private void textParam_MouseHover ( object sender, EventArgs e )
+    {
+      tt.Show( tooltip, (IWin32Window)sender, 10, -25, 4000 );
+    }
+
+    private void textParam_KeyPress ( object sender, KeyPressEventArgs e )
+    {
+      if ( e.KeyChar == (char)Keys.Enter )
+      {
+        e.Handled = true;
+        redraw();
+      }
     }
   }
 }
