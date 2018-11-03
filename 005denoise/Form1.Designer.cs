abab5c50979c3909a63360c39eff9e6d53b2a093
pepcape <pepcape@gmail.com>
2018-11-03 16:24:54 +0000

    005 refactor II (modern filtering system, tooltip, bitmap locking..).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@722 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 005denoise/Form1.Designer.cs
+++ 005denoise/Form1.Designer.cs
@@ -32,117 +32,139 @@
       this.pictureBox1 = new System.Windows.Forms.PictureBox();
       this.buttonOpen = new System.Windows.Forms.Button();
       this.buttonSave = new System.Windows.Forms.Button();
-      this.numericParam = new System.Windows.Forms.NumericUpDown();
       this.label1 = new System.Windows.Forms.Label();
-      this.buttonRecalc = new System.Windows.Forms.Button();
+      this.buttonRedraw = new System.Windows.Forms.Button();
+      this.textParam = new System.Windows.Forms.TextBox();
+      this.buttonStop = new System.Windows.Forms.Button();
+      this.labelElapsed = new System.Windows.Forms.Label();
       this.panel1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericParam)).BeginInit();
       this.SuspendLayout();
       // 
       // panel1
       // 
-      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                  | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
       this.panel1.AutoScroll = true;
       this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.panel1.Controls.Add( this.pictureBox1 );
-      this.panel1.Location = new System.Drawing.Point( 13, 13 );
+      this.panel1.Controls.Add(this.pictureBox1);
+      this.panel1.Location = new System.Drawing.Point(13, 13);
       this.panel1.Name = "panel1";
-      this.panel1.Size = new System.Drawing.Size( 680, 380 );
+      this.panel1.Size = new System.Drawing.Size(883, 380);
       this.panel1.TabIndex = 0;
       // 
       // pictureBox1
       // 
-      this.pictureBox1.Location = new System.Drawing.Point( 0, 0 );
+      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
       this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size( 680, 380 );
+      this.pictureBox1.Size = new System.Drawing.Size(883, 380);
       this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
       this.pictureBox1.TabIndex = 2;
       this.pictureBox1.TabStop = false;
+      this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+      this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
       // 
       // buttonOpen
       // 
       this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.buttonOpen.Location = new System.Drawing.Point( 13, 411 );
+      this.buttonOpen.Location = new System.Drawing.Point(13, 408);
       this.buttonOpen.Name = "buttonOpen";
-      this.buttonOpen.Size = new System.Drawing.Size( 130, 23 );
+      this.buttonOpen.Size = new System.Drawing.Size(83, 23);
       this.buttonOpen.TabIndex = 1;
-      this.buttonOpen.Text = "Open image";
+      this.buttonOpen.Text = "Load image";
       this.buttonOpen.UseVisualStyleBackColor = true;
-      this.buttonOpen.Click += new System.EventHandler( this.buttonOpen_Click );
+      this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
       // 
       // buttonSave
       // 
       this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.buttonSave.Location = new System.Drawing.Point( 563, 411 );
+      this.buttonSave.Location = new System.Drawing.Point(813, 408);
       this.buttonSave.Name = "buttonSave";
-      this.buttonSave.Size = new System.Drawing.Size( 130, 23 );
-      this.buttonSave.TabIndex = 2;
+      this.buttonSave.Size = new System.Drawing.Size(83, 23);
+      this.buttonSave.TabIndex = 3;
       this.buttonSave.Text = "Save image";
       this.buttonSave.UseVisualStyleBackColor = true;
-      this.buttonSave.Click += new System.EventHandler( this.buttonSave_Click );
-      // 
-      // numericParam
-      // 
-      this.numericParam.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.numericParam.DecimalPlaces = 3;
-      this.numericParam.Increment = new decimal( new int[] {
-            1,
-            0,
-            0,
-            65536} );
-      this.numericParam.Location = new System.Drawing.Point( 222, 414 );
-      this.numericParam.Maximum = new decimal( new int[] {
-            10,
-            0,
-            0,
-            0} );
-      this.numericParam.Name = "numericParam";
-      this.numericParam.Size = new System.Drawing.Size( 90, 20 );
-      this.numericParam.TabIndex = 3;
+      this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
       // 
       // label1
       // 
       this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point( 161, 416 );
+      this.label1.Location = new System.Drawing.Point(108, 413);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size( 58, 13 );
-      this.label1.TabIndex = 4;
-      this.label1.Text = "Parameter:";
+      this.label1.Size = new System.Drawing.Size(40, 13);
+      this.label1.TabIndex = 5;
+      this.label1.Text = "Param:";
       // 
-      // buttonRecalc
+      // buttonRedraw
       // 
-      this.buttonRecalc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.buttonRecalc.Location = new System.Drawing.Point( 337, 412 );
-      this.buttonRecalc.Name = "buttonRecalc";
-      this.buttonRecalc.Size = new System.Drawing.Size( 110, 23 );
-      this.buttonRecalc.TabIndex = 5;
-      this.buttonRecalc.Text = "Recalculate";
-      this.buttonRecalc.UseVisualStyleBackColor = true;
-      this.buttonRecalc.Click += new System.EventHandler( this.buttonRecalc_Click );
+      this.buttonRedraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonRedraw.Location = new System.Drawing.Point(387, 408);
+      this.buttonRedraw.Name = "buttonRedraw";
+      this.buttonRedraw.Size = new System.Drawing.Size(84, 23);
+      this.buttonRedraw.TabIndex = 6;
+      this.buttonRedraw.Text = "Recompute";
+      this.buttonRedraw.UseVisualStyleBackColor = true;
+      this.buttonRedraw.Click += new System.EventHandler(this.buttonRedraw_Click);
+      // 
+      // textParam
+      // 
+      this.textParam.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.textParam.Location = new System.Drawing.Point(159, 410);
+      this.textParam.Name = "textParam";
+      this.textParam.Size = new System.Drawing.Size(213, 20);
+      this.textParam.TabIndex = 7;
+      this.textParam.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textParam_KeyPress);
+      this.textParam.MouseHover += new System.EventHandler(this.textParam_MouseHover);
+      // 
+      // buttonStop
+      // 
+      this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonStop.Enabled = false;
+      this.buttonStop.Location = new System.Drawing.Point(487, 408);
+      this.buttonStop.Name = "buttonStop";
+      this.buttonStop.Size = new System.Drawing.Size(56, 23);
+      this.buttonStop.TabIndex = 8;
+      this.buttonStop.Text = "Stop";
+      this.buttonStop.UseVisualStyleBackColor = true;
+      this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+      // 
+      // labelElapsed
+      // 
+      this.labelElapsed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.labelElapsed.AutoSize = true;
+      this.labelElapsed.Location = new System.Drawing.Point(560, 413);
+      this.labelElapsed.Name = "labelElapsed";
+      this.labelElapsed.Size = new System.Drawing.Size(48, 13);
+      this.labelElapsed.TabIndex = 9;
+      this.labelElapsed.Text = "Elapsed:";
+      this.labelElapsed.MouseHover += new System.EventHandler(this.labelElapsed_MouseHover);
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+      this.AllowDrop = true;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size( 712, 446 );
-      this.Controls.Add( this.buttonRecalc );
-      this.Controls.Add( this.label1 );
-      this.Controls.Add( this.numericParam );
-      this.Controls.Add( this.buttonSave );
-      this.Controls.Add( this.buttonOpen );
-      this.Controls.Add( this.panel1 );
-      this.MinimumSize = new System.Drawing.Size( 620, 200 );
+      this.ClientSize = new System.Drawing.Size(913, 443);
+      this.Controls.Add(this.labelElapsed);
+      this.Controls.Add(this.buttonStop);
+      this.Controls.Add(this.textParam);
+      this.Controls.Add(this.buttonRedraw);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.buttonSave);
+      this.Controls.Add(this.buttonOpen);
+      this.Controls.Add(this.panel1);
+      this.MinimumSize = new System.Drawing.Size(850, 200);
       this.Name = "Form1";
       this.Text = "005 denoise";
-      this.panel1.ResumeLayout( false );
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+      this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+      this.panel1.ResumeLayout(false);
       this.panel1.PerformLayout();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericParam)).EndInit();
-      this.ResumeLayout( false );
+      this.ResumeLayout(false);
       this.PerformLayout();
 
     }
@@ -153,9 +175,11 @@
     private System.Windows.Forms.PictureBox pictureBox1;
     private System.Windows.Forms.Button buttonOpen;
     private System.Windows.Forms.Button buttonSave;
-    private System.Windows.Forms.NumericUpDown numericParam;
     private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Button buttonRecalc;
+    private System.Windows.Forms.Button buttonRedraw;
+    private System.Windows.Forms.TextBox textParam;
+    private System.Windows.Forms.Button buttonStop;
+    private System.Windows.Forms.Label labelElapsed;
   }
 }
 
--- 005denoise/Form1.cs
+++ 005denoise/Form1.cs
@@ -1,28 +1,116 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Utilities;
 
 namespace _005denoise
 {
   public partial class Form1 : Form
   {
-    static readonly string rev = "$Rev$".Split( ' ' )[ 1 ];
+    static readonly string rev = Util.SetVersion( "$Rev$" );
 
     protected Bitmap inputImage = null;
     protected Bitmap outputImage = null;
 
+    /// <summary>
+    /// Window title prefix.
+    /// </summary>
+    protected string titlePrefix = null;
+
+    /// <summary>
+    /// Param string tooltip = help.
+    /// </summary>
+    string tooltip = "";
+
+    /// <summary>
+    /// Shared ToolTip instance.
+    /// </summary>
+    ToolTip tt = new ToolTip();
+
     public Form1 ()
     {
       InitializeComponent();
-      Text += " (rev: " + rev + ')';
+
+      string par, name;
+      Filter.InitParams( out name, out par, out tooltip );
+      textParam.Text = par;
+
+      titlePrefix = (Text += " (" + rev + ") '" + name + '\'');
     }
+
+    protected Thread aThread = null;
+
+    /// <summary>
+    /// User-break variable.
+    /// </summary>
+    volatile public static bool cont = true;
+
+    /// <summary>
+    /// Picture-box background color (for alpha-images).
+    /// </summary>
+    public static Color imageBoxBackground = Color.White;
 
     private void setImage ( ref Bitmap bakImage, Bitmap newImage )
     {
-      pictureBox1.Image = newImage;
       if ( bakImage != null )
         bakImage.Dispose();
       bakImage = newImage;
+    }
+
+    delegate void SetImageCallback ( Bitmap newImage );
+
+    protected void SetImage ( Bitmap newImage )
+    {
+      if ( pictureBox1.InvokeRequired )
+      {
+        SetImageCallback si = new SetImageCallback( SetImage );
+        BeginInvoke( si, new object[] { newImage } );
+      }
+      else
+      {
+        pictureBox1.Image = newImage;
+        pictureBox1.BackColor = imageBoxBackground;
+        setImage( ref outputImage, newImage );
+      }
+    }
+
+    delegate void SetTextCallback ( string text );
+
+    protected void SetText ( string text )
+    {
+      if ( labelElapsed.InvokeRequired )
+      {
+        SetTextCallback st = new SetTextCallback( SetText );
+        BeginInvoke( st, new object[] { text } );
+      }
+      else
+        labelElapsed.Text = text;
+    }
+
+    private void imageProbe ( int x, int y )
+    {
+      if ( outputImage == null )
+        return;
+
+      x = Util.Clamp( x, 0, outputImage.Width - 1 );
+      y = Util.Clamp( y, 0, outputImage.Height - 1 );
+
+      Color c = outputImage.GetPixel( x, y );
+      StringBuilder sb = new StringBuilder( titlePrefix );
+      sb.AppendFormat( " image[{0},{1}] = ", x, y );
+      if ( outputImage.PixelFormat == PixelFormat.Format32bppArgb ||
+           outputImage.PixelFormat == PixelFormat.Format64bppArgb ||
+           outputImage.PixelFormat == PixelFormat.Format16bppArgb1555 )
+        sb.AppendFormat( "[{0},{1},{2},{3}] = #{0:X02}{1:X02}{2:X02}{3:X02}", c.R, c.G, c.B, c.A );
+      else
+        sb.AppendFormat( "[{0},{1},{2}] = #{0:X02}{1:X02}{2:X02}", c.R, c.G, c.B );
+
+      Text = sb.ToString();
     }
 
     private void buttonOpen_Click ( object sender, EventArgs e )
@@ -42,23 +130,96 @@ namespace _005denoise
       if ( ofd.ShowDialog() != DialogResult.OK )
         return;
 
-      setImage( ref inputImage, (Bitmap)Image.FromFile( ofd.FileName ) );
+      newImage( ofd.FileName );
+    }
+
+    /// <summary>
+    /// Sets the new image defined by its file-name. Does all the checks.
+    /// </summary>
+    private bool newImage ( string fn )
+    {
+      Bitmap inp = null;
+      try
+      {
+        inp = (Bitmap)Image.FromFile( fn );
+      }
+      catch ( Exception )
+      {
+        return false;
+      }
+
+      if ( inp == null )
+        return false;
+
+      pictureBox1.Image = null;
+      setImage( ref inputImage, inp );
+      setImage( ref outputImage, null );
 
       recompute();
+
+      return true;
+    }
+
+    protected void EnableGUI ( bool enable )
+    {
+      buttonRedraw.Enabled =
+      buttonOpen.Enabled   =
+      buttonSave.Enabled   =
+      textParam.Enabled    = enable;
+      buttonStop.Enabled   = !enable;
+    }
+
+    delegate void StopComputationCallback ();
+
+    protected void StopComputation ()
+    {
+      if ( aThread == null )
+        return;
+
+      if ( buttonRedraw.InvokeRequired )
+      {
+        StopComputationCallback ea = new StopComputationCallback( StopComputation );
+        BeginInvoke( ea );
+      }
+      else
+      {
+        // actually stop the computation:
+        cont = false;
+        aThread.Join();
+        aThread = null;
+
+        // GUI stuff:
+        EnableGUI( true );
+      }
+    }
+
+    private void transform ()
+    {
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+
+      Bitmap bmp = Filter.Recompute( inputImage, textParam.Text );
+
+      sw.Stop();
+      float elapsed = 1.0e-3f * sw.ElapsedMilliseconds;
+
+      SetImage( bmp );
+      SetText( string.Format( CultureInfo.InvariantCulture, "Elapsed: {0:f3}s ({1})", elapsed, bmp.PixelFormat.ToString() ) );
+
+      StopComputation();
     }
 
     private void recompute ()
     {
-      if ( inputImage == null ) return;
+      if ( aThread != null ||
+           inputImage == null )
+        return;
 
-      Cursor.Current = Cursors.WaitCursor;
+      EnableGUI( false );
+      cont = true;
 
-      Bitmap newImage;
-      Denoise.TransformImage( inputImage, out newImage, (double)numericParam.Value );
-
-      setImage( ref outputImage, newImage );
-
-      Cursor.Current = Cursors.Default;
+      aThread = new Thread( new ThreadStart( this.transform ) );
+      aThread.Start();
     }
 
     private void buttonSave_Click ( object sender, EventArgs e )
@@ -70,15 +231,72 @@ namespace _005denoise
       sfd.Filter = "PNG Files|*.png";
       sfd.AddExtension = true;
       sfd.FileName = "";
+
       if ( sfd.ShowDialog() != DialogResult.OK )
         return;
 
-      outputImage.Save( sfd.FileName, System.Drawing.Imaging.ImageFormat.Png );
+      outputImage.Save( sfd.FileName, ImageFormat.Png );
     }
 
-    private void buttonRecalc_Click ( object sender, EventArgs e )
+    private void buttonRedraw_Click ( object sender, EventArgs e )
     {
       recompute();
+    }
+
+    private void buttonStop_Click ( object sender, EventArgs e )
+    {
+      StopComputation();
+    }
+
+    private void Form1_DragDrop ( object sender, DragEventArgs e )
+    {
+      string[] strFiles = (string[])e.Data.GetData( DataFormats.FileDrop );
+      newImage( strFiles[ 0 ] );
+    }
+
+    private void Form1_DragEnter ( object sender, DragEventArgs e )
+    {
+      if ( e.Data.GetDataPresent( DataFormats.FileDrop ) )
+        e.Effect = DragDropEffects.Copy;
+    }
+
+    private void Form1_FormClosing ( object sender, FormClosingEventArgs e )
+    {
+      StopComputation();
+    }
+
+    private void pictureBox1_MouseDown ( object sender, MouseEventArgs e )
+    {
+      if ( aThread == null &&
+           e.Button == MouseButtons.Left )
+        imageProbe( e.X, e.Y );
+    }
+
+    private void pictureBox1_MouseMove ( object sender, MouseEventArgs e )
+    {
+      if ( aThread == null &&
+           e.Button == MouseButtons.Left )
+        imageProbe( e.X, e.Y );
+    }
+
+    private void textParam_KeyPress ( object sender, KeyPressEventArgs e )
+    {
+      if ( e.KeyChar == (char)Keys.Enter )
+      {
+        e.Handled = true;
+        recompute();
+      }
+    }
+
+    private void textParam_MouseHover ( object sender, EventArgs e )
+    {
+      tt.Show( tooltip, (IWin32Window)sender, 10, -25, 2000 );
+    }
+
+    private void labelElapsed_MouseHover ( object sender, EventArgs e )
+    {
+      tt.Show( Util.TargetFramework + " (" + Util.RunningFramework + ')',
+               (IWin32Window)sender, 10, -25, 2000 );
     }
   }
 }
