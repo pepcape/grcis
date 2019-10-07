1e23a602ec5ffc82051c24b14bcfb5a1e99eb75b
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2019-10-07 07:16:03 +0000

    117raster: pilot histogram module (preparation).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@823 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 117raster/ModuleHistogram/HistogramForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace _066histogram
+{
+  partial class HistogramForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose ( bool disposing )
+    {
+      if ( disposing && (components != null) )
+      {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent ()
+    {
+      this.SuspendLayout();
+      // 
+      // HistogramForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(552, 262);
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.MinimumSize = new System.Drawing.Size(200, 100);
+      this.Name = "HistogramForm";
+      this.ShowIcon = false;
+      this.Text = "Histogram";
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HistogramForm_FormClosed);
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.HistogramForm_Paint);
+      this.Resize += new System.EventHandler(this.HistogramForm_Resize);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
