ca506b4b849130ace621e33bb700ac780159d594
pepcape <pepcape@gmail.com>
2019-10-07 04:21:59 +0000

    117raster: initial version.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@821 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ 117raster/FormMain.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace _117raster
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose (bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent ()
+    {
+      this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.comboBoxModule = new System.Windows.Forms.ComboBox();
+      this.buttonModule = new System.Windows.Forms.Button();
+      this.labelStatus = new System.Windows.Forms.Label();
+      this.buttonLoadImage = new System.Windows.Forms.Button();
+      this.buttonSaveImage = new System.Windows.Forms.Button();
+      this.buttonUpdateImage = new System.Windows.Forms.Button();
+      this.checkBoxResult = new System.Windows.Forms.CheckBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBoxParam = new System.Windows.Forms.TextBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBoxMain
+      // 
+      this.pictureBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBoxMain.Location = new System.Drawing.Point(13, 13);
+      this.pictureBoxMain.Name = "pictureBoxMain";
+      this.pictureBoxMain.Size = new System.Drawing.Size(859, 439);
+      this.pictureBoxMain.TabIndex = 0;
+      this.pictureBoxMain.TabStop = false;
+      this.pictureBoxMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMain_MouseDown);
+      this.pictureBoxMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMain_MouseMove);
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(13, 472);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(45, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Module:";
+      // 
+      // comboBoxModule
+      // 
+      this.comboBoxModule.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.comboBoxModule.FormattingEnabled = true;
+      this.comboBoxModule.Location = new System.Drawing.Point(65, 469);
+      this.comboBoxModule.Name = "comboBoxModule";
+      this.comboBoxModule.Size = new System.Drawing.Size(216, 21);
+      this.comboBoxModule.TabIndex = 2;
+      // 
+      // buttonModule
+      // 
+      this.buttonModule.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonModule.Location = new System.Drawing.Point(299, 468);
+      this.buttonModule.Name = "buttonModule";
+      this.buttonModule.Size = new System.Drawing.Size(108, 23);
+      this.buttonModule.TabIndex = 3;
+      this.buttonModule.Text = "Open module";
+      this.buttonModule.UseVisualStyleBackColor = true;
+      // 
+      // labelStatus
+      // 
+      this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.labelStatus.AutoEllipsis = true;
+      this.labelStatus.Location = new System.Drawing.Point(493, 471);
+      this.labelStatus.Name = "labelStatus";
+      this.labelStatus.Size = new System.Drawing.Size(377, 20);
+      this.labelStatus.TabIndex = 4;
+      this.labelStatus.Text = "---  status ---";
+      this.labelStatus.MouseHover += new System.EventHandler(this.labelStatus_MouseHover);
+      // 
+      // buttonLoadImage
+      // 
+      this.buttonLoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonLoadImage.Location = new System.Drawing.Point(16, 506);
+      this.buttonLoadImage.Name = "buttonLoadImage";
+      this.buttonLoadImage.Size = new System.Drawing.Size(127, 23);
+      this.buttonLoadImage.TabIndex = 5;
+      this.buttonLoadImage.Text = "Load image";
+      this.buttonLoadImage.UseVisualStyleBackColor = true;
+      this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+      // 
+      // buttonSaveImage
+      // 
+      this.buttonSaveImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonSaveImage.Location = new System.Drawing.Point(299, 506);
+      this.buttonSaveImage.Name = "buttonSaveImage";
+      this.buttonSaveImage.Size = new System.Drawing.Size(108, 23);
+      this.buttonSaveImage.TabIndex = 6;
+      this.buttonSaveImage.Text = "Save image";
+      this.buttonSaveImage.UseVisualStyleBackColor = true;
+      this.buttonSaveImage.Click += new System.EventHandler(this.buttonSaveImage_Click);
+      // 
+      // buttonUpdateImage
+      // 
+      this.buttonUpdateImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.buttonUpdateImage.Location = new System.Drawing.Point(162, 506);
+      this.buttonUpdateImage.Name = "buttonUpdateImage";
+      this.buttonUpdateImage.Size = new System.Drawing.Size(119, 23);
+      this.buttonUpdateImage.TabIndex = 7;
+      this.buttonUpdateImage.Text = "Update image";
+      this.buttonUpdateImage.UseVisualStyleBackColor = true;
+      // 
+      // checkBoxResult
+      // 
+      this.checkBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.checkBoxResult.AutoSize = true;
+      this.checkBoxResult.Location = new System.Drawing.Point(426, 472);
+      this.checkBoxResult.Name = "checkBoxResult";
+      this.checkBoxResult.Size = new System.Drawing.Size(51, 17);
+      this.checkBoxResult.TabIndex = 8;
+      this.checkBoxResult.Text = "result";
+      this.checkBoxResult.UseVisualStyleBackColor = true;
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(423, 511);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(40, 13);
+      this.label2.TabIndex = 9;
+      this.label2.Text = "Param:";
+      // 
+      // textBoxParam
+      // 
+      this.textBoxParam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBoxParam.Location = new System.Drawing.Point(474, 508);
+      this.textBoxParam.Name = "textBoxParam";
+      this.textBoxParam.Size = new System.Drawing.Size(396, 20);
+      this.textBoxParam.TabIndex = 10;
+      this.textBoxParam.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxParam_KeyPress);
+      this.textBoxParam.MouseHover += new System.EventHandler(this.textBoxParam_MouseHover);
+      // 
+      // FormMain
+      // 
+      this.AllowDrop = true;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(884, 581);
+      this.Controls.Add(this.textBoxParam);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.checkBoxResult);
+      this.Controls.Add(this.buttonUpdateImage);
+      this.Controls.Add(this.buttonSaveImage);
+      this.Controls.Add(this.buttonLoadImage);
+      this.Controls.Add(this.labelStatus);
+      this.Controls.Add(this.buttonModule);
+      this.Controls.Add(this.comboBoxModule);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.pictureBoxMain);
+      this.MinimizeBox = false;
+      this.MinimumSize = new System.Drawing.Size(650, 300);
+      this.Name = "FormMain";
+      this.Text = "117 raster";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+      this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FormMain_DragDrop);
+      this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormMain_DragEnter);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBoxMain;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ComboBox comboBoxModule;
+    private System.Windows.Forms.Button buttonModule;
+    private System.Windows.Forms.Label labelStatus;
+    private System.Windows.Forms.Button buttonLoadImage;
+    private System.Windows.Forms.Button buttonSaveImage;
+    private System.Windows.Forms.Button buttonUpdateImage;
+    private System.Windows.Forms.CheckBox checkBoxResult;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBoxParam;
+  }
+}
+
---
+++ 117raster/FormMain.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilities;
+
+namespace _117raster
+{
+  public partial class FormMain : Form
+  {
+    static readonly string rev = Util.SetVersion("$Rev$");
+
+    /// <summary>
+    /// Input image read from disk / drag-and-dropped.
+    /// </summary>
+    protected Bitmap inputImage = null;
+
+    /// <summary>
+    /// Optional output image for 'input->output' template.
+    /// </summary>
+    protected Bitmap outputImage = null;
+
+    /// <summary>
+    /// Window title prefix.
+    /// </summary>
+    protected string titlePrefix = null;
+
+    /// <summary>
+    /// Param string tooltip = help.
+    /// </summary>
+    string tooltip = "-- no tooltip yet --";
+
+    /// <summary>
+    /// Shared ToolTip instance.
+    /// </summary>
+    ToolTip tt = new ToolTip();
+
+    public FormMain ()
+    {
+      InitializeComponent();
+
+      titlePrefix = Text += " (" + rev + ")";
+    }
+
+    private static void setImage (ref Bitmap bakImage, Bitmap newImage)
+    {
+      bakImage?.Dispose();
+      bakImage = newImage;
+    }
+
+    /// <summary>
+    /// Picture-box background color (for alpha-images).
+    /// </summary>
+    public static Color imageBoxBackground = Color.White;
+
+    delegate void SetImageCallback (Bitmap newImage);
+
+    protected void SetImage (Bitmap newImage)
+    {
+      if (pictureBoxMain.InvokeRequired)
+      {
+        SetImageCallback si = new SetImageCallback(SetImage);
+        BeginInvoke(si, new object[] { newImage });
+      }
+      else
+      {
+        pictureBoxMain.Image = newImage;
+        pictureBoxMain.BackColor = imageBoxBackground;
+        setImage(ref outputImage, newImage);
+      }
+    }
+
+    delegate void SetTextCallback (string text);
+
+    protected void SetText (string text)
+    {
+      if (labelStatus.InvokeRequired)
+      {
+        SetTextCallback st = new SetTextCallback(SetText);
+        BeginInvoke(st, new object[] { text });
+      }
+      else
+        labelStatus.Text = text;
+    }
+
+    private void imageProbe (int x, int y)
+    {
+      if (outputImage == null)
+        return;
+
+      x = Util.Clamp(x, 0, outputImage.Width - 1);
+      y = Util.Clamp(y, 0, outputImage.Height - 1);
+
+      Color c = outputImage.GetPixel(x, y);
+      StringBuilder sb = new StringBuilder();
+      sb.AppendFormat(" image[{0},{1}] = ", x, y);
+      if (outputImage.PixelFormat == PixelFormat.Format32bppArgb ||
+          outputImage.PixelFormat == PixelFormat.Format64bppArgb ||
+          outputImage.PixelFormat == PixelFormat.Format16bppArgb1555)
+        sb.AppendFormat("[{0},{1},{2},{3}] = #{0:X02}{1:X02}{2:X02}{3:X02}", c.R, c.G, c.B, c.A);
+      else
+        sb.AppendFormat("[{0},{1},{2}] = #{0:X02}{1:X02}{2:X02}", c.R, c.G, c.B);
+
+      SetText(sb.ToString());
+    }
+
+    private void buttonLoadImage_Click (object sender, EventArgs e)
+    {
+      using (OpenFileDialog ofd = new OpenFileDialog()
+      {
+        Title = "Open Image File",
+        Filter = "Bitmap Files|*.bmp" +
+          "|Gif Files|*.gif" +
+          "|JPEG Files|*.jpg" +
+          "|PNG Files|*.png" +
+          "|TIFF Files|*.tif" +
+          "|All image types|*.bmp;*.gif;*.jpg;*.png;*.tif",
+        FilterIndex = 6,
+        FileName = ""
+      })
+      {
+        if (ofd.ShowDialog() == DialogResult.OK)
+          newImage(ofd.FileName);
+      }
+    }
+
+    /// <summary>
+    /// Sets the new image defined by its file-name. Does all the checks.
+    /// </summary>
+    /// <returns>True if succeeded.</returns>
+    private bool newImage (string fn)
+    {
+      Bitmap inp;
+      try
+      {
+        inp = (Bitmap)Image.FromFile(fn);
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+
+      if (inp == null)
+        return false;
+
+      pictureBoxMain.Image = null;
+      setImage(ref inputImage, inp);
+      setImage(ref outputImage, null);
+
+      //recompute();
+      SetImage(inputImage);
+
+      Text = titlePrefix + ' ' + fn;
+
+      return true;
+    }
+
+    private void buttonSaveImage_Click (object sender, EventArgs e)
+    {
+      if (outputImage == null)
+        return;
+
+      using (SaveFileDialog sfd = new SaveFileDialog
+      {
+        Title = "Save PNG file",
+        Filter = "PNG Files|*.png",
+        AddExtension = true,
+        FileName = ""
+      })
+      {
+        if (sfd.ShowDialog() == DialogResult.OK)
+          outputImage.Save(sfd.FileName, ImageFormat.Png);
+      }
+    }
+
+    private void FormMain_DragDrop (object sender, DragEventArgs e)
+    {
+      string[] strFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+      newImage(strFiles[0]);
+    }
+
+    private void FormMain_DragEnter (object sender, DragEventArgs e)
+    {
+      if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        e.Effect = DragDropEffects.Copy;
+    }
+
+    private void FormMain_FormClosing (object sender, FormClosingEventArgs e)
+    {
+      // !!! TODO: stop computing, etc.
+    }
+
+    private void FormMain_FormClosed (object sender, FormClosedEventArgs e)
+    {
+      inputImage?.Dispose();
+      outputImage?.Dispose();
+      tt.Dispose();
+    }
+
+    private void textBoxParam_KeyPress (object sender, KeyPressEventArgs e)
+    {
+      if (e.KeyChar == (char)Keys.Enter)
+      {
+        e.Handled = true;
+        // recompute();
+      }
+    }
+
+    private void textBoxParam_MouseHover (object sender, EventArgs e)
+    {
+      tt.Show(tooltip, (IWin32Window)sender, 10, -25, 2000);
+    }
+
+    private void labelStatus_MouseHover (object sender, EventArgs e)
+    {
+      tt.Show(Util.TargetFramework + " (" + Util.RunningFramework + ')',
+              (IWin32Window)sender, 10, -25, 2000);
+    }
+
+    private void pictureBoxMain_MouseDown (object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+        imageProbe(e.X, e.Y);
+    }
+
+    private void pictureBoxMain_MouseMove (object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+        imageProbe(e.X, e.Y);
+    }
+  }
+}
