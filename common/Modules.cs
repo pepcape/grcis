876493f12d62a0c1f0c22e5c2ad6bdee5065cb9e
pepcape <pepcape@gmail.com>
2019-10-07 07:16:03 +0000

    117raster: pilot histogram module (preparation).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@823 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- common/Modules.cs
+++ common/Modules.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 
 namespace Utilities
 {
-  public interface IModule : ICloneable
+  public interface IRasterModule : ICloneable
   {
     string Author
     {
@@ -17,27 +17,78 @@ namespace Utilities
     }
 
     /// <summary>
+    /// Tooltip for Param (text parameters).
+    /// </summary>
+    string Tooltip
+    {
+      get;
+    }
+
+    /// <summary>
     /// Param string was changed in the client/caller.
     /// </summary>
-    /// <param name="par"></param>
     void UpdateParam (string par);
 
     /// <summary>
     /// Activates/creates a new window.
+    /// Resets the input window.
     /// </summary>
     void InitWindow ();
 
     /// <summary>
     /// The input image was changed in the client/caller.
     /// </summary>
-    /// <param name="inputImage"></param>
     void InputImage (Bitmap inputImage);
 
     /// <summary>
-    /// Returns current output image.
+    /// Action performed at the given pixel.
+    /// Non-mandatory.
     /// </summary>
-    /// <returns></returns>
+    void Action (int x, int y);
+
+    /// <summary>
+    /// Returns current output image.
+    /// Can return null if the module doesn't compute an output image.
+    /// </summary>
     Bitmap OutputImage ();
+  }
+
+  public abstract class DefaultRasterModule
+  {
+    // Author & Name are mandatory.
+
+    public virtual string Tooltip => "-- no param --";
+
+    /// <summary>
+    /// Param string was changed in the client/caller.
+    /// Default behavior: do nothing.
+    /// </summary>
+    public virtual void UpdateParam (string par)
+    {}
+
+    /// <summary>
+    /// Activates/creates a new window.
+    /// Resets the input window.
+    /// Default behavior: no module window.
+    /// </summary>
+    public virtual void InitWindow ()
+    {}
+
+    // InputImage() is mandatory.
+
+    /// <summary>
+    /// Action performed at the given pixel.
+    /// Non-mandatory.
+    /// Default behavior: no action.
+    /// </summary>
+    public virtual void Action (int x, int y)
+    {}
+
+    /// <summary>
+    /// Returns current output image.
+    /// Can return null if the module doesn't compute an output image.
+    /// </summary>
+    public virtual Bitmap OutputImage () => null;
   }
 
   public class ModuleRegistry
@@ -45,16 +96,16 @@ namespace Utilities
     /// <summary>
     /// Module.Name => prototype class.
     /// </summary>
-    internal static Dictionary<string, IModule> reg = new Dictionary<string, IModule>();
+    internal static Dictionary<string, IRasterModule> reg = new Dictionary<string, IRasterModule>();
 
-    public static void Register (IModule m)
+    public static void Register (IRasterModule m)
     {
       reg[m.Name] = m;
     }
 
-    public static IModule CreateModule (string name)
+    public static IRasterModule CreateModule (string name)
     {
-      return reg.ContainsKey(name) ? (IModule)reg[name].Clone() : null;
+      return reg.ContainsKey(name) ? (IRasterModule)reg[name].Clone() : null;
     }
   }
 }
