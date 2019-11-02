3ca834757c658db16cdb995f729b9a2eb4abfee5
pepcape <pepcape@gmail.com>
2019-11-02 22:35:49 +0000

    093animation: format.
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@857 c021de9b-3f10-4dc4-9d02-502951e1e0de

--- 093animation/Animation.cs
+++ 093animation/Animation.cs
@@ -20,7 +20,7 @@ namespace _093animation
     /// <param name="fps">Frames-per-seconds.</param>
     /// <param name="param">Optional text to initialize the form's text-field.</param>
     /// <param name="tooltip">Optional tooltip = param help.</param>
-    public static void InitParams ( out string name, out int wid, out int hei, out double from, out double to, out double fps, out string param, out string tooltip )
+    public static void InitParams (out string name, out int wid, out int hei, out double from, out double to, out double fps, out string param, out string tooltip)
     {
       // {{
 
@@ -32,7 +32,7 @@ namespace _093animation
       hei = 480;
 
       // Animation.
-      from =  0.0;
+      from = 0.0;
       to   = 10.0;
       fps  = 25.0;
 
@@ -40,7 +40,7 @@ namespace _093animation
       param = "width=1.0,anti=true,objects=100,hatches=12,prob=0.95";
 
       // Tooltip = help.
-      tooltip = "width=<double>, anti=<bool>, objects=<int>, hatches=<int>, prob=<double>";
+      tooltip = "width=<double>, anti[=<bool>], objects=<int>, hatches=<int>, prob=<double>";
 
       // }}
     }
@@ -55,7 +55,7 @@ namespace _093animation
     /// <param name="end">End time (for animation length normalization).</param>
     /// <param name="fps">Required fps.</param>
     /// <param name="param">Optional string parameter from the form.</param>
-    public static void InitAnimation ( int width, int height, double start, double end, double fps, string param )
+    public static void InitAnimation (int width, int height, double start, double end, double fps, string param)
     {
       // {{ TODO: put your init code here
 
@@ -64,53 +64,52 @@ namespace _093animation
 
     /// <summary>
     /// Draw single animation frame.
+    /// Has to be re-entrant!
     /// </summary>
     /// <param name="c">Canvas to draw to.</param>
     /// <param name="time">Current time in seconds.</param>
     /// <param name="start">Start time (t0)</param>
     /// <param name="end">End time (for animation length normalization).</param>
     /// <param name="param">Optional string parameter from the form.</param>
-    public static void DrawFrame ( Canvas c, double time, double start, double end, string param )
+    public static void DrawFrame (Canvas c, double time, double start, double end, string param)
     {
       // {{ TODO: put your drawing code here
 
-      double timeNorm = Arith.Clamp( (time - start) / (end - start), 0.0, 1.0 );
+      double timeNorm = Arith.Clamp((time - start) / (end - start), 0.0, 1.0);
 
       // input params:
-      float penWidth = 1.0f;  // pen width
-      bool antialias = true;  // use anti-aliasing?
-      bool explicitAntialias = false;
-      int objects = 100;      // number of randomly generated objects (squares, stars, Brownian particles)
-      int hatches = 12;       // number of hatch-lines for the squares
-      double prob = 0.95;     // continue-probability for the Brownian motion simulator
+      float penWidth = 1.0f;   // pen width
+      bool antialias = false;  // use anti-aliasing?
+      int objects    = 100;    // number of randomly generated objects (squares, stars, Brownian particles)
+      int hatches    = 12;     // number of hatch-lines for the squares
+      double prob    = 0.95;   // continue-probability for the Brownian motion simulator
 
-      Dictionary<string, string> p = Util.ParseKeyValueList( param );
-      if ( p.Count > 0 )
+      Dictionary<string, string> p = Util.ParseKeyValueList(param);
+      if (p.Count > 0)
       {
         // with=<line-width>
-        if ( Util.TryParse( p, "width", ref penWidth ) )
+        if (Util.TryParse(p, "width", ref penWidth))
         {
-          if ( penWidth < 0.0f )
+          if (penWidth < 0.0f)
             penWidth = 0.0f;
         }
 
-        // anti=<bool>
-        if ( Util.TryParse( p, "anti", ref antialias ) )
-          explicitAntialias = true;
+        // anti[=<bool>]
+        Util.TryParse(p, "anti", ref antialias);
 
         // squares=<number>
-        if ( Util.TryParse( p, "objects", ref objects ) &&
-             objects < 0 )
+        if (Util.TryParse(p, "objects", ref objects) &&
+            objects < 0)
           objects = 0;
 
         // hatches=<number>
-        if ( Util.TryParse( p, "hatches", ref hatches ) &&
-             hatches < 1 )
+        if (Util.TryParse(p, "hatches", ref hatches) &&
+            hatches < 1)
           hatches = 1;
 
         // prob=<probability>
-        if ( Util.TryParse( p, "prob", ref prob ) &&
-             prob > 0.999 )
+        if (Util.TryParse(p, "prob", ref prob) &&
+            prob > 0.999)
           prob = 0.999;
       }
 
@@ -118,54 +117,55 @@ namespace _093animation
       int hq = c.Height / 4;
       int wh = wq + wq;
       int hh = hq + hq;
-      int minh = Math.Min( wh, hh );
+      int minh = Math.Min(wh, hh);
       double t;
       int i, j;
       double cx, cy, angle, x, y;
 
-      c.Clear( Color.Black );
+      c.Clear(Color.Black);
 
-      // 1st quadrant - star
-      c.SetPenWidth( penWidth );
-      c.SetAntiAlias( explicitAntialias && antialias );
+      // 1st quadrant - star.
+      c.SetPenWidth(penWidth);
+      c.SetAntiAlias(antialias);
 
       const int MAX_LINES = 30;
-      for ( i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES )
+      for (i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES)
       {
-        c.SetColor( Color.FromArgb( (i * 255) / MAX_LINES, 255, 255 - (i * 255) / MAX_LINES ) ); // [0,255,255] -> [255,255,0]
-        c.Line( t * wh, 0, wh - t * wh, hh );
+        c.SetColor(Color.FromArgb(i * 255 / MAX_LINES, 255, 255 - i * 255 / MAX_LINES)); // [0,255,255] -> [255,255,0]
+        c.Line(t * wh, 0, wh - t * wh, hh);
       }
-      for ( i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES )
+      for (i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES)
       {
-        c.SetColor( Color.FromArgb( 255, 255 - (i * 255) / MAX_LINES, (i * 255) / MAX_LINES ) ); // [255,255,0] -> [255,0,255]
-        c.Line( 0, hh - t * hh, wh, t * hh );
+        c.SetColor(Color.FromArgb(255, 255 - i * 255 / MAX_LINES, i * 255 / MAX_LINES)); // [255,255,0] -> [255,0,255]
+        c.Line(0, hh - t * hh, wh, t * hh);
       }
 
-      // 2nd quadrant - random hatched squares
+      // 2nd quadrant - random hatched squares.
       double size = minh / 10.0;
-      double padding = size * Math.Sqrt( 0.5 );
-      c.SetColor( Color.LemonChiffon );
-      c.SetPenWidth( 1.0f );
-      Random r = new Random( 12 );
+      double padding = size * Math.Sqrt(0.5);
+      c.SetColor(Color.LemonChiffon);
+      c.SetPenWidth(1.0f);
+      Random r = new Random(12);
 
-      for ( i = 0; i < objects; i++ )
+      for (i = 0; i < objects; i++)
       {
         do
           cx = r.NextDouble() * wh;
-        while ( cx < padding ||
-                cx > wh - padding );
-        c.SetAntiAlias( cx > wq );
+        while (cx < padding ||
+               cx > wh - padding);
+
+        c.SetAntiAlias(cx > wq);
         cx += wh;
 
         do
           cy = r.NextDouble() * hh;
-        while ( cy < padding ||
-                cy > hh - padding );
+        while (cy < padding ||
+               cy > hh - padding);
 
         angle = (r.NextDouble() + timeNorm) * Math.PI;
 
-        double dirx = Math.Sin( angle ) * size * 0.5;
-        double diry = Math.Cos( angle ) * size * 0.5;
+        double dirx = Math.Sin(angle) * size * 0.5;
+        double diry = Math.Cos(angle) * size * 0.5;
         cx -= dirx - diry;
         cy -= diry + dirx;
         double dx = -diry * 2.0 / hatches;
@@ -173,86 +173,89 @@ namespace _093animation
         double linx = dirx + dirx;
         double liny = diry + diry;
 
-        for ( j = 0; j++ < hatches; cx += dx, cy += dy )
-          c.Line( cx, cy, cx + linx, cy + liny );
+        for (j = 0; j++ < hatches; cx += dx, cy += dy)
+          c.Line(cx, cy, cx + linx, cy + liny);
       }
 
-      // 3rd quadrant - random stars
-      c.SetColor( Color.LightCoral );
-      c.SetPenWidth( penWidth );
+      // 3rd quadrant - random stars.
+      c.SetColor(Color.LightCoral);
+      c.SetPenWidth(penWidth);
       size = minh / 16.0;
       padding = size;
       const int MAX_SIDES = 30;
-      List<PointF> v = new List<PointF>( MAX_SIDES + 1 );
+      List<PointF> v = new List<PointF>(MAX_SIDES + 1);
 
-      for ( i = 0; i < objects; i++ )
+      for (i = 0; i < objects; i++)
       {
         do
           cx = r.NextDouble() * wh;
-        while ( cx < padding ||
-                cx > wh - padding );
-        c.SetAntiAlias( cx > wq );
+        while (cx < padding ||
+               cx > wh - padding);
+
+        c.SetAntiAlias(cx > wq);
 
         do
           cy = r.NextDouble() * hh;
-        while ( cy < padding ||
-                cy > hh - padding );
+        while (cy < padding ||
+               cy > hh - padding);
+
         cy += hh;
 
-        int sides = r.Next( 3, MAX_SIDES );
+        int sides = r.Next(3, MAX_SIDES);
         double dAngle = Math.PI * 2.0 / sides;
 
         v.Clear();
         angle = 0.0;
 
-        for ( j = 0; j++ < sides; angle += dAngle )
+        for (j = 0; j++ < sides; angle += dAngle)
         {
           double rad = size * (0.1 + 0.9 * r.NextDouble());
-          x = cx + rad * Math.Sin( angle );
-          y = cy + rad * Math.Cos( angle );
-          v.Add( new PointF( (float)x, (float)y ) );
+          x = cx + rad * Math.Sin(angle);
+          y = cy + rad * Math.Cos(angle);
+          v.Add(new PointF((float)x, (float)y));
         }
-        v.Add( v[ 0 ] );
-        c.PolyLine( v );
+        v.Add(v[0]);
+        c.PolyLine(v);
       }
 
-      // 4th quadrant - Brownian motion
-      c.SetPenWidth( penWidth );
-      c.SetAntiAlias( true );
+      // 4th quadrant - Brownian motion.
+      c.SetPenWidth(penWidth);
+      c.SetAntiAlias(true);
       size = minh / 10.0;
       padding = size;
 
-      for ( i = 0; i < objects; i++ )
+      for (i = 0; i < objects; i++)
       {
         do
           x = r.NextDouble() * wh;
-        while ( x < padding ||
-                x > wh - padding );
+        while (x < padding ||
+               x > wh - padding);
 
         do
           y = r.NextDouble() * hh;
-        while ( y < padding ||
-                y > hh - padding );
+        while (y < padding ||
+               y > hh - padding);
 
-        c.SetColor( Color.FromArgb( 127 + r.Next( 0, 128 ),
-                                    127 + r.Next( 0, 128 ),
-                                    127 + r.Next( 0, 128 ) ) );
+        c.SetColor(Color.FromArgb(127 + r.Next(0, 128),
+                                  127 + r.Next(0, 128),
+                                  127 + r.Next(0, 128)));
 
-        for ( j = 0; j++ < 1000; )
+        for (j = 0; j++ < 1000;)
         {
           angle = r.NextDouble() * Math.PI * 2.0;
           double rad = size * r.NextDouble();
-          cx = x + rad * Math.Sin( angle );
-          cy = y + rad * Math.Cos( angle );
-          if ( cx < 0.0 || cx > wh ||
-               cy < 0.0 || cy > hh )
+          cx = x + rad * Math.Sin(angle);
+          cy = y + rad * Math.Cos(angle);
+          if (cx < 0.0 || cx > wh ||
+              cy < 0.0 || cy > hh)
             break;
 
-          if ( j < 70.0 * timeNorm )
-            c.Line( x + wh, y + hh, cx + wh, cy + hh );
+          if (j < 70.0 * timeNorm)
+            c.Line(x + wh, y + hh, cx + wh, cy + hh);
 
-          x = cx; y = cy;
-          if ( r.NextDouble() > prob )
+          x = cx;
+          y = cy;
+          if (r.NextDouble() > prob)
             break;
         }
       }
