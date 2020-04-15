e94a5a279de0ee01cb7dfcc63607a3795b3e10e8
pepca <pepca@c021de9b-3f10-4dc4-9d02-502951e1e0de>
2020-04-15 01:30:29 +0000

    ObStripesTexture: Oblique stripes color texture (or Orienteering?).
    git-svn-id: svn://cgg.mff.cuni.cz/grcis/trunk@881 c021de9b-3f10-4dc4-9d02-502951e1e0de

---
+++ data/rtscenes/ToroidsOrienteering.cs
@@ -0,0 +1,66 @@
+public class ObStripesTexture : ITexture
+{
+  public double[] Color2;
+  public double Fu;
+  public double Fv;
+
+  public ObStripesTexture(double fu, double fv, double[] color)
+  {
+    Fu = fu;
+    Fv = fv;
+    Color2 = (double[])color.Clone();
+  }
+
+  public long Apply(Intersection inter)
+  {
+    double u = inter.TextureCoord.X * Fu;
+    double v = inter.TextureCoord.Y * Fv;
+
+    long ui = (long)System.Math.Floor(u);
+    long vi = (long)System.Math.Floor(v);
+
+    bool parity = ((ui + vi) & 1) != 0;
+    if (parity == ((u - ui) > (v - vi)))
+      System.Array.Copy(Color2, inter.SurfaceColor, System.Math.Min(Color2.Length, inter.SurfaceColor.Length));
+
+    inter.textureApplied = true;
+
+    return (ui + MathSupport.RandomStatic.numericRecipes(vi));
+  }
+}
+
+// CSG scene:
+var white = new double[] {1, 1, 1};
+var root = new CSGInnerNode(SetOperation.Union);
+root.SetAttribute(PropertyName.REFLECTANCE_MODEL, new PhongModel());
+root.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(white, 0.2, 0.8, 0.0, 1));
+scene.Intersectable = root;
+
+// Background color:
+scene.BackgroundColor = new double[] {0.05, 0.05, 0.05};
+
+// Camera:
+scene.Camera = new StaticCamera(new Vector3d(0.0, 0.0, -10.0),
+                                new Vector3d(0.0, -0.04, 1.0),
+                                60.0);
+
+// Light sources:
+scene.Sources = new System.Collections.Generic.LinkedList<ILightSource>();
+scene.Sources.Add(new AmbientLightSource(1.8));
+scene.Sources.Add(new PointLightSource(new Vector3d(-5.0, 3.0, -3.0), 2.0));
+
+// --- NODE DEFINITIONS ----------------------------------------------------
+
+var red = new double[] { 0.7, 0, 0 };
+
+var t = new Torus(2.0, 1.0);
+t.SetAttribute(PropertyName.TEXTURE, new ObStripesTexture(2, 2, red));
+root.InsertChild(t, Matrix4d.Scale(0.8) * Matrix4d.CreateRotationX(0.9) * Matrix4d.CreateTranslation(2.7, 1.0, 0.0));
+
+t = new Torus(1.5, 0.8);
+t.SetAttribute(PropertyName.TEXTURE, new ObStripesTexture(4, 4, red));
+root.InsertChild(t, Matrix4d.CreateRotationX(0.8) * Matrix4d.CreateTranslation(-2.5, 0.0, 0.0));
+
+t = new Torus(2.0, 1.0);
+t.SetAttribute(PropertyName.TEXTURE, new ObStripesTexture(6, 2, red));
+root.InsertChild(t, Matrix4d.Scale(0.6) * Matrix4d.CreateRotationX(1.3) * Matrix4d.CreateTranslation(1.6, -2.2, 0.0));
