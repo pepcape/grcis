537d81533f7437b3f31b17d0e4de3bb499a46de8
pepca <pepcape@gmail.com>
2021-09-28 23:44:23 +0200

    0974 051: modernization.

--- 051colormap/Colormap.cs
+++ 051colormap/Colormap.cs
@@ -8,7 +8,7 @@ namespace _051colormap
     /// <summary>
     /// Form data initialization.
     /// </summary>
-    public static void InitForm ( out string author )
+    public static void InitForm (out string author)
     {
       author = "Josef Pelikán";
     }
@@ -19,37 +19,39 @@ namespace _051colormap
     /// <param name="input">Input raster image.</param>
     /// <param name="numCol">Required colormap size (ignore it if you must).</param>
     /// <param name="colors">Output palette (array of colors).</param>
-    public static void Generate ( Bitmap input, int numCol, out Color[] colors )
+    public static void Generate (Bitmap input, int numCol, out Color[] colors)
     {
       // !!!{{ TODO - generate custom palette based on the given image
 
       int width  = input.Width;
       int height = input.Height;
 
-      colors = new Color[ numCol ];            // accepting the required palette size..
+      colors = new Color[numCol];            // accepting the required palette size..
 
-      colors[ 0 ] = input.GetPixel( 0, 0 );    // upper left image corner
+      colors[0] = input.GetPixel(0, 0);      // upper left image corner
 
       double H, S, V;
-      Color center = input.GetPixel( width / 2, height / 2 ); // image center
-      Arith.ColorToHSV( center, out H, out S, out V );
-      if ( S > 1.0e-3 )
-        colors[ numCol - 1 ] = Arith.HSVToColor( H, 1.0, 1.0 ); // non-monochromatic color => using Hue only
+      Color center = input.GetPixel(width / 2, height / 2);   // image center
+      Arith.ColorToHSV(center, out H, out S, out V);
+      if (S > 1.0e-3)
+        colors[numCol - 1] = Arith.HSVToColor(H, 1.0, 1.0);   // non-monochromatic color => using Hue only
       else
-        colors[ numCol - 1 ] = center;                          // monochromatic color => using it directly
+        colors[numCol - 1] = center;                          // monochromatic color => using it directly
 
       // color-ramp linear interpolation:
-      float r = colors[ 0 ].R;
-      float g = colors[ 0 ].G;
-      float b = colors[ 0 ].B;
-      float dr = (colors[ numCol - 1 ].R - r) / (numCol - 1.0f);
-      float dg = (colors[ numCol - 1 ].G - g) / (numCol - 1.0f);
-      float db = (colors[ numCol - 1 ].B - b) / (numCol - 1.0f);
+      float r = colors[0].R;
+      float g = colors[0].G;
+      float b = colors[0].B;
+      float dr = (colors[numCol - 1].R - r) / (numCol - 1.0f);
+      float dg = (colors[numCol - 1].G - g) / (numCol - 1.0f);
+      float db = (colors[numCol - 1].B - b) / (numCol - 1.0f);
 
-      for ( int i = 1; i < numCol; i++ )
+      for (int i = 1; i < numCol; i++)
       {
-        r += dr; g += dg; b += db;
-        colors[ i ] = Color.FromArgb( (int)r, (int)g, (int)b );
+        r += dr;
+        g += dg;
+        b += db;
+        colors[i] = Color.FromArgb((int)r, (int)g, (int)b);
       }
 
       // !!!}}
--- 051colormap/Program.cs
+++ 051colormap/Program.cs
@@ -5,12 +5,12 @@ using System.Windows.Forms;
 
 namespace _051colormap
 {
-  [Designer( "System.Windows.Forms.Design.PictureBoxDesigner, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" )]
+  [Designer("System.Windows.Forms.Design.PictureBoxDesigner, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
   public class MyBox : PictureBox
   {
-    protected override void OnPaint ( PaintEventArgs pe )
+    protected override void OnPaint (PaintEventArgs pe)
     {
-      if ( Form1.colors == null ) return;
+      if (Form1.colors == null) return;
 
       // Get the graphics object
       Graphics gfx = pe.Graphics;
@@ -23,27 +23,27 @@ namespace _051colormap
       int y = 0;
 
       // 1. create color brushes:
-      Brush[] brushes = new Brush[ stripes ];
-      for ( int i = 0; i < stripes; i++ )
-        brushes[ i ] = new SolidBrush( Form1.colors[ i ] );
-      Brush brBlack = new SolidBrush( Color.Black );
-      Brush brWhite = new SolidBrush( Color.White );
+      Brush[] brushes = new Brush[stripes];
+      for (int i = 0; i < stripes; i++)
+        brushes[i] = new SolidBrush(Form1.colors[i]);
+      Brush brBlack = new SolidBrush(Color.Black);
+      Brush brWhite = new SolidBrush(Color.White);
 
       // 2. draw color stripes:
-      Font myFont = new System.Drawing.Font( "Helvetica", 10, FontStyle.Regular );
-      for ( int i = 0; i < stripes; i++, y += stripHeight )
+      Font myFont = new System.Drawing.Font("Helvetica", 10, FontStyle.Regular);
+      for (int i = 0; i < stripes; i++, y += stripHeight)
       {
-        gfx.FillRectangle( brushes[ i ], 0, y, width, stripHeight );
-        for ( int j = 0; j < stripes; j++ )
-          gfx.DrawString( "test", myFont, brushes[ j ], 4 + j * columnWidth, y + 4 );
-        gfx.DrawString( Form1.color2string( Form1.colors[ i ] ),
-                        myFont, Form1.colors[ i ].GetBrightness() < 0.5f ? brWhite : brBlack,
-                        12, y + stripHeight - 20 );
+        gfx.FillRectangle(brushes[i], 0, y, width, stripHeight);
+        for (int j = 0; j < stripes; j++)
+          gfx.DrawString("test", myFont, brushes[j], 4 + j * columnWidth, y + 4);
+        gfx.DrawString(Form1.color2string(Form1.colors[i]),
+                       myFont, Form1.colors[i].GetBrightness() < 0.5f ? brWhite : brBlack,
+                       12, y + stripHeight - 20);
       }
     }
   }
 
-  [Designer( "System.Windows.Forms.Design.PictureBoxDesigner, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" )]
+  [Designer("System.Windows.Forms.Design.PictureBoxDesigner, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
   public class BaseBox : PictureBox
   {
     public Color color
@@ -52,16 +52,16 @@ namespace _051colormap
       set;
     }
 
-    protected override void OnPaint ( PaintEventArgs pe )
+    protected override void OnPaint (PaintEventArgs pe)
     {
       Graphics gfx = pe.Graphics;
 
       int width = this.Size.Width;
       int height = this.Size.Height;
 
-      Brush brush = new SolidBrush( color );
+      Brush brush = new SolidBrush(color);
 
-      gfx.FillRectangle( brush, 0, 0, width, height );
+      gfx.FillRectangle(brush, 0, 0, width, height);
     }
   }
 
@@ -74,8 +74,8 @@ namespace _051colormap
     static void Main ()
     {
       Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault( false );
-      Application.Run( new Form1() );
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
     }
   }
 }
